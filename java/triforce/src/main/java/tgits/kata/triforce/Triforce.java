e064cf90d152a0f7a47e0495a9445489411ddb57
TGITS <christophe.vaudry@gmail.com>
2017-02-05 15:27:45 +0100

    Correcting the .gitignore in order to have te possibility to commit pom.xml file

---
+++ java/triforce/src/main/java/tgits/kata/triforce/Triforce.java
@@ -0,0 +1,104 @@
+package tgits.kata.triforce;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class Triforce 
+{
+	private Triforce(){
+    	super();
+    }
+	
+    public static void main( String[] args )
+    {	
+    	drawTriForce(createTriforce(1));
+    	System.out.println("");
+    	drawTriForce(createTriforce(2));
+    	System.out.println("");
+    	drawTriForce(createTriforce(3));    	
+    }
+    
+    public static String center(String centralPattern, int width){
+    	if (centralPattern == null) {
+    		throw new IllegalArgumentException("The provided String must not be null");
+    	}
+    	
+    	if (width <= 0) {
+    		throw new IllegalArgumentException("The provided integer must be strictly positive");
+    	}
+    	
+    	StringBuilder sb = new StringBuilder();
+    	int padding = (width - centralPattern.length()) / 2 ;
+    	pad(sb, padding, ' ');
+    	sb.append(centralPattern);
+    	pad(sb, padding, ' ');
+    	return sb.toString();
+    }
+   
+    public static void pad(StringBuilder sb, int padding, char paddingChar){
+    	for(int i = 0; i < padding; i++){
+    		sb.append(paddingChar);
+    	}
+    }
+    
+    public static int calculateNumberOfCharactersForALine(int seed) {
+    	if (seed <= 0) {
+    		throw new IllegalArgumentException("The provided integer must be strictly positive");
+    	}
+    	return 2 * seed - 1;
+    }
+    
+    public static String createStringPattern(char base, int repetition){
+    	if (repetition <= 0) {
+    		throw new IllegalArgumentException("The provided integer must be strictly positive");
+    	}
+    	
+    	StringBuilder sb = new StringBuilder();
+    	for(int i = 0; i < repetition; i++){
+    		sb.append(base);
+    	}
+    	return sb.toString();
+    }
+    
+    public static int calculateMaxWidthForSingleTriangle(int height){
+    	if (height <= 0) {
+    		throw new IllegalArgumentException("The provided integer must be strictly positive");
+    	}
+    	
+    	return (2 * height - 1) * 2 + 1;
+    }
+    
+    public static int calculateMaxWidthForDuplicateTriangle(int height){
+    	if (height <= 0) {
+    		throw new IllegalArgumentException("The provided integer must be strictly positive");
+    	}
+    	
+    	return (2 * height - 1);
+    }
+    
+    public static List<String> createTriforce(int height) {
+    	List<String> lines = new ArrayList<>();
+    	int maxWidth = calculateMaxWidthForSingleTriangle(height);
+    	String line;
+    	for(int i=1; i<=height; i++) {
+    		line = center(createStringPattern('*', calculateNumberOfCharactersForALine(i)),maxWidth);
+    		lines.add(line);
+    	}
+    	maxWidth = calculateMaxWidthForDuplicateTriangle(height);
+    	for(int i=1; i<=height; i++) {
+    		line = center(createStringPattern('*', calculateNumberOfCharactersForALine(i)),maxWidth);
+    		lines.add(line + " " + line);
+    	}
+    	return lines;
+    }
+    
+    public static void drawTriForce(List<String> lines) {
+    	for(String line:lines){
+    		System.out.println(line);
+    	}
+    }
+}
---
+++ java/triforce/src/test/java/tgits/kata/triforce/TriforceTest.java
@@ -0,0 +1,63 @@
+package tgits.kata.triforce;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TriforceTest 
+{
+	@Test
+	public void testPad() {
+	   String expected = "  *";
+	   char paddingChar = ' ';
+	   int padding = 2;
+	   StringBuilder sb = new StringBuilder();
+	   Triforce.pad(sb, padding, paddingChar);
+	   sb.append('*');
+	   String result = sb.toString();
+	   Assert.assertEquals(expected, result);
+	}
+	 
+    @Test
+    public void testCenter() {
+    	String centralPattern = "*";
+    	String expectedResult = " * ";
+        Assert.assertEquals(expectedResult, Triforce.center(centralPattern,3));
+        centralPattern = "***";
+    	expectedResult = "  ***  ";
+        Assert.assertEquals(expectedResult, Triforce.center(centralPattern,7));
+        centralPattern = "***";
+    	expectedResult = "   ***   ";
+        Assert.assertEquals(expectedResult, Triforce.center(centralPattern,9));
+        centralPattern = "**";
+    	expectedResult = "  **  ";
+        Assert.assertEquals(expectedResult, Triforce.center(centralPattern,6));
+    }
+    
+    @Test
+    public void testCalculateNumberOfCharactersForALine() {
+    	int seed = 1;
+    	int expected = 1;
+    	Assert.assertEquals(expected, Triforce.calculateNumberOfCharactersForALine(seed));
+    	seed = 2;
+    	expected = 3;
+    	Assert.assertEquals(expected, Triforce.calculateNumberOfCharactersForALine(seed));
+    	seed = 10;
+    	expected = 19;
+    	Assert.assertEquals(expected, Triforce.calculateNumberOfCharactersForALine(seed));
+    }
+    
+    @Test
+    public void testCalculateMaxWidth() {
+    	int height = 1;
+    	int expected = 3;
+    	Assert.assertEquals(expected, Triforce.calculateMaxWidthForSingleTriangle(height));
+    	height = 2;
+    	expected = 7;
+    	Assert.assertEquals(expected, Triforce.calculateMaxWidthForSingleTriangle(height));
+    }
+    
+    @Test
+    public void testCreateTriforce() {
+    	
+    }
+}
