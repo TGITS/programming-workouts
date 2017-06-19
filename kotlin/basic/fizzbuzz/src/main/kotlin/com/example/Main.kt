b5519b33e25320450f0f56c3a667f1e4bf1c0d5e
TGITS <christophe.vaudry@gmail.com>
2017-06-19 08:41:05 +0200

    Utilisation de when dans FizzBuzz pour Kotlin

--- kotlin/basic/fizzbuzz/src/main/kotlin/com/example/Main.kt
+++ kotlin/basic/fizzbuzz/src/main/kotlin/com/example/Main.kt
@@ -1,9 +1,13 @@
 package com.example
 
 fun main(args: Array<String>): Unit {
-    println("\n\nAnd know your average FizzBuzz for the numbers between 1 and 20\n\n")
-    val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
-    val toFizzbuzz = numbers.map { fizzbuzzification(it) }.forEach { print(it + " ") }
+    println("\n\nAnd now your average FizzBuzz for the numbers between 1 and 30\n\n")
+    val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30)
+    println ("Fizzbuzzification of ${numbers} without when")
+    numbers.map { fizzbuzzification(it) }.forEach { print(it + " ") }
+    println ("")
+    println ("Fizzbuzzification of ${numbers} with when")
+    numbers.map { fizzbuzzificationWithWhen(it) }.forEach { print(it + " ") }
 }
 
 fun fizzbuzzification(number: Int): String {
@@ -14,6 +18,15 @@ fun fizzbuzzification(number: Int): String {
     } else if (number % 5 == 0) {
         "Buzz"
     } else {
-        return number.toString()
+        number.toString()
+    }
+}
+
+fun fizzbuzzificationWithWhen(number: Int): String {
+    return when {
+        number % 15 == 0 -> "FizzBuzz"
+        number % 3 == 0  ->  "Fizz"
+        number % 5 == 0 -> "Buzz"
+        else  -> number.toString()
     }
 }
