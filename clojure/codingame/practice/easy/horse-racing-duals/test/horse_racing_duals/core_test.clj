5d579ef95739b20b21ec33f9d031fb4806c19ff8
TGITS <christophe.vaudry@gmail.com>
2017-07-03 09:04:29 +0200

    Solution en Clojure du puzzle facile de CodinGame "Horse Racing Duals".

---
+++ clojure/codingame/practice/easy/horse-racing-duals/test/horse_racing_duals/core_test.clj
@@ -0,0 +1,7 @@
+(ns horse-racing-duals.core-test
+  (:require [clojure.test :refer :all]
+            [horse-racing-duals.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
