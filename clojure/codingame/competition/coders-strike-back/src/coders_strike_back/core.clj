7b6b59b8c393ede7e81ee47f9a9a27194bc37e3d
TGITS <christophe.vaudry@gmail.com>
2017-07-08 13:24:57 +0200

    Evolution en cours de la logique de fonctionnement [Silver League en cours]

--- clojure/codingame/competition/coders-strike-back/src/coders_strike_back/core.clj
+++ clojure/codingame/competition/coders-strike-back/src/coders_strike_back/core.clj
@@ -3,12 +3,37 @@
 
 (defn rand-boolean [] (= (rem (rand-int 10) 2) 0))
 
-(defn use-boost [boost-used?] (if (not @boost-used?) (if (rand-boolean) (do (reset! boost-used? true) "BOOST") "100") "100"))
+;; Random boost using
+(defn rand-boost [boost-used?] (if (not @boost-used?) (if (rand-boolean) (do (reset! boost-used? true) "BOOST") "100") "100"))
 
-(defn compute-thrust [next-checkpoint-angle boost-used?]
+;; Distance entre 2 points ou norme du vecteur Norme = ||(x, y) || = sqrt(x*x+y*y)
+(defn distance [x y] (Math/sqrt (+ (* x x) (* y y))))
+;; Conversion degrés en radians :
+;; rad  = degrés * M_PI / 180
+(defn convert-degree-to-radian [degree]
+  (* degree (/ Math/PI 180) )
+  )
+
+(defn calculate-thrust-from-angle [angle] (str (Integer/valueOf (Math/round (* 100 (Math/cos (convert-degree-to-radian angle)))))))
+
+;; Ajouter le boost quand l’angle est bon et la distance assez grande
+;; Si il reste un boost
+;; Si l’angle est < 10
+;; Si la distance à la cible est > 4000
+;;Alors « BOOST »
+(defn use-boost [next-checkpoint-angle next-checkpoint-x next-checkpoint-y boost-used?]
+      (if (and (not @boost-used?) (or (< next-checkpoint-angle 10) (> next-checkpoint-angle -10)) (> (distance next-checkpoint-x next-checkpoint-y) 4000))
+          (do (reset! boost-used? true)
+              "BOOST"
+              (calculate-thrust-from-angle next-checkpoint-angle)) (calculate-thrust-from-angle next-checkpoint-angle)))
+
+(defn compute-action [next-checkpoint-angle next-checkpoint-x next-checkpoint-y boost-used?]
       (cond
-        (or (> next-checkpoint-angle 90) (< next-checkpoint-angle -90)) "0"
-         true (use-boost boost-used?)))
+        (>= (Math/abs next-checkpoint-angle) 90) "0"
+        (<= 90 (Math/abs next-checkpoint-angle)  60) "25"
+        (<= 60 (Math/abs next-checkpoint-angle)  45) "50"
+        (<= 45 (Math/abs next-checkpoint-angle)  30) "75"
+         true (use-boost next-checkpoint-angle next-checkpoint-x next-checkpoint-y boost-used?)))
 
 (def boost-used? (atom false))
 
@@ -30,5 +55,5 @@
                   (print " ")
                   (print nextCheckpointY)
                   (print " ")
-                  (print (compute-thrust (Integer/valueOf nextCheckpointAngle) boost-used?))
+                  (print (compute-action (Integer/valueOf nextCheckpointAngle) (Integer/valueOf nextCheckpointX) (Integer/valueOf nextCheckpointY) boost-used?))
                   (println ""))))
