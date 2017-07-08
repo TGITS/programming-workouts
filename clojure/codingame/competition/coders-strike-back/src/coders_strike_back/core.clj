df2e6860fea9f20f5b3688af8f8dc5cbef0ba56e
TGITS <christophe.vaudry@gmail.com>
2017-07-08 16:44:58 +0200

    Développements en cours sur CSB (CodinGame). Au fin fond du classement de la league Silver... Algorithme à améliorer. Pas encore d'utilisation du bouclier.

--- clojure/codingame/competition/coders-strike-back/src/coders_strike_back/core.clj
+++ clojure/codingame/competition/coders-strike-back/src/coders_strike_back/core.clj
@@ -1,53 +1,41 @@
 (ns coders-strike-back.core
   (:gen-class))
 
-;(defn rand-boolean [] (= (rem (rand-int 10) 2) 0))
+(defn inc-atom [curr-val] (inc curr-val))
+(defn dec-atom [curr-val] (dec curr-val))
 
-;; Random boost using
-;(defn rand-boost [boost-used?] (if (not @boost-used?) (if (rand-boolean) (do (reset! boost-used? true) "BOOST") "100") "100"))
-
-;; Distance entre 2 points ou norme du vecteur Norme = ||(x, y) || = sqrt(x*x+y*y)
-(defn distance [x y] (Math/sqrt (+ (* x x) (* y y))))
+;; norme du vecteur Norme = ||(x, y) || = sqrt(x*x+y*y)
+(defn norm [x y] (Math/sqrt (+ (* x x) (* y y))))
+;;distance between 2 points
+(defn distance [x1 y1 x2 y2] (Math/sqrt (+ (Math/pow (- x2 x1) 2) (Math/pow (- y2 y1) 2))))
 ;; Conversion degrés en radians :
 ;; rad  = degrés * M_PI / 180
 (defn convert-degree-to-radian [degree]
-  (* degree (/ Math/PI 180) )
-  )
+  (* degree (/ Math/PI 180)))
 
-(defn calculate-thrust-from-angle [angle] (str (Integer/valueOf (Math/round (* 100 (Math/abs (Math/cos (convert-degree-to-radian angle))))))))
+;;Compute boost value between 0 and 100
+(defn compute-boost [next-checkpoint-distance next-checkpoint-angle]
+  (cond
+    (> (Math/abs next-checkpoint-angle) 90) "0"
+    (< (Math/abs next-checkpoint-angle) 30) "100"
+    (and (< next-checkpoint-distance 1000) (> (Math/abs next-checkpoint-angle) 30)) "0"
+    (and (< next-checkpoint-distance 1000) (< (Math/abs next-checkpoint-angle) 30)) "50"
+    (> next-checkpoint-distance 4000) "100"
+    true (str (Integer/valueOf (Math/round (* 100 (Math/abs (Math/cos (convert-degree-to-radian next-checkpoint-angle)))))))))
 
-;; Ajouter le boost quand l’angle est bon et la distance assez grande
-;; Si il reste un boost
-;; Si l’angle est < 10
-;; Si la distance à la cible est > 4000
-;;Alors « BOOST »
-;;(defn use-boost [next-checkpoint-angle next-checkpoint-x next-checkpoint-y boost-used?]
-;;      (if (and (not @boost-used?) (or (< next-checkpoint-angle 10) (> next-checkpoint-angle -10)) (> (distance next-checkpoint-x next-checkpoint-y) 4000))
-;;          (do (reset! boost-used? true)
-;;              "BOOST"
-;;              (calculate-thrust-from-angle next-checkpoint-angle)) (calculate-thrust-from-angle next-checkpoint-angle)))
+(defn compute-x [next-checkpoint-x] next-checkpoint-x)
 
-;;(defn compute-action [next-checkpoint-angle next-checkpoint-x next-checkpoint-y boost-used?]
-;;      (cond
-;;        (>= (Math/abs next-checkpoint-angle) 90) "0"
-;;        (<= 90 (Math/abs next-checkpoint-angle)  60) "25"
-;;        (<= 60 (Math/abs next-checkpoint-angle)  45) "50"
-;;        (<= 45 (Math/abs next-checkpoint-angle)  30) "75"
-;;         true (use-boost next-checkpoint-angle next-checkpoint-x next-checkpoint-y boost-used?)))
+(defn compute-y [next-checkpoint-y] next-checkpoint-y)
 
 (def boost-used? (atom false))
+(def turn-counter (atom 0))
 
-(defn compute-action [next-checkpoint-angle next-checkpoint-x next-checkpoint-y boost-used?]
-      (binding [*out* *err*]
-          (println (str
-                     "distance next-checkpoint-x next-checkpoint-y : " (str (distance next-checkpoint-x next-checkpoint-y))
-                     " ; pod angle with checkpoint : " (str next-checkpoint-angle))))
-      (binding [*out* *err*]
-                    (println (str "Boost used ? (in compute-action) : " @boost-used? )))
-      (if (and (not @boost-used?) (< (Math/abs next-checkpoint-angle) 10) (> (distance next-checkpoint-x next-checkpoint-y) 4000))
+;;Compute BOOST, SHIELD or boost value between 0 and 100
+(defn compute-action [next-checkpoint-x next-checkpoint-y next-checkpoint-distance next-checkpoint-angle opponent-x opponent-y boost-used? turn-counter]
+      (if (and (not @boost-used?) (> @turn-counter 3) (< (Math/abs next-checkpoint-angle) 10) (> next-checkpoint-distance 4000))
           (do (reset! boost-used? true)
-              "BOOST"
-              (calculate-thrust-from-angle next-checkpoint-angle)) (calculate-thrust-from-angle next-checkpoint-angle)))
+              "BOOST")
+          (compute-boost next-checkpoint-distance next-checkpoint-angle)))
 
 (defn -main [& args]
       (while true
@@ -57,16 +45,27 @@
                   ; nextCheckpointDist: distance to the next checkpoint
                   ; nextCheckpointAngle: angle between your pod orientation and the direction of the next checkpoint
 
+                  (swap! turn-counter inc-atom)
+
                   ; (binding [*out* *err*]
                   ;   (println "Debug messages..."))
                   (binding [*out* *err*]
-                    (println (str "Boost used ?" @boost-used? )))
+                    (println (str "Has the boost be used ? : " @boost-used? ))
+                    (println (str "Given next check point distance : " nextCheckpointDist))
+                    (println (str "Calculated next check point distance : " (distance x y opponentX opponentY)))
+                    (println (str "Pod angle with checkpoint : " nextCheckpointAngle))
+                    (println (str "Turn : " @turn-counter)))
+
                   ; You have to output the target position
                   ; followed by the power (0 <= thrust <= 100)
                   ; i.e.: "x y thrust"
-                  (print nextCheckpointX)
+                  (print (compute-x nextCheckpointX))
                   (print " ")
-                  (print nextCheckpointY)
+                  (print (compute-y nextCheckpointY))
                   (print " ")
-                  (print (compute-action (Integer/valueOf nextCheckpointAngle) (Integer/valueOf nextCheckpointX) (Integer/valueOf nextCheckpointY) boost-used?))
+                  (print (compute-action
+                           (Integer/valueOf nextCheckpointX) (Integer/valueOf nextCheckpointY)
+                           (Integer/valueOf nextCheckpointDist) (Integer/valueOf nextCheckpointAngle)
+                           (Integer/valueOf opponentX) (Integer/valueOf opponentY)
+                           boost-used? turn-counter))
                   (println ""))))
