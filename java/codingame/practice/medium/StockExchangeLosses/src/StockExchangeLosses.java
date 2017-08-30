e7b9102ef4a468be9e619159a7e40ef138348e8a
TGITS <thegeekintheshell@gmail.com>
2017-08-30 22:33:11 +0200

    WIP - Solution non-optimisée de StockExchangeLosses

--- java/codingame/practice/medium/StockExchangeLosses/src/StockExchangeLosses.java
+++ java/codingame/practice/medium/StockExchangeLosses/src/StockExchangeLosses.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.util.stream.*;
 
 /**
  * Auto-generated code below aims at helping you parse the standard input
@@ -10,30 +9,23 @@ class Solution {
 	public static void main(String args[]) {
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
-		Integer[] values = new Integer[n];
+		int[] values = new int[n];
 		for (int i = 0; i < n; i++) {
 			values[i] = in.nextInt();
 		}
 		in.close();
-		
-		System.err.println("Values : " + String.join(";", Stream.of(values).map(i -> Integer.toString(i)).collect(Collectors.joining(" , "))));
-		
-		List<Integer> gaps = new ArrayList<>();
-		
-		for(int i=0; i < values.length - 1; i++){
-			for (int j = i+1; j < values.length; j++){
-				if(values[j]<values[i]) {
-					gaps.add(values[i] - values[j]);
+
+		int gap = 0;
+
+		for (int i = 0; i < values.length - 1; i++) {
+			for (int j = i + 1; j < values.length; j++) {
+				if (gap < (values[i] - values[j])) {
+					gap = values[i] - values[j];
 				}
 			}
 		}
-		int gap = 0;
-		if(!gaps.isEmpty()) {
-			gap = -Collections.max(gaps);
-		}
-		
-		System.err.println("Gap : " + gap);
-		System.out.println(gap);
+
+		System.out.println(-gap);
 	}
-	
+
 }
