0ef54c68246664a50deac4f400760b3f0c18b171
TGITS <thegeekintheshell@gmail.com>
2017-09-01 14:11:37 +0200

    Solution en Java au puzzle medium CG "Stock Exchange Losses"

--- java/codingame/practice/medium/StockExchangeLosses/src/StockExchangeLosses.java
+++ java/codingame/practice/medium/StockExchangeLosses/src/StockExchangeLosses.java
@@ -12,16 +12,29 @@ class Solution {
 		int[] values = new int[n];
 		for (int i = 0; i < n; i++) {
 			values[i] = in.nextInt();
+			System.err.print(values[i] + " ");
 		}
+		System.err.println();
 		in.close();
 
 		int gap = 0;
 
-		for (int i = 0; i < values.length - 1; i++) {
+		int i = 0;
+		while(i < values.length - 1){
+			int temp = 0;
 			for (int j = i + 1; j < values.length; j++) {
+			    if (values[j] > values[i]) {
+			        break;
+			    }
 				if (gap < (values[i] - values[j])) {
 					gap = values[i] - values[j];
+					temp = j;
 				}
+			}
+			if(temp == 0){
+				i++;
+			}else {
+				i = temp;
 			}
 		}
 
