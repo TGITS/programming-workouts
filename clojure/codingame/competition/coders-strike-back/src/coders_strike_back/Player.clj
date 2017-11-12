116009e00c4c6602cc6061659ee16cefdc407508
TGITS <thegeekintheshell@gmail.com>
2017-11-12 14:36:42 +0100

    WIP - Evolution du bot CSB en Clojure avec l'objectif d'améliorer le classement en ligue argent

--- clojure/codingame/competition/coders-strike-back/src/coders_strike_back/Player.clj
+++ clojure/codingame/competition/coders-strike-back/src/coders_strike_back/Player.clj
@@ -3,6 +3,7 @@
 
 (def checkpoint-core-size 100)
 (def delay-between-shield-use 10)
+(def min-distance-to-activate-shield 950)
 
 (defn inc-atom [curr-val] (inc curr-val))
 (defn dec-atom [curr-val] (dec curr-val))
@@ -32,17 +33,17 @@
   (if (> next-checkpoint-distance 1000)
     (cond
       (and (>= next-checkpoint-angle -90) (<= next-checkpoint-angle 90) (before next-checkpoint-x x))
-      (convert-to-int (+ next-checkpoint-x (* checkpoint-core-size (Math/cos next-checkpoint-angle))))
+      (convert-to-int (+ next-checkpoint-x (* checkpoint-core-size (Math/cos (convert-degree-to-radian next-checkpoint-angle)))))
       (and (>= next-checkpoint-angle -90) (<= next-checkpoint-angle 90) (after next-checkpoint-x x))
-      (convert-to-int (- next-checkpoint-x (* checkpoint-core-size (Math/cos next-checkpoint-angle))))
+      (convert-to-int (- next-checkpoint-x (* checkpoint-core-size (Math/cos (convert-degree-to-radian next-checkpoint-angle)))))
       (or (<= -180 next-checkpoint-angle -90 ) (>= 180 next-checkpoint-angle 90))
-      (convert-to-int (- next-checkpoint-x (* checkpoint-core-size (Math/cos next-checkpoint-angle)))))
+      (convert-to-int (- next-checkpoint-x (* checkpoint-core-size (Math/cos (convert-degree-to-radian next-checkpoint-angle))))))
     next-checkpoint-x))
 
 (defn compute-y [y next-checkpoint-y next-checkpoint-angle next-checkpoint-distance]
   (if (cond
-        (and (>= next-checkpoint-angle 0)  (<= next-checkpoint-angle 180)) (convert-to-int (- next-checkpoint-y (* checkpoint-core-size (Math/sin next-checkpoint-angle))))
-        (and (< next-checkpoint-angle 0) (>= next-checkpoint-angle -180)) (convert-to-int (+ next-checkpoint-y (* checkpoint-core-size (Math/sin next-checkpoint-angle)))))
+        (and (>= next-checkpoint-angle 0)  (<= next-checkpoint-angle 180)) (convert-to-int (- next-checkpoint-y (* checkpoint-core-size (Math/sin (convert-degree-to-radian next-checkpoint-angle)))))
+        (and (< next-checkpoint-angle 0) (>= next-checkpoint-angle -180)) (convert-to-int (+ next-checkpoint-y (* checkpoint-core-size (Math/sin (convert-degree-to-radian next-checkpoint-angle))))))
     next-checkpoint-y))
 
 ;;Angle entre 2 points (x1,y1) et (x2,y2)
@@ -58,6 +59,7 @@
   (cond
     ;(>= (Math/abs next-checkpoint-angle) 90) "0"
     (<= next-checkpoint-distance 1000) "40"
+    (<= next-checkpoint-distance 800) "20"
     (<= (Math/abs next-checkpoint-angle) 18) "100"
     true (compute-boost-from-angle (Math/abs next-checkpoint-angle))))
 
@@ -70,7 +72,7 @@
     (and (not (= @last-boost-value "SHIELD")) (not (= @last-boost-value "BOOST")) (= @last-shield-usage 0))))
 
 (defn use-shield? [x y opponent-x opponent-y boost-used? game-loop-counter max-shield-usage last-shield-usage last-boost-value]
-  (and (= @last-shield-usage 0) (> @game-loop-counter 10) (> @max-shield-usage 0) (< (distance opponent-x opponent-y x y) 1000)))
+  (and (= @last-shield-usage 0) (> @game-loop-counter 10) (> @max-shield-usage 0) (< (distance opponent-x opponent-y x y) min-distance-to-activate-shield)))
 
 ;;Compute BOOST, SHIELD or boost value between 0 and 100
 (defn compute-action [next-checkpoint-x next-checkpoint-y next-checkpoint-distance next-checkpoint-angle x y opponent-x opponent-y boost-used? game-loop-counter max-shield-usage last-shield-usage last-boost-value]
@@ -102,21 +104,21 @@
 
         ; (binding [*out* *err*]
         ;   (println "Debug messages..."))
-        ;(binding [*out* *err*]
-        ;  (println (str "Has the boost been used ? : " @boost-used?))
-        ;  (println (str "Next check point distance coordinates : (" nextCheckpointX "," nextCheckpointY ")"))
-        ;  (println (str "Given next check point distance : " nextCheckpointDist))
-        ;  (println (str "Calculated next check point distance : " (distance x y nextCheckpointX nextCheckpointY)))
-        ;  (println (str "Pod angle with checkpoint : " nextCheckpointAngle))
-        ;  (println (str "Calculated Pod angle with checkpoint : " (angle-between-2-points x y nextCheckpointX nextCheckpointY)))
-        ;  (println (str "Pod Position : (" x "," y ")"))
-        ;  (println (str "Opponent pod Position : (" opponentX "," opponentY ")"))
-        ;  (println (str "Opponent distance : " opponent-distance))
-        ;  (println (str "Calculated Pod angle with opponent : " (angle-between-2-points x y opponentX opponentY)))
-        ;  (println (str "Max Shield Usage : " @max-shield-usage))
-        ;  (println (str "Last Shield Usage : " @last-shield-usage))
-        ;  (println (str "Game loop counter : " @game-loop-counter))
-        ;  (println (str "Last boost value : " @last-boost-value)))
+        (binding [*out* *err*]
+          (println (str "Has the boost been used ? : " @boost-used?))
+          (println (str "Next check point distance coordinates : (" nextCheckpointX "," nextCheckpointY ")"))
+          (println (str "Given next check point distance : " nextCheckpointDist))
+          (println (str "Calculated next check point distance : " (distance x y nextCheckpointX nextCheckpointY)))
+          (println (str "Pod angle with checkpoint : " nextCheckpointAngle))
+          (println (str "Calculated Pod angle with checkpoint : " (angle-between-2-points x y nextCheckpointX nextCheckpointY)))
+          (println (str "Pod Position : (" x "," y ")"))
+          (println (str "Opponent pod Position : (" opponentX "," opponentY ")"))
+          (println (str "Opponent distance : " opponent-distance))
+          (println (str "Calculated Pod angle with opponent : " (angle-between-2-points x y opponentX opponentY)))
+          (println (str "Max Shield Usage : " @max-shield-usage))
+          (println (str "Last Shield Usage : " @last-shield-usage))
+          (println (str "Game loop counter : " @game-loop-counter))
+          (println (str "Last boost value : " @last-boost-value)))
 
 
 
