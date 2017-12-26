65b1ba313130b134f9594d499923efcaf4a8ccae
TGITS <thegeekintheshell@gmail.com>
2017-12-26 10:03:44 +0100

    Solution en Scala complète au puzzle "community" "Bruce Lee" avant nettoyage du code. Solution assez inélégante mais fonctionnelle.

--- scala/codingame/practice/community/bruce_lee/src/main/scala/Solution.scala
+++ scala/codingame/practice/community/bruce_lee/src/main/scala/Solution.scala
@@ -8,52 +8,52 @@ import scala.io._
   **/
 object Solution extends App {
   val encrypt = StdIn.readLine
-  val pattern = """^((00 0+( )?)|(0 0+( )?))+$""".r
+  //val pattern = """((00 0+ ?)|(0 0+ ?))+""".r
   Console.err.println("encrypt : " + encrypt)
-  val decoded = encrypt match {
-    case pattern(_*) => {
-      val encryptToArray = encrypt.split(" ")
-      if (encryptToArray.size % 2 == 0) {
-        val encryptToList = encryptToArray.sliding(2, 2).toList
-        Console.err.println("encryptToList : " + encryptToList.mkString(" "))
-        val binarySequence = encryptToList.foldLeft("") {
-          (accumulator, current) => {
-            Console.err.println("accumulator : " + accumulator)
-            Console.err.println("current : " + current.mkString(";"))
-            if (accumulator.equals("INVALID")) {
-              "INVALID"
-            } else {
-              val value = current(0) match {
-                case "0" => "1"
-                case "00" => "0"
-                case _ => "INVALID"
-              }
-              if (value.equals("INVALID")) {
-                "INVALID"
-              }
-              else {
-                accumulator + value * current(1).length
-              }
-            }
+  /*val decoded = encrypt match {
+    case pattern(_*) => {*/
+  val encryptToArray = encrypt.split(" ")
+  val decoded = if (encryptToArray.size % 2 == 0) {
+    val encryptToList = encryptToArray.sliding(2, 2).toList
+    Console.err.println("encryptToList : " + encryptToList.mkString(" "))
+    val binarySequence = encryptToList.foldLeft("") {
+      (accumulator, current) => {
+        Console.err.println("accumulator : " + accumulator)
+        Console.err.println("current : " + current.mkString(";"))
+        if (accumulator.equals("INVALID")) {
+          "INVALID"
+        } else {
+          val value = current(0) match {
+            case "0" => "1"
+            case "00" => "0"
+            case _ => "INVALID"
           }
-        }
-        Console.err.println("binarySequence : " + binarySequence.mkString(""))
-        if (!binarySequence.equals("INVALID")) {
-          if (binarySequence.length >= 7) {
-          binarySequence.sliding(7, 7).map(it => Integer.parseInt(it, 2).toChar.toString).mkString("")
-          }
-          else {
+          if (value.equals("INVALID")) {
             "INVALID"
           }
-        } else {
-          "INVALID"
+          else {
+            accumulator + value * current(1).length
+          }
         }
-      } else {
-        "INVALID"
       }
     }
-    case _ => "INVALID"
+    Console.err.println("binarySequence : " + binarySequence.mkString(""))
+    if (!binarySequence.equals("INVALID")) {
+      if (binarySequence.length >= 7) {
+        binarySequence.sliding(7, 7).map(it => Integer.parseInt(it, 2).toChar.toString).mkString("")
+      }
+      else {
+        "INVALID"
+      }
+    } else {
+      "INVALID"
+    }
+  } else {
+    "INVALID"
   }
+  /*}
+  case _ => "INVALID"
+}*/
 
   Console.err.println("decoded : " + decoded)
   println(decoded)
