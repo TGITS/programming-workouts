ee3f17e42bb6072b161a2d194a5eb880d199143a
TGITS <thegeekintheshell@gmail.com>
2017-12-26 10:33:19 +0100

    Solution en Scala au puzzle "community" "Bruce Lee". Il existe probablement des solutions plus élégantes mais elle a le mérite de fonctionner. Il était intéressant d'essayer d'utiliser le foldLeft.

--- scala/codingame/practice/community/bruce_lee/src/main/scala/Solution.scala
+++ scala/codingame/practice/community/bruce_lee/src/main/scala/Solution.scala
@@ -1,60 +1,33 @@
-import math._
-import scala.util._
 import scala.io._
 
-/**
-  * Auto-generated code below aims at helping you parse
-  * the standard input according to the problem statement.
-  **/
 object Solution extends App {
+  val InvalidInput = "INVALID"
   val encrypt = StdIn.readLine
-  //val pattern = """((00 0+ ?)|(0 0+ ?))+""".r
-  Console.err.println("encrypt : " + encrypt)
-  /*val decoded = encrypt match {
-    case pattern(_*) => {*/
+  Console.err.println("Given input string : " + encrypt)
   val encryptToArray = encrypt.split(" ")
-  val decoded = if (encryptToArray.size % 2 == 0) {
+  val decoded = if (!encrypt.isEmpty && encryptToArray.size % 2 == 0) {
     val encryptToList = encryptToArray.sliding(2, 2).toList
-    Console.err.println("encryptToList : " + encryptToList.mkString(" "))
     val binarySequence = encryptToList.foldLeft("") {
       (accumulator, current) => {
-        Console.err.println("accumulator : " + accumulator)
-        Console.err.println("current : " + current.mkString(";"))
-        if (accumulator.equals("INVALID")) {
-          "INVALID"
+        if (accumulator.equals(InvalidInput)) {
+          InvalidInput
         } else {
-          val value = current(0) match {
-            case "0" => "1"
-            case "00" => "0"
-            case _ => "INVALID"
-          }
-          if (value.equals("INVALID")) {
-            "INVALID"
-          }
-          else {
-            accumulator + value * current(1).length
+          current(0) match {
+            case "0" => accumulator + "1" * current(1).length
+            case "00" => accumulator + "0" * current(1).length
+            case "" => InvalidInput
+            case _ => InvalidInput
           }
         }
       }
     }
-    Console.err.println("binarySequence : " + binarySequence.mkString(""))
-    if (!binarySequence.equals("INVALID")) {
-      if (binarySequence.length >= 7) {
-        binarySequence.sliding(7, 7).map(it => Integer.parseInt(it, 2).toChar.toString).mkString("")
-      }
-      else {
-        "INVALID"
-      }
+    if (!binarySequence.equals(InvalidInput) && (binarySequence.length >= 7) && (binarySequence.length % 7 == 0)) {
+      binarySequence.sliding(7, 7).map(it => Integer.parseInt(it, 2).toChar.toString).mkString("")
     } else {
-      "INVALID"
+      InvalidInput
     }
   } else {
-    "INVALID"
+    InvalidInput
   }
-  /*}
-  case _ => "INVALID"
-}*/
-
-  Console.err.println("decoded : " + decoded)
   println(decoded)
 }
