cca85a22eff76680cb0478877c2cf224cb80fadc
TGITS <thegeekintheshell@gmail.com>
2018-01-22 14:11:12 +0100

    WIP - Travail en cours sur la version en Python de la solution au puzzle medium de CG "Bender episode 1". Accent mis sur l'apprentissage de Python plus que sur la résolution du problème en lui-même.

--- python/codingame/practice/medium/Bender_Episode1/bender-episode-1.py
+++ python/codingame/practice/medium/Bender_Episode1/bender-episode-1.py
@@ -9,12 +9,28 @@ import math
 
 # What are the abstractions needed to express the problem ?
 
-class Cell:
-    """The class Cell represent an element of the map."""
+class Coordinate:
+    """Class to represent a Coordinate x,y. We can add to coordinate."""
 
-    def __init__(self,x,y,content):
+    def __init__(self, x, y):
+        """Initialisation of the class Coordinate which needs a x and a y coordinate"""
         self.x = x
         self.y = y
+
+    def __str__(self):
+        """String representation of a Coordinate"""
+        return "({0},{1})".format(self.x, self.y)
+
+    def __add__(self, other_coordinate):
+        """Addition of 2 coordinate - Return a new object"""
+        return Coordinate(self.x + other_coordinate.x, self.y + other_coordinate.y)
+
+class Cell:
+    """The class Cell is a class that represent an element of the map."""
+
+    def __init__(self, coordinate, content):
+        """Initialisation of the class Cell which needs a coordinate and a content"""
+        self.coordinate = coordinate
         self.content = content
 
     def is_start(self):
@@ -51,37 +67,41 @@ class Cell:
 
     def is_path_modifier(self):
         """Return True if the cell is a path modifier"""
-        return self.content == "S" or self.content == "N" or self.content == "W" of self.content == "E"    
+        return self.content == "S" or self.content == "N" or self.content == "W" or self.content == "E"    
 
     def __str__(self):
         """String representation of a Cell"""
-        return "{0} at ({1},{2})".format(self.content, self.x, self.y)
+        return "{0} at {1}".format(self.content, str(self.coordinate))
 
 class CityMap:
     """The class CityMap represent the map of the city in which Bender is moving."""
 
     def __init__(self, height, width, city_map):
+        """The initialisation method of the class CityMap which needs the height and the width of the map, 
+        and the map of the city (a list of list of cells)"""
         self.height = height
         self.width = width
         self.map = city_map
 
-    def display(self, output=sys.stderr):
-        """Print the map"""
+    def cell_at(self, coordinate):
+        """Return the cell which is at the coordinate given in parameter"""
+        return self.map[coordinate.x][coordinate.y]
+
+    def __str__(self):
+        """String representation of the map"""
+        str = ""
         for row in self.map :
             for cell in row :
-                print("{0}".format(cell.content), file=output, end='', flush=True)
-            print(flush=True)
-
-    def get_cell(x,y):
-        return self.map[x][y]
+                str += "{0}".format(cell.content)
+            str += "\n"
+        return str
 
 class Bender:
     """The class Bender represent the robot of the type of bender"""
 
-    def __init__(self, x, y, city_map, direction="SOUTH"):
+    def __init__(self, coordinate, city_map, direction="SOUTH"):
         """Initialisation of Bender object"""
-        self.x = x
-        self.y = y
+        self.coordinate = coordinate
         self.city_map = city_map
         self.direction = direction
         self.direction_priorities = ["SOUTH", "EAST", "NORTH", "WEST"]
@@ -89,25 +109,24 @@ class Bender:
         self.in_breaker_mode = False
         self.moves = []
         self.direction_to_coordinate_mapping = { 
-            "SOUTH" : (0,1),
-            "NORTH" : (0,-1),
-            "EAST"  : (1,0),
-            "WEST"  : (-1,0)
+            "SOUTH" : Coordinate(0,1),
+            "NORTH" : Coordinate(0,-1),
+            "EAST"  : Coordinate(1,0),
+            "WEST"  : Coordinate(-1,0)
         }
-        
     
     def __update_direction(self, direction):
         """Private method to update the direction of Bender"""
         self.direction = direction
 
-    def __update_position(self,x,y):
-        self.x = x
-        self.y = y
+    def __update_coordinate(self,coordinate):
+        """Private method to update the coordinate of a bender object"""
+        self.coordinate = coordinate
 
     def __next_position(self):
         """Determine the next position of Bender"""
-        x_offset, y_offset = self.direction_to_coordinate_mapping[self.direction]
-        return self.x + x_offset, self.y + y_offset
+        position_offset = self.direction_to_coordinate_mapping[self.direction]
+        return self.coordinate + position_offset
 
     def __reverse_direction_priorities(self):
         """Reverser the list of direction priorities"""
@@ -123,8 +142,8 @@ class Bender:
 
     def compute_moves(self):
         """Compute the list of moves of Bender. The list contains only the value 'LOOP' if bender cannot attain the suicide booth"""
-        x,y = self.__next_position()
-        next_cell = self.city_map.get_cell(x,y)
+        next_position = self.__next_position()
+        next_cell = self.city_map.cell_at(next_position)
         if next_cell.is_obstacle:
             if self.in_breaker_mode and next_cell.is_breakable_obstacle:
                 pass
@@ -137,7 +156,7 @@ class Bender:
 
     def __str__(self):
         """The String value of a Bender object"""
-        return "Position : ({0},{1}) - Direction : {2}".format(self.x,self.y,self.direction)
+        return "Position : {0} - Direction : {1}".format(self.coordinate,self.direction)
 
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
@@ -148,24 +167,21 @@ start = None
 suicide_booth = None
 teleporters = []
 
-
 for i in range(l):
     row = list(input())
     row_of_cells = []
     for j in range(c):
-        current_cell = Cell(j,i,row[j])
+        current_cell = Cell(Coordinate(j,i),row[j])
         row_of_cells.append(current_cell)
         if current_cell.is_start():
             start_cell = current_cell
     city_map.append(row_of_cells)
 
 futurama = CityMap(l, c, city_map)
-futurama.display()
-bender = Bender(start_cell.x,start_cell.y, city_map)
+print(str(futurama), file=sys.stderr)
+bender = Bender(start_cell.coordinate, city_map)
 print(str(bender), file=sys.stderr)
 print(str(start_cell), file=sys.stderr)
-# print("map : {0}".format(str(city_map)), file=sys.stderr)
-
 
 # Write an action using print
 # To debug: print("Debug messages...", file=sys.stderr)
