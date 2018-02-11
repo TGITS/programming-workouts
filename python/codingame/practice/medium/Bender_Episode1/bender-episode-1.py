bd2d4c6932854e0e9c2baedaeacd4debf3c3b2df
TGITS <thegeekintheshell@gmail.com>
2018-02-11 18:56:28 +0100

    WIP - Travail sur une solution en Python 3 au puzzle CG "Bender - part 1"

--- python/codingame/practice/medium/Bender_Episode1/bender-episode-1.py
+++ python/codingame/practice/medium/Bender_Episode1/bender-episode-1.py
@@ -10,10 +10,10 @@ import math
 # What are the abstractions needed to express the problem ?
 
 class Coordinate:
-    """Class to represent a Coordinate x,y. We can add to coordinate."""
+    """Class to represent a Coordinate x,y. We can add two coordinates."""
 
     def __init__(self, x, y):
-        """Initialisation of the class Coordinate which needs a x and a y coordinate"""
+        """Initialisation of the class Coordinate which needs an 'x' and a 'y' coordinate"""
         self.x = x
         self.y = y
 
@@ -22,7 +22,7 @@ class Coordinate:
         return "({0},{1})".format(self.x, self.y)
 
     def __add__(self, other_coordinate):
-        """Addition of 2 coordinate - Return a new object"""
+        """Addition of 2 coordinates - Return a new object"""
         return Coordinate(self.x + other_coordinate.x, self.y + other_coordinate.y)
 
 class Cell:
@@ -41,6 +41,10 @@ class Cell:
         """Return True if the cell is a suicide booth"""
         return self.content == "$"
 
+    def is_blank(self):
+        """Return True if the cell is a blank cell"""
+        return self.content == " "
+
     def is_obstacle(self):
         """Return True if the cell is an obstacle (breakable or not)"""
         return self.content == "X" or self.content == "#"
@@ -48,6 +52,11 @@ class Cell:
     def is_breakable_obstacle(self):
         """Return True if the cell is a breakable obstacle """
         return self.content == "X"
+
+    def break_obstacle(self):
+        """Delete the breakable obstacle"""
+        if self.is_breakable_obstacle():
+            self.content = " "
 
     def is_unbreakable_obstacle(self):
         """Return True if the cell is an unbreakable obstacle"""
@@ -69,6 +78,14 @@ class Cell:
         """Return True if the cell is a path modifier"""
         return self.content == "S" or self.content == "N" or self.content == "W" or self.content == "E"    
 
+    def get_content(self):
+        """Get the content of the cell"""
+        return self.content
+
+    def get_coordinate(self):
+        """Get the coordinate of the cell"""
+        return self.coordinate
+
     def __str__(self):
         """String representation of a Cell"""
         return "{0} at {1}".format(self.content, str(self.coordinate))
@@ -76,17 +93,31 @@ class Cell:
 class CityMap:
     """The class CityMap represent the map of the city in which Bender is moving."""
 
-    def __init__(self, height, width, city_map):
+    def __init__(self, height, width, city_map, start_cell, teleporters):
         """The initialisation method of the class CityMap which needs the height and the width of the map, 
         and the map of the city (a list of list of cells)"""
         self.height = height
         self.width = width
         self.map = city_map
+        self.start_cell = start_cell
+        self.teleporters = teleporters
 
     def cell_at(self, coordinate):
         """Return the cell which is at the coordinate given in parameter"""
         return self.map[coordinate.x][coordinate.y]
 
+    def get_other_teleporter(self, teleporter):
+        """Return the other teleporter"""
+        teleporter_index = self.teleporters.index(teleporter)
+        if teleporter_index == 0:
+            return self.teleporters[1]
+        else:
+            return self.teleporters[0]
+
+    def get_number_of_cells(self):
+        """Give the number of cells on this map"""
+        return self.height * self.width
+        
     def __str__(self):
         """String representation of the map"""
         str = ""
@@ -99,14 +130,16 @@ class CityMap:
 class Bender:
     """The class Bender represent the robot of the type of bender"""
 
-    def __init__(self, coordinate, city_map, direction="SOUTH"):
+    def __init__(self, coordinate, city_map):
         """Initialisation of Bender object"""
         self.coordinate = coordinate
         self.city_map = city_map
-        self.direction = direction
+        self.direction = "SOUTH"
         self.direction_priorities = ["SOUTH", "EAST", "NORTH", "WEST"]
+        self.next_direction_index = 1
         self.selected_direction_index = self.direction_priorities.index(direction)
         self.in_breaker_mode = False
+        self.dead = False
         self.moves = []
         self.direction_to_coordinate_mapping = { 
             "SOUTH" : Coordinate(0,1),
@@ -114,10 +147,15 @@ class Bender:
             "EAST"  : Coordinate(1,0),
             "WEST"  : Coordinate(-1,0)
         }
+        self.times_blocked = 0
     
     def __update_direction(self, direction):
         """Private method to update the direction of Bender"""
         self.direction = direction
+
+    def __change_direction(self):
+        """To change the direction of Bender - The next direction in the possible direction is taken"""
+        self.next_direction_index = (self.next_direction_index + 1) % 4
 
     def __update_coordinate(self,coordinate):
         """Private method to update the coordinate of a bender object"""
@@ -139,20 +177,105 @@ class Bender:
     def __is_in_breaker_mode(self):
         """Return true if Bender is in Breaker mode"""
         return self.in_breaker_mode   
+    
+    def __is_in_normal_mode(self):
+        """Return true if Bender is in Normal mode"""
+        return not self.in_breaker_mode   
 
-    def compute_moves(self):
-        """Compute the list of moves of Bender. The list contains only the value 'LOOP' if bender cannot attain the suicide booth"""
+    def __commit_suicide(self):
+        self.dead = True
+
+    def __is_dead(self):
+        """Return true if Bender is Dead"""
+        return self.dead
+    
+    def __inc_times_blocked(self):
+        """Increment the number of times Bender has been blocked"""
+        self.times_blocked += 1
+
+    def __reset_times_blocked(self):
+        """Reset the value of times_blocked to 0"""
+        self.times_blocked = 0
+
+    def __is_blocked(self):
+        """Return true if Bender is blocked, this is if the times_blocked is greater than 4"""    
+
+    def __compute_next_move(self):
+        """Compute the next moves and position of Bender. The list contains only the value 'LOOP' if bender cannot attain the suicide booth"""
+        current_direction = self.direction
         next_position = self.__next_position()
         next_cell = self.city_map.cell_at(next_position)
-        if next_cell.is_obstacle:
-            if self.in_breaker_mode and next_cell.is_breakable_obstacle:
-                pass
-            else:
-                pass
+        if next_cell.is_suicide_booth():
+            self.__reset_times_blocked()
+            self.__update_coordinate(next_position)
+            self.__commit_suicide()
+            return current_direction
 
+        if next_cell.is_blank():
+            self.__reset_times_blocked()
+            self.__update_coordinate(next_position)
+            return current_direction
+
+        if next_cell.is_beer():
+            self.__reset_times_blocked()
+            self.__toogle_breaker_mode()
+            self.__update_coordinate(next_position)
+            return current_direction
+
+        if next_cell.is_inverter():
+            self.__reverse_direction_priorities()
+            self.__reset_times_blocked()
+            self.__update_coordinate(next_position)
+            return current_direction   
+
+        if next_cell.is_teleporter():
+            self.__reset_times_blocked()
+            current_cell = self.city_map.get_other_teleporter()
+            self.__update_coordinate(current_cell.get_position())
+            return current_direction
+
+        if next_cell.is_unbreakable_obstacle():
+            self.__change_direction()
+            self.__inc_times_blocked()
+            return self.__compute_next_move()  
+
+        if next_cell.is_unbreakable_obstacle() and self.__is_blocked():
+            return "LOOP"    
+
+        if next_cell.is_path_modifier():
+            self.__update_direction(next_cell.get_content())
+            self.__reset_times_blocked()
+            self.__update_coordinate(next_position)
+            return current_direction
+
+        if self.__is_in_normal_mode():
+            #Obstacle in normal mode Bender change direction and will try to move in another cell
+            if next_cell.is_breakable_obstacle() and self.__is_blocked():
+               return "LOOP"
+
+            if next_cell.is_breakable_obstacle():
+               self.__change_direction()
+               self.__inc_times_blocked()
+               return self.__compute_next_move()   
+            
+        if self.__is_in_breaker_mode() and next_cell.is_breakable_obstacle():
+            next_cell.break_obstacle()
+            self.__reset_times_blocked()
+            return self.__compute_next_move()
+        
     def get_computed_moves(self):
         """Print the list of computed moves"""
-        pass
+        moves = []
+        max_iterations = self.city_map.get_number_of_cells() * 2
+        number_iterations = 0
+        while not self.__is_dead and number_iterations < max_iterations :
+            moves.append(self.__compute_next_move())
+            number_iterations += 1
+
+        if number_iterations >= max_iterations or "LOOP" in moves :
+            return "LOOP"
+        else:
+           return " ".join(moves)
 
     def __str__(self):
         """The String value of a Bender object"""
@@ -175,15 +298,19 @@ for i in range(l):
         row_of_cells.append(current_cell)
         if current_cell.is_start():
             start_cell = current_cell
+        if current_cell.is_teleporter():
+            teleporters.append(current_cell)
     city_map.append(row_of_cells)
 
-futurama = CityMap(l, c, city_map)
+futurama = CityMap(l, c, city_map, start_cell, teleporters)
 print(str(futurama), file=sys.stderr)
 bender = Bender(start_cell.coordinate, city_map)
 print(str(bender), file=sys.stderr)
 print(str(start_cell), file=sys.stderr)
-
+result = bender.get_computed_moves()
+print(result, file=sys.stderr)
+print(result)
 # Write an action using print
 # To debug: print("Debug messages...", file=sys.stderr)
 
-print("answer")
+#print("answer")
