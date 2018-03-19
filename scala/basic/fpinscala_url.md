f78ef7c0eb404be2380b2294101c8f907f9b3e46
TGITS <thegeekintheshell@gmail.com>
2018-03-19 09:27:41 +0100

    Ajout des corrections de FPIS et proposition de solution pour curry, uncurry et compose

---
+++ scala/basic/fpinscala_url.md
@@ -0,0 +1,3 @@
+= Some URL in relation to Functional Programming in Scala
+
+* https://github.com/fpinscala/fpinscala
--- scala/basic/fpis_chap2/src/main/scala/Fibonacci.scala
+++ scala/basic/fpis_chap2/src/main/scala/Fibonacci.scala
@@ -12,5 +12,17 @@ object Fibonacci {
     else loop(n, 1, 1, 0, 1)
   }
 
+  // From https://github.com/fpinscala/fpinscala/blob/master/answers/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+  // 0 and 1 are the first two numbers in the sequence,
+  // so we start the accumulators with those.
+  // At every iteration, we add the two numbers to get the next one.
+  def fibFpisSol(n: Int): Int = {
+    @annotation.tailrec
+    def loop(n: Int, prev: Int, cur: Int): Int =
+      if (n == 0) prev
+      else loop(n - 1, cur, prev + cur)
+    loop(n, 0, 1)
+  }
+
 }
 
