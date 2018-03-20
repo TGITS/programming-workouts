0206cce1f6ddc091526edde5e644cf4393091f65
TGITS <thegeekintheshell@gmail.com>
2018-03-20 09:28:49 +0100

    WIP => Développement d'un JDD (épisode de GOT en JSON) pour monter un tutoriel sur hbase Initialisation d'un projet Scala pour faire les exercices du chapitre 3 "Functional Data Structures" de Functional Programming In Scala

---
+++ scala/basic/fpis_chp3_functional_data_structures/build.sbt
@@ -0,0 +1,5 @@
+name := "fpis_chp3_functional_data_structures"
+
+version := "0.1"
+
+scalaVersion := "2.12.5"
---
+++ scala/basic/fpis_chp3_functional_data_structures/src/main/scala/fpis/datastructures/Main.scala
@@ -0,0 +1,33 @@
+package fpis.datastructures
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val ex1: List[Double] = Nil
+    val ex2: List[Int] = Cons(1, Nil)
+    val ex3: List[String] = Cons("a", Cons("b", Nil))
+    val ex4: List[String] = Cons("a", Cons("b", Cons("c", Cons("d",Nil))))
+
+    println(ex1)
+    println(ex2)
+    println(ex3)
+    println(ex4)
+
+    //Exercise 3.1 - answer : 3 if sum is defined for any type, otherwise compiled error
+    val x = List(1, 2, 3, 4, 5) match {
+      case Cons(x, Cons(2, Cons(4, _))) => x
+      case Nil => 42
+      case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+      //case Cons(h,t) => h + sum(t)
+      case _ => 101
+    }
+    println(x)
+    println(List.tail(ex1))
+    println(List.tail(ex2))
+    println(List.tail(ex3))
+    println(List.tail(ex4))
+
+
+
+  }
+
+}
