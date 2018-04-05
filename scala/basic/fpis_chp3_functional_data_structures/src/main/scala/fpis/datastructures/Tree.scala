6820bcf32b6649596ef35440ce878c3d84d9a033
TGITS <thegeekintheshell@gmail.com>
2018-04-05 16:30:50 +0200

    Travail sur les exercices sur la structure de données Tree dans le chapitre 3 de FPIS

--- scala/basic/fpis_chp3_functional_data_structures/src/main/scala/fpis/datastructures/Tree.scala
+++ scala/basic/fpis_chp3_functional_data_structures/src/main/scala/fpis/datastructures/Tree.scala
@@ -22,23 +22,43 @@ object Tree {
    * Write a function maximum that returns the maximum element in a Tree[Int]. (Note:
    * In Scala, you can use x.max(y) or x max y to compute the maximum of two integers x and y.)
    * */
-  def maximum(t:Tree[Int]): Int = t match {
+  def maximum(t: Tree[Int]): Int = t match {
     case Leaf(v) => v
-    case Branch(l,r) => maximum(l) max maximum(r)
+    case Branch(l, r) => maximum(l) max maximum(r)
   }
 
   /* Exercise 3.27
    * Write a function depth that returns the maximum path length from the root of a tree to any leaf.
    * */
+  def depth[A](t: Tree[A]): Int = t match {
+    case Leaf(_) => 1
+    case Branch(l, r) => 1 + (depth(l) max depth(r))
+  }
 
   /* Exercise 3.28
    * Write a function map, analogous to the method of the same name on List,
    * that modifies each element in a tree with a given function.
    * */
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
+    case Leaf(a) => Leaf(f(a))
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  }
 
   /* Exercise 3.29
    * Generalize size, maximum, depth, and map, writing a new function fold that abstracts
    * over their similarities. Re-implement them in terms of this more general function. Can
    * you draw an analogy between this fold function and the left and right folds for List?
    * */
+  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = t match {
+    case Leaf(a) => f(a)
+    case Branch(l, r) => g((fold(l)(f)(g)), (fold(r)(f)(g)))
+  }
+
+  def size_2[A](t: Tree[A]): Int = fold[A, Int](t)(_ => 1)((b1, b2) => (1 + b1 + b2))
+
+  def maximum_2(t: Tree[Int]): Int = fold[Int, Int](t)(a => a)((b1, b2) => b1 max b2)
+
+  def depth_2[A](t: Tree[A]): Int = fold[A, Int](t)(_ => 1)((b1, b2) => 1 + (b1 max b2))
+
+  def map_2[A, B](t: Tree[A])(f: A => B): Tree[B] = fold[A, Tree[B]](t)(a => Leaf(f(a)))((b1, b2) => Branch(b1, b2))
 }
--- scala/basic/fpis_chp3_functional_data_structures/src/test/scala/fpis/datasctructures/TreeSpec.scala
+++ scala/basic/fpis_chp3_functional_data_structures/src/test/scala/fpis/datasctructures/TreeSpec.scala
@@ -36,4 +36,106 @@ class TreeSpec extends FlatSpec with Matchers {
     Tree.maximum(br1) should be(2)
     Tree.maximum(br2) should be(4)
   }
+
+  "Applying depth on a Tree" should "return the longest path of nodes" in {
+    val lf1 = Leaf(1)
+    val lf2 = Leaf(2)
+    val lf3 = Leaf(3)
+    val lf4 = Leaf(4)
+    val br1 = Branch(lf1, lf2)
+    val br2 = Branch(lf3, lf4)
+    val root = Branch(br1, br2)
+    Tree.depth(root) should be(3)
+    Tree.depth(lf1) should be(1)
+    Tree.depth(br1) should be(2)
+    Tree.depth(br2) should be(2)
+    val lf5 = Leaf(5)
+    val root2 = Branch(root, lf5)
+    Tree.depth(root2) should be(4)
+  }
+
+  "Applying map on a Tree of Int with a function that convert to String" should "return a Tree of Strings" in {
+    val ilf1 = Leaf(1)
+    val ilf2 = Leaf(2)
+    val ilf3 = Leaf(3)
+    val ilf4 = Leaf(4)
+    val ibr1 = Branch(ilf1, ilf2)
+    val ibr2 = Branch(ilf3, ilf4)
+    val iroot = Branch(ibr1, ibr2)
+
+    val slf1 = Leaf("1")
+    val slf2 = Leaf("2")
+    val slf3 = Leaf("3")
+    val slf4 = Leaf("4")
+    val sbr1 = Branch(slf1, slf2)
+    val sbr2 = Branch(slf3, slf4)
+    val sroot = Branch(sbr1, sbr2)
+
+    Tree.map(iroot)(i => i.toString) should be(sroot)
+  }
+
+  "Applying size_2 on a Tree with 7 nodes" should "return 7" in {
+    val lf1 = Leaf(1)
+    val lf2 = Leaf(2)
+    val lf3 = Leaf(3)
+    val lf4 = Leaf(4)
+    val br1 = Branch(lf1, lf2)
+    val br2 = Branch(lf3, lf4)
+    val root = Branch(br1, br2)
+    Tree.size_2(lf1) should be(1)
+    Tree.size_2(br1) should be(3)
+    Tree.size_2(root) should be(7)
+  }
+
+  "Applying maximum_2 on a Tree of Integer" should "return the biggest integer in the tree" in {
+    val lf1 = Leaf(1)
+    val lf2 = Leaf(2)
+    val lf3 = Leaf(3)
+    val lf4 = Leaf(4)
+    val br1 = Branch(lf1, lf2)
+    val br2 = Branch(lf3, lf4)
+    val root = Branch(br1, br2)
+    Tree.maximum_2(root) should be(4)
+    Tree.maximum_2(lf1) should be(1)
+    Tree.maximum_2(br1) should be(2)
+    Tree.maximum_2(br2) should be(4)
+  }
+
+  "Applying depth_2 on a Tree" should "return the longest path of nodes" in {
+    val lf1 = Leaf(1)
+    val lf2 = Leaf(2)
+    val lf3 = Leaf(3)
+    val lf4 = Leaf(4)
+    val br1 = Branch(lf1, lf2)
+    val br2 = Branch(lf3, lf4)
+    val root = Branch(br1, br2)
+    Tree.depth_2(root) should be(3)
+    Tree.depth_2(lf1) should be(1)
+    Tree.depth_2(br1) should be(2)
+    Tree.depth_2(br2) should be(2)
+    val lf5 = Leaf(5)
+    val root2 = Branch(root, lf5)
+    Tree.depth_2(root2) should be(4)
+  }
+
+  "Applying map_2 on a Tree of Int with a function that convert to String" should "return a Tree of Strings" in {
+    val ilf1 = Leaf(1)
+    val ilf2 = Leaf(2)
+    val ilf3 = Leaf(3)
+    val ilf4 = Leaf(4)
+    val ibr1 = Branch(ilf1, ilf2)
+    val ibr2 = Branch(ilf3, ilf4)
+    val iroot = Branch(ibr1, ibr2)
+
+    val slf1 = Leaf("1")
+    val slf2 = Leaf("2")
+    val slf3 = Leaf("3")
+    val slf4 = Leaf("4")
+    val sbr1 = Branch(slf1, slf2)
+    val sbr2 = Branch(slf3, slf4)
+    val sroot = Branch(sbr1, sbr2)
+
+    Tree.map_2(iroot)(i => i.toString) should be(sroot)
+    Tree.map_2(iroot)(i => i.toString) should be(Tree.map(iroot)(i => i.toString))
+  }
 }
