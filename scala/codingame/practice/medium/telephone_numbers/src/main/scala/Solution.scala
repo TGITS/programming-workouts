ffcabf623066b83993f73874c5b7d646f9726196
TGITS <thegeekintheshell@gmail.com>
2018-04-13 12:32:16 +0200

    Solution en Scala au puzzle medium de CodinGame "Telephone Numbers"

--- scala/codingame/practice/medium/telephone_numbers/src/main/scala/Solution.scala
+++ scala/codingame/practice/medium/telephone_numbers/src/main/scala/Solution.scala
@@ -1,5 +1,4 @@
-import math._
-import scala.util._
+import scala.collection.mutable.ListBuffer
 import scala.io.StdIn
 
 /**
@@ -8,14 +7,45 @@ import scala.io.StdIn
   **/
 object Solution extends App {
   val n = StdIn.readInt()
-  for(i <- 0 until n) {
-    val telephone = StdIn.readLine()
+  val telephoneNumbers = new ListBuffer[List[Char]]()
+  for (i <- 0 until n) {
+    telephoneNumbers += StdIn.readLine().toList
   }
 
+  val root = Node[Char](' ', new ListBuffer[Node[Char]])
+  for (number <- telephoneNumbers) {
+    root.translateInto(number)
+  }
+
+  val answer = root.count
   // Write an action using println
   // To debug: Console.err.println("Debug messages...")
 
 
   // The number of elements (referencing a number) stored in the structure.
-  println("number")
+  println((answer - 1).toString)
+}
+
+case class Node[A](content: A, children: ListBuffer[Node[A]]) {
+  def translateInto(list: List[A]): Node[A] = list match {
+    case Nil => this
+    case _ => {
+      children.find(n => n.content == list.head) match {
+        case None => {
+          val newNode = Node[A](list.head, new ListBuffer[Node[A]])
+          children += newNode
+          newNode.translateInto(list.tail)
+        }
+        case Some(node) => node.translateInto(list.tail)
+      }
+    }
+  }
+
+  def findLongestBranch[A]: Int = {
+    if (this.children.isEmpty) 1 else 1 + (children map (node => node.findLongestBranch)).max
+  }
+
+  def count[A]: Int = {
+    if (this.children.isEmpty) 1 else 1 + (children map (node => node.count)).sum
+  }
 }
--- scala/codingame/practice/medium/telephone_numbers/src/test/scala/SolutionSpec.scala
+++ scala/codingame/practice/medium/telephone_numbers/src/test/scala/SolutionSpec.scala
@@ -1,6 +1,48 @@
-import Solution._
 import org.scalatest.{FlatSpec, Matchers}
 
-class SolutionSpec extends FlatSpec with Matchers {
+import scala.collection.mutable.ListBuffer
 
+class SolutionSpec extends FlatSpec with Matchers {
+  "Applying findLongestBranch on a Node on which we had added 0467123456" should "result in the value 11" in {
+    val root = Node[Char](' ', new ListBuffer[Node[Char]])
+    root.translateInto("0467123456".toList)
+    root.findLongestBranch should be(11)
+  }
+
+  "Applying findLongestBranch on a Node on which we had added 0123456789 and 1123456789" should "result in the value 11" in {
+    val root = Node[Char](' ', new ListBuffer[Node[Char]])
+    root.translateInto("0123456789".toList)
+    root.translateInto("1123456789".toList)
+    root.findLongestBranch should be(11)
+  }
+
+  "Applying count on a Node on which we had added 0467123456" should "result in the value 11" in {
+    val root = Node[Char](' ', new ListBuffer[Node[Char]])
+    root.translateInto("0467123456".toList)
+    root.count should be(11)
+  }
+
+  "Applying count on a Node on which we had added 0123456789 and 1123456789" should "result in the value 21" in {
+    val root = Node[Char](' ', new ListBuffer[Node[Char]])
+    root.translateInto("0123456789".toList)
+    root.translateInto("1123456789".toList)
+    root.count should be(21)
+  }
+
+  "Applying count on a Node on which we had added 0123456789 and 0123" should "result in the value 11" in {
+    val root = Node[Char](' ', new ListBuffer[Node[Char]])
+    root.translateInto("0123456789".toList)
+    root.translateInto("0123".toList)
+    root.count should be(11)
+  }
+
+  "Applying count on a Node on which we had added 0412578440, 0412199803, 0468892011, 112 and 15" should "result in the value 29" in {
+    val root = Node[Char](' ', new ListBuffer[Node[Char]])
+    root.translateInto("0412578440".toList)
+    root.translateInto("0412199803".toList)
+    root.translateInto("0468892011".toList)
+    root.translateInto("112".toList)
+    root.translateInto("15".toList)
+    root.count should be(29)
+  }
 }
