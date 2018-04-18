e1c7f85be21ce562f81a92f15df1066777328b2f
TGITS <thegeekintheshell@gmail.com>
2018-04-18 11:48:58 +0200

    WIP - Travail sur une solution en Scala pour résoudre le puzzle Medium "Teads Contest"

--- scala/codingame/practice/medium/teads_contest/src/main/scala/Solution.scala
+++ scala/codingame/practice/medium/teads_contest/src/main/scala/Solution.scala
@@ -1,4 +1,5 @@
-import scala.collection.mutable.{Set,Map}
+import scala.collection.mutable
+import scala.collection.mutable.{ArrayBuffer, HashMap, Map, Set}
 import scala.io.StdIn
 
 /**
@@ -7,7 +8,7 @@ import scala.io.StdIn
   **/
 object Solution extends App {
   val n = StdIn.readInt // the number of adjacency relations
-  val graph = Graph()
+  val graph = Graph(n)
   for (i <- 0 until n) {
     // xi: the ID of a person which is adjacent to yi
     // yi: the ID of a person which is adjacent to xi
@@ -24,33 +25,39 @@ object Solution extends App {
   println(graph.computeMinimalTime().toString)
 }
 
-case class Graph() {
+case class Graph(adjacencyRelations:Int) {
 
   private val nodes = Set[Int]()
-  private val nodesById:Map[Int,Set[Int]] = Map[Int,Set[Int]]()
+  private val nodesById:mutable.LongMap[Set[Int]] = new mutable.LongMap[Set[Int]](adjacencyRelations+1)
   def addEdges(source:Int,destination:Int):Unit = {
     def addOrUpdateNode(source:Int,destination:Int):Unit = {
       if(!nodesById.contains(source)) {
-        nodesById.put(source,Set[Int](destination))
+        nodesById += (source.toLong -> Set[Int](destination))
       }
       else {
-        nodesById.get(source).get += destination
+        nodesById.getOrNull(source) += destination
       }
     }
 
     addOrUpdateNode(source,destination)
     addOrUpdateNode(destination, source)
     nodes += source += destination
+    //addNodes(source)
+    //addNodes(destination)
   }
 
+//  def addNodes(n:Int):Unit = {
+//    if(!nodes.contains(n)) nodes += n
+//  }
+
   def computeAdjacencyReach(n:Int): Int = {
-    var setOfAllNodes = Set[Int](n)
+    val setOfAllNodes = Set[Int](n)
     var number = 0
-    var currentSet = nodesById.get(n).getOrElse(Set[Int]())
+    var currentSet = nodesById.get(n).get
     while (!currentSet.isEmpty) {
       setOfAllNodes ++= currentSet
       number += 1
-      currentSet = currentSet.flatMap(n => this.nodesById.get(n).getOrElse(Set[Int]())) -- setOfAllNodes
+      currentSet = currentSet.flatMap(n => this.nodesById.get(n).get) -- setOfAllNodes
     }
     number
   }
