b40746aabe338d6a4aaa770f8b967d774054b23f
TGITS <thegeekintheshell@gmail.com>
2018-04-18 10:22:20 +0200

    WIP - Travail sur une solution en Scala pour résoudre le puzzle Medium "Teads Contest"

--- scala/codingame/practice/medium/teads_contest/src/main/scala/Solution.scala
+++ scala/codingame/practice/medium/teads_contest/src/main/scala/Solution.scala
@@ -7,19 +7,13 @@ import scala.io.StdIn
   **/
 object Solution extends App {
   val n = StdIn.readInt // the number of adjacency relations
-  val edges = Set[Edge]()
-  val nodes = Set[Int]()
+  val graph = Graph()
   for (i <- 0 until n) {
     // xi: the ID of a person which is adjacent to yi
     // yi: the ID of a person which is adjacent to xi
     val Array(id1, id2) = for (id <- StdIn.readLine split " ") yield id.toInt
-    edges += Edge(id1, id2) += Edge(id2, id1)
-    nodes += id1 += id2
+    graph.addEdges(id1,id2)
   }
-
-  //Console.err.println(edges)
-
-  val graph = Graph(Node.constructNodesList(nodes, edges), edges)
 
   //Console.err.println(graph)
 
@@ -27,43 +21,41 @@ object Solution extends App {
   // To debug: Console.err.println("Debug messages...")
 
   // The minimal amount of steps required to completely propagate the advertisement
-  println(Graph.computeMinimalTime(graph).toString)
+  println(graph.computeMinimalTime().toString)
 }
 
-case class Edge(source: Int, destination: Int)
+case class Graph() {
 
-case class Node(content: Int, adjacency: Set[Int])
-
-case class Graph(nodes: Set[Node], edges: Set[Edge]) {
-  val cache:Map[Int,Node] = Map[Int,Node]()
-  def getNodeFromId(id: Int): Node = {
-    if(!cache.contains(id)) {
-      cache.put(id,this.nodes.filter(_.content == id).head)
+  private val nodes = Set[Int]()
+  private val nodesById:Map[Int,Set[Int]] = Map[Int,Set[Int]]()
+  def addEdges(source:Int,destination:Int):Unit = {
+    def addOrUpdateNode(source:Int,destination:Int):Unit = {
+      if(!nodesById.contains(source)) {
+        nodesById.put(source,Set[Int](destination))
+      }
+      else {
+        nodesById.get(source).get += destination
+      }
     }
-    cache(id)
-  }
-}
 
-object Node {
-  def constructNodesList(nodes: Set[Int], edges: Set[Edge]): Set[Node] = {
-    nodes.map(n => Node(n, edges.filter(e => e.source == n).map(e => e.destination)))
+    addOrUpdateNode(source,destination)
+    addOrUpdateNode(destination, source)
+    nodes += source += destination
   }
-}
 
-object Graph {
-  def computeAdjacencyReach(graph: Graph, node: Node): Int = {
-    var setsOfAllNode = Set[Node](node)
+  def computeAdjacencyReach(n:Int): Int = {
+    var setOfAllNodes = Set[Int](n)
     var number = 0
-    var currentSet = node.adjacency.map(graph.getNodeFromId(_))
+    var currentSet = nodesById.get(n).getOrElse(Set[Int]())
     while (!currentSet.isEmpty) {
-      setsOfAllNode ++= currentSet
+      setOfAllNodes ++= currentSet
       number += 1
-      currentSet = currentSet.flatMap(n => n.adjacency.map(graph.getNodeFromId(_))) -- setsOfAllNode
+      currentSet = currentSet.flatMap(n => this.nodesById.get(n).getOrElse(Set[Int]())) -- setOfAllNodes
     }
     number
   }
 
-  def computeMinimalTime(graph: Graph): Int = {
-    graph.nodes.map(computeAdjacencyReach(graph, _)).min
+  def computeMinimalTime(): Int = {
+    this.nodes.map(computeAdjacencyReach(_)).min
   }
 }
