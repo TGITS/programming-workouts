6df687539151bdd2c5421151dc092b50f7196511
TGITS <thegeekintheshell@gmail.com>
2018-05-12 19:24:23 +0200

    Une solution en Scala au puzzle Medium "Network Cabling" de CodinGame

--- scala/codingame/practice/medium/network_cabling/src/main/scala/Solution.scala
+++ scala/codingame/practice/medium/network_cabling/src/main/scala/Solution.scala
@@ -1,7 +1,5 @@
-import math._
 import scala.collection.mutable.ListBuffer
 import scala.io.StdIn
-import scala.util._
 
 /**
   * Auto-generated code below aims at helping you parse
@@ -9,36 +7,43 @@ import scala.util._
   **/
 object Solution extends App {
   val n = StdIn.readInt
-  val houses:ListBuffer[Coordinate] = ListBuffer()
-  for(i <- 0 until n) {
-    val Array(x, y) = for(i <- StdIn.readLine split " ") yield i.toInt
-    houses += Coordinate(x,y)
+  val houses: ListBuffer[Coordinate] = ListBuffer()
+  for (i <- 0 until n) {
+    val Array(x, y) = for (i <- StdIn.readLine split " ") yield i.toLong
+    houses += Coordinate(x, y)
   }
 
   Console.err.println("List of the coordinates of the houses : ")
   Console.err.println(houses)
 
-  val minX = houses.minBy(c => Math.abs(c.x)).x
-  val maxX = houses.maxBy(c => Math.abs(c.x)).x
-  val minY = houses.minBy(c => Math.abs(c.y)).y
-  val maxY = houses.maxBy(c => Math.abs(c.y)).y
+  val minX = houses.minBy(_.x).x
+  val maxX = houses.maxBy(_.x).x
   Console.err.println("minX : " + minX)
   Console.err.println("maxX : " + maxX)
-  Console.err.println("minY : " + minY)
-  Console.err.println("maxY : " + maxY)
 
   // Write an action using println
   // To debug: Console.err.println("Debug messages...")
-  val mainCableLength = maxX - minX
-  val answer = mainCableLength
+  val mainCableLength:Long = Math.abs(maxX - minX)
+  val yList:ListBuffer[Long] = houses.map(c => c.y).sorted
+  val numberOfHouses = yList.size
+  val medianY:Long = if (numberOfHouses % 2 == 0) {
+    Math.ceil((yList(numberOfHouses/2 - 1) + yList(numberOfHouses/2)) / 2).toLong
+  } else {
+    yList((numberOfHouses - 1)/2)
+  }
+
+  val listOfDistances:ListBuffer[Long] = houses.map(c => Math.abs(c.y - medianY))
+  val sumHousesFromMainCable:Long = listOfDistances.sum
+  val answer:Long = mainCableLength + sumHousesFromMainCable
+
+  Console.err.println("yList : " + yList)
+  Console.err.println("numberOfHouses : " + numberOfHouses)
+  Console.err.println("medianY : " + medianY)
+  Console.err.println("listOfDistances : " + listOfDistances)
+  Console.err.println("mainCableLength : " + mainCableLength)
+  Console.err.println("sumHousesFromMainCable : " + sumHousesFromMainCable)
 
   println(answer)
 }
 
-case class Coordinate(x:Int,y:Int)
-
-object Tool {
-  def taxicabDistance(c1:Coordinate,c2:Coordinate):Int = {
-    Math.abs(c2.x - c1.x) + Math.abs(c2.y - c1.y)
-  }
-}
+case class Coordinate(x: Long, y: Long)
