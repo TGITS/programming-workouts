c71d7f14f7996b986406dff0dd8dce13a4b8d76d
TGITS <thegeekintheshell@gmail.com>
2018-07-09 10:53:11 +0200

    Solution en Scala au puzzle CG "Horse Racing Hyperdual"

--- scala/codingame/practice/easy/horse-racing-hyperduals/src/main/scala/Solution.scala
+++ scala/codingame/practice/easy/horse-racing-hyperduals/src/main/scala/Solution.scala
@@ -1,37 +1,38 @@
-import math._
-import scala.util._
+import scala.collection.mutable._
 
 /**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
+  * Auto-generated code below aims at helping you parse
+  * the standard input according to the problem statement.
+  **/
 object Solution extends App {
-    val n = scala.io.StdIn.readInt
-    //for(i <- 0 until n) {
-    //    val pi = readInt
-    //}
-    var vector: Vector[HorseStrength] = Vector()
-    for(i <- 0 until n) {
-        val Array(v, e) = for(i <- scala.io.StdIn.readLine split " ") yield i.toInt
-        vector = vector :+ HorseStrength(v,e)
+  val n = scala.io.StdIn.readInt
+  var strengthes: Vector[HorseStrength] = Vector()
+  val mutableStrengthesBuffer: ListBuffer[HorseStrength] = ListBuffer()
+  for (i <- 0 until n) {
+    val Array(v, e) = for (i <- scala.io.StdIn.readLine split " ") yield i.toInt
+    val hs = HorseStrength(v, e)
+    strengthes = strengthes :+ hs
+    mutableStrengthesBuffer += hs
+  }
+
+  //Console.err.println(differences.mkString(" "))
+
+  var minimalDistances: ListBuffer[Int] = ListBuffer()
+  for (hs <- strengthes) {
+    mutableStrengthesBuffer -= hs
+    if (mutableStrengthesBuffer.nonEmpty) {
+      val minimalDistance = mutableStrengthesBuffer.map(x => x.distance(hs)).min
+      minimalDistances += minimalDistance
     }
-    Console.err.println(vector.mkString(" "))
-    val origin = HorseStrength(0,0)
-    val sortedHorseStrengthes = vector.sortWith( (s1,s2) => s1.distance(origin) < s2.distance(origin))
-    Console.err.println(sortedHorseStrengthes.mkString(" "))
-    // Write an action using println
-    // To debug: Console.err.println("Debug messages...")
-    val differences = (sortedHorseStrengthes.tail zip sortedHorseStrengthes.dropRight(1)).map (x => x._1.distance(x._2))
-    
-    Console.err.println(differences.mkString(" "))
-    println(differences.sorted.head.toString)
+  }
+  println(minimalDistances.min.toString)
 }
 
 case class HorseStrength(var velocity: Int, var elegance: Int) {
 
-    def distance(otherStrength: HorseStrength): Int = {
-        //distance between two strength  
-        //The distance between two strengths (V1,E1) and (V2,E2) is abs(V2-V1)+abs(E2-E1).
-        Math.abs(otherStrength.velocity - this.velocity) + Math.abs(otherStrength.elegance - this.elegance)
-    }
+  def distance(otherStrength: HorseStrength): Int = {
+    //distance between two strength
+    //The distance between two strengths (V1,E1) and (V2,E2) is abs(V2-V1)+abs(E2-E1).
+    Math.abs(otherStrength.velocity - this.velocity) + Math.abs(otherStrength.elegance - this.elegance)
+  }
 }
