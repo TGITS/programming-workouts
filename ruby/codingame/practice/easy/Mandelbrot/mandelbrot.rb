9e33c2f2014b94dae48563660001d6157f70ece9
TGITS <thegeekintheshell@gmail.com>
2018-07-30 18:41:24 +0200

    Solution en Ruby du puzzle "What's so complex about Mandelbrot ?"

--- ruby/codingame/practice/easy/Mandelbrot/mandelbrot.rb
+++ ruby/codingame/practice/easy/Mandelbrot/mandelbrot.rb
@@ -1,5 +1,18 @@
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
 
-@c = gets.chomp
-@m = gets.to_i
+c = gets.chomp
+m = gets.to_i
+
+STDERR.puts c
+STDERR.puts m.to_s
+
+c = c.to_c
+i = 0
+f = Complex(0,0)
+while i < m && f.abs < 2 do
+    f = f ** 2 + c 
+    i += 1
+end
+
+puts i.to_s
