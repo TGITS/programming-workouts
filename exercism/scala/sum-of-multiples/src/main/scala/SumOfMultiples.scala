c5d50e482dbaa2c5fa776af6f31bc55ebd542e3e
z23cvaud <christophe.vaudry.partner@decathlon.com>
2018-11-25 18:00:34 +0100

    WIP - Travail sur une solution au problème de la sommes des multiples de exercism

--- exercism/scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ exercism/scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,4 +1,6 @@
 object SumOfMultiples {
-  def sum(factors: Set[Int], limit: Int): Int = ???
+  def sum(factors: Set[Int], limit: Int): Int = {
+    (1 to limit).toList.flatmap( i => factors.map(f => if (i % f == 0) f else 0).sum
+  }
 }
 
