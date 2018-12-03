357d5f1d5ef6c05f71eaa9a77b7793d6ed110697
z23cvaud <christophe.vaudry.partner@decathlon.com>
2018-12-03 16:24:42 +0100

    WIP - Initialization of the work of the exercise of exercism for scala 'robot name'

---
+++ exercism/scala/robot-name/src/main/scala/Robot.scala
@@ -0,0 +1,7 @@
+class Robot() {
+  val name: String = createName(2,3)
+  def reset(): Unit = ???
+  def createName(numberOfLetter: Int, numberOfDigit: Int):String = {
+    return "AB" + "123"
+  }
+}
