848f5e60a42bd0c3d4e14a9ac752c11eb0ef86a2
z23cvaud <christophe.vaudry.partner@decathlon.com>
2018-12-03 16:23:39 +0100

    A proposed solution to the exercise SumOfMultiples of exercism for Scala

--- exercism/scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ exercism/scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,6 +1,6 @@
 object SumOfMultiples {
   def sum(factors: Set[Int], limit: Int): Int = {
-    (1 to limit).toList.flatmap( i => factors.map(f => if (i % f == 0) f else 0).sum
+    (1 to (limit-1)).toList.flatMap {i => factors.map{f => if (i % f == 0) i else 0}}.sum
   }
 }
 
