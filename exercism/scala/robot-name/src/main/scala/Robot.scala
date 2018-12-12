a6f61d212e61990cf7f1efdc1e889492f6ad0086
z23cvaud <christophe.vaudry.partner@decathlon.com>
2018-12-12 16:04:56 +0100

    [exercism]Exercice non finalisé sur la track Scala (RobotName) : la solution n'est pas scalable [exercism]Initialisation de la track Clojure avec l'exercice Hello World [exercism]Récupération de l'exercice sur les nombres de Armstrong

--- exercism/scala/robot-name/src/main/scala/Robot.scala
+++ exercism/scala/robot-name/src/main/scala/Robot.scala
@@ -1,7 +1,45 @@
+import scala.collection.mutable
+import scala.util.Random
+
 class Robot() {
-  val name: String = createName(2,3)
-  def reset(): Unit = ???
-  def createName(numberOfLetter: Int, numberOfDigit: Int):String = {
-    return "AB" + "123"
+  var name = Robot.createUniqueName(2,3)
+  def reset(): Unit = {
+    //Robot.deleteNameFromSet(name)
+    name = Robot.createUniqueName(2,3)
   }
+}
+
+object Robot {
+  val random = new Random()
+  val distinctNames = mutable.HashSet.empty[String]
+
+  def createUniqueName(numberOfLetters: Int, numberOfDigit: Int):String = {
+    var uniqueName = createRandomName(numberOfLetters,numberOfDigit)
+    while(distinctNames contains uniqueName) {
+      uniqueName = createRandomName(numberOfLetters,numberOfDigit)
+    }
+    distinctNames += uniqueName
+    uniqueName
+  }
+
+  def deleteNameFromSet(name:String):Unit = {
+    distinctNames -= name
+  }
+
+  def createRandomName(numberOfLetter: Int, numberOfDigit: Int):String = {
+    randomString(2) + randomDigits(3)
+  }
+
+  def randomDigit(): String = {
+    random.nextInt(10).toString
+  }
+
+  def randomDigits(times:Int): String = {
+    return List.fill(times){randomDigit()}.mkString
+  }
+
+  def randomString(length:Int):String = {
+    random.shuffle(('A' to 'Z').toList).take(length).mkString
+  }
+
 }
