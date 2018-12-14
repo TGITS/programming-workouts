d80ca233667bb1fccd0ab3919164648dfd483c1c
z23cvaud <christophe.vaudry.partner@decathlon.com>
2018-12-14 08:22:04 +0100

    MAJ sur les exercices Scala pour exercism somme de multiples et RobotName

--- exercism/scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ exercism/scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,6 +1,6 @@
 object SumOfMultiples {
   def sum(factors: Set[Int], limit: Int): Int = {
-    (1 to (limit-1)).toList.flatMap {i => factors.map{f => if (i % f == 0) i else 0}}.sum
+    (1 until limit).toList.map { i => if (factors exists { f => (i % f == 0)} ) i else 0 }.sum
   }
 }
 
