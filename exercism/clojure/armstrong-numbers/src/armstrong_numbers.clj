bb83280ab0e34f1bda2f8e0fddab6edf9848068a
z23cvaud <christophe.vaudry.partner@decathlon.com>
2018-12-18 20:57:37 +0100

    WIP : Trying to solbe Armstrong Number in Clojure for erxercism

--- exercism/clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ exercism/clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -1,5 +1,14 @@
-(ns armstrong-numbers)
+(ns armstrong-numbers
+  (:require [clojure.math.numeric-tower :as math]))
+
+(defn count-number-of-digits [num]
+  (count (str num)))
+
+(defn number-to-digits-list [num] 
+  (map #(Integer/parseInt %) (map str (seq (str num)))))
+
+(defn exponentiate-all-digits [exponent num]
+  (reduce + (map (partial math/expt exponent) (number-to-digits-list num))))
 
 (defn armstrong? [num] ;; <- arglist goes here
-  ;; your code goes here
-  )
+  (= num (exponentiate-all-digits (count-number-of-digits num) num)))
