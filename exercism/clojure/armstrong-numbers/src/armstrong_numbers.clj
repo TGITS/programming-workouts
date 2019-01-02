7981b08d7aae77ae3b3e75305a8749a6259a943e
z23cvaud <christophe.vaudry.partner@decathlon.com>
2019-01-02 11:08:55 +0100

    Integrating the suggestion of the mentor for the Armstrong Numbers on exercism.io

--- exercism/clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ exercism/clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -5,13 +5,10 @@
   (count (str num)))
 
 (defn number-to-digits-list [num] 
-  (map #(Integer/parseInt %) (map str (seq (str num)))))
-
-(defn my-expt [exponent num]
-  (math/expt num exponent))
+  (map #(Integer/parseInt (str %)) (seq (str num))))
 
 (defn exponentiate-all-digits [exponent num]
-  (reduce + (map (partial my-expt exponent) (number-to-digits-list num))))
+  (apply + (map #(math/expt % exponent) (number-to-digits-list num))))
 
 (defn armstrong? [num] ;; <- arglist goes here
   (= num (exponentiate-all-digits (count-number-of-digits num) num)))
