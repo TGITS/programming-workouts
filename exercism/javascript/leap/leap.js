ee591a74568de93882cbe83b119b0c7998ace057
VAUDRY Christophe <christophe.vaudry@ext.imprimerienationale.fr>
2019-03-25 14:43:58 +0100

    Solution en Javascript à l'exercice leap en Javascript (suite retours mentor)

--- exercism/javascript/leap/leap.js
+++ exercism/javascript/leap/leap.js
@@ -1,4 +1,3 @@
 export const isLeap = (year) => {
-    return (year % 4 == 0 && year % 400 == 0) 
-    || (year % 4 == 0 && year % 100 != 0);
+    return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
 };
