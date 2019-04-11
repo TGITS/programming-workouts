bbb4e9f161b33e3bacd1e70181687b89b1cfbcb0
VAUDRY Christophe <christophe.vaudry@ext.imprimerienationale.fr>
2019-04-11 19:06:01 +0200

    Ajout d'éléments en préparation de Workouts

---
+++ java/misc/fakerexperimentation/src/main/java/tgits/workout/App.java
@@ -0,0 +1,13 @@
+package tgits.workout;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
