64ad1fe5076bb3caa9bf15b900255efb0c696a51
cvaudry <cvaudry@norsys.fr>
2019-04-19 15:01:17 +0200

    Une solution en Java à l'exercice "Luhn" de exercism

--- exercism/java/luhn/src/main/java/LuhnValidator.java
+++ exercism/java/luhn/src/main/java/LuhnValidator.java
@@ -1,7 +1,26 @@
 class LuhnValidator {
 
     boolean isValid(String candidate) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return candidate != null && candidate.trim().length() > 1 && candidate.matches("(\\d|\\s)+") && processStringOfDigits(candidate.trim()) % 10 == 0;
     }
 
+    private int processStringOfDigits(String candidate) {
+        int result = 0;
+        boolean toggle = false;
+        for (int i = candidate.length() - 1; i >= 0; i--) {
+            String currentString = candidate.substring(i, i + 1);
+            if (!currentString.isBlank()) {
+                int currentNumber = Integer.parseInt(currentString);
+                if (toggle) {
+                    currentNumber = currentNumber * 2;
+                    if (currentNumber > 9) {
+                        currentNumber -= 9;
+                    }
+                }
+                result += currentNumber;
+                toggle = !toggle;
+            }
+        }
+        return result;
+    }
 }
