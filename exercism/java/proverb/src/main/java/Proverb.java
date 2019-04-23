833b96ea4641ccf2a0fdd065cdfeec1180f0300a
VAUDRY Christophe <christophe.vaudry@ext.imprimerienationale.fr>
2019-04-23 13:38:23 +0200

    Une solution à l'exercice "Proverb" de la track Java de exercism

--- exercism/java/proverb/src/main/java/Proverb.java
+++ exercism/java/proverb/src/main/java/Proverb.java
@@ -1,11 +1,28 @@
 class Proverb {
 
-    Proverb(String[] words) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    private static final String AND_ALL_FOR_THE_WANT_OF = "And all for the want of a %s.";
+    private static final String FOR_THE_WANT_OF = "For want of a %s the %s was lost.\n";
+
+
+    private String[] words;
+
+    public Proverb(String[] words) {
+        this.words = words;
     }
 
-    String recite() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    public String recite() {
+        if (words == null || words.length == 0) {
+            return "";
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        if (words.length > 1) {
+            for (int i = 0; i < words.length - 1; i++) {
+                sb.append(String.format(FOR_THE_WANT_OF, words[i], words[i + 1]));
+            }
+        }
+        sb.append(String.format(AND_ALL_FOR_THE_WANT_OF, words[0]));
+        return sb.toString();
     }
 
 }
