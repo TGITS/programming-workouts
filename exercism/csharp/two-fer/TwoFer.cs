02f02dd767908ae2ed7768514ba66705f2495531
VAUDRY Christophe <christophe.vaudry@ext.imprimerienationale.fr>
2019-05-14 16:49:00 +0200

    Nettoyage sur Exercism (suppression des tracks que je ne pratique pas réellement) Nettoyage également du côté des tracks que j'avais oublié (Clojure, Scala)

--- exercism/csharp/two-fer/TwoFer.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-  public static class TwoFer
-  {
-      public static string Name(string input = null)
-      {
-          throw new NotImplementedException("You need to implement this function.");
-      }
-  }
