d29a0197ac40943e8ad28d55284491c0270cf601
VAUDRY Christophe <christophe.vaudry@ext.imprimerienationale.fr>
2019-05-20 18:51:15 +0200

    Une solution a l'exercice ResistorColor de exercism dans la track Java

--- exercism/java/resistor-color/src/main/java/ResistorColor.java
+++ exercism/java/resistor-color/src/main/java/ResistorColor.java
@@ -1,9 +1,42 @@
 class ResistorColor {
-    int colorCode(String color) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+
+    private static final String[] colors = {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
+
+    /* A possible iterative solution can be :
+
+        int colorCode(String color) {
+        int index = 0;
+        boolean notFound = true;
+        while (notFound) {
+            if (colors[index].equalsIgnoreCase(color)) {
+                notFound = false;
+            } else {
+                index++;
+            }
+        }
+        return index;
+    }*/
+
+    public int colorCode(String color) {
+        return findColorCodeIndex(color, 0);
     }
 
-    String[] colors() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    private int findColorCodeIndex(final String color, final int index) {
+        if (index < 0) {
+            throw new IllegalArgumentException("The provided index should be greater or equal to 0");
+        }
+
+        if (index > colors.length) {
+            throw new IllegalArgumentException("The provided color must be correctly spelled or be a legitimate color for a resistor");
+        }
+
+        if (colors[index].equalsIgnoreCase(color)) {
+            return index;
+        }
+        return findColorCodeIndex(color, index + 1);
+    }
+
+    public String[] colors() {
+        return colors;
     }
 }
