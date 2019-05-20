6f863e619fe285f902265383be66bad47bd9cd43
VAUDRY Christophe <christophe.vaudry@ext.imprimerienationale.fr>
2019-05-20 19:00:02 +0200

    Solution purement récursive à l'exercice Resistor Color de exercism

--- exercism/java/resistor-color/src/main/java/ResistorColor.java
+++ exercism/java/resistor-color/src/main/java/ResistorColor.java
@@ -1,22 +1,7 @@
 class ResistorColor {
 
     private static final String[] colors = {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
-
-    /* A possible iterative solution can be :
-
-        int colorCode(String color) {
-        int index = 0;
-        boolean notFound = true;
-        while (notFound) {
-            if (colors[index].equalsIgnoreCase(color)) {
-                notFound = false;
-            } else {
-                index++;
-            }
-        }
-        return index;
-    }*/
-
+    
     public int colorCode(String color) {
         return findColorCodeIndex(color, 0);
     }
