8d0e2e3b9036177483f765658ec4dc05386f89ac
VAUDRY Christophe <christophe.vaudry@ext.imprimerienationale.fr>
2019-05-21 17:39:44 +0200

    Solution à l'exercice IsbnVerifier de la track Java de Exercism

--- exercism/java/isbn-verifier/src/main/java/IsbnVerifier.java
+++ exercism/java/isbn-verifier/src/main/java/IsbnVerifier.java
@@ -1,7 +1,26 @@
 class IsbnVerifier {
 
     boolean isValid(String stringToVerify) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String stringWithoutOutDashes = stringToVerify.trim().replaceAll("-", "");
+
+        if (!stringWithoutOutDashes.matches("(\\d){9}(\\d|X)")) {
+            return false;
+        }
+
+        int length = stringWithoutOutDashes.length();
+        int sum = 0;
+        for (int i = 0; i < length - 1; i++) {
+            sum += Integer.parseInt(Character.toString(stringWithoutOutDashes.charAt(i))) * (10 - i);
+        }
+
+        String lastChar = Character.toString(stringWithoutOutDashes.charAt(length - 1));
+        if (lastChar.equals("X")) {
+            sum += 10;
+        } else {
+            sum += Integer.parseInt(lastChar);
+        }
+
+        return sum % 11 == 0;
     }
 
 }
