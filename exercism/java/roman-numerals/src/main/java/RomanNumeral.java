6a6bacf4ed425d5d85c4297f05f1ee8a4383c947
VAUDRY Christophe <christophe.vaudry@ext.imprimerienationale.fr>
2019-05-27 17:51:44 +0200

    Une 2ème solution à l'exercice RomanNumeral d'exercism dans la track Java

--- exercism/java/roman-numerals/src/main/java/RomanNumeral.java
+++ exercism/java/roman-numerals/src/main/java/RomanNumeral.java
@@ -20,12 +20,12 @@ public class RomanNumeral {
         DIGIT_TO_ROMAN_NUMERAL.put(900, "CM");
         DIGIT_TO_ROMAN_NUMERAL.put(1000, "M");
     }
-    
-    private final int i;
+
+    private final static int[] BASE_VALUES = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+
     private final String romanNumeral;
 
     public RomanNumeral(int i) {
-        this.i = i;
         this.romanNumeral = computeRomanNumeralFromNumber(i);
     }
 
@@ -34,45 +34,10 @@ public class RomanNumeral {
     }
 
     private String computeRomanNumeralFromNumber(int number) {
-
-        if (number >= 1000) {
-            return DIGIT_TO_ROMAN_NUMERAL.get(1000) + computeRomanNumeralFromNumber(number - 1000);
-        }
-        if (number >= 900) {
-            return DIGIT_TO_ROMAN_NUMERAL.get(900) + computeRomanNumeralFromNumber(number - 900);
-        }
-        if (number >= 500) {
-            return DIGIT_TO_ROMAN_NUMERAL.get(500) + computeRomanNumeralFromNumber(number - 500);
-        }
-        if (number >= 400) {
-            return DIGIT_TO_ROMAN_NUMERAL.get(400) + computeRomanNumeralFromNumber(number - 400);
-        }
-        if (number >= 100) {
-            return DIGIT_TO_ROMAN_NUMERAL.get(100) + computeRomanNumeralFromNumber(number - 100);
-        }
-        if (number >= 90) {
-            return DIGIT_TO_ROMAN_NUMERAL.get(90) + computeRomanNumeralFromNumber(number - 90);
-        }
-        if (number >= 50) {
-            return DIGIT_TO_ROMAN_NUMERAL.get(50) + computeRomanNumeralFromNumber(number - 50);
-        }
-        if (number >= 40) {
-            return DIGIT_TO_ROMAN_NUMERAL.get(40) + computeRomanNumeralFromNumber(number - 40);
-        }
-        if (number >= 10) {
-            return DIGIT_TO_ROMAN_NUMERAL.get(10) + computeRomanNumeralFromNumber(number - 10);
-        }
-        if (number >= 9) {
-            return DIGIT_TO_ROMAN_NUMERAL.get(9) + computeRomanNumeralFromNumber(number - 9);
-        }
-        if (number >= 5) {
-            return DIGIT_TO_ROMAN_NUMERAL.get(5) + computeRomanNumeralFromNumber(number - 5);
-        }
-        if (number >= 4) {
-            return DIGIT_TO_ROMAN_NUMERAL.get(4) + computeRomanNumeralFromNumber(number - 4);
-        }
-        if (number >= 1) {
-            return DIGIT_TO_ROMAN_NUMERAL.get(1) + computeRomanNumeralFromNumber(number - 1);
+        for (int value : BASE_VALUES) {
+            if (number >= value) {
+                return DIGIT_TO_ROMAN_NUMERAL.get(value) + computeRomanNumeralFromNumber(number - value);
+            }
         }
         return "";
     }
