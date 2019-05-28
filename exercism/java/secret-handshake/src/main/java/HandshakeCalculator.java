32964bcd3f2464d252c707076e44f2caa6dad8ff
VAUDRY Christophe <christophe.vaudry@ext.imprimerienationale.fr>
2019-05-28 09:00:53 +0200

    1 première solution un peu brute et probablement à refactorer de l'exercice Secret Handshake de exercism dans la track Java.

--- exercism/java/secret-handshake/src/main/java/HandshakeCalculator.java
+++ exercism/java/secret-handshake/src/main/java/HandshakeCalculator.java
@@ -1,9 +1,43 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 class HandshakeCalculator {
 
-    List<Signal> calculateHandshake(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    public List<Signal> calculateHandshake(int number) {
+        return computeHandshake(decimalToBinary(number),new ArrayList<>());
     }
 
+    private int decimalToBinary(int decimal) {
+        return Integer.parseInt(Integer.toBinaryString(decimal));
+    }
+
+    private List<Signal> computeHandshake(int numberInBinary, List<Signal> handshake) {
+        if(numberInBinary >= 10000) {
+            final List<Signal> temp = computeHandshake(numberInBinary - 10000, handshake);
+            Collections.reverse(temp);
+            return temp;
+        }
+        if(numberInBinary >= 1000) {
+            final List<Signal> temp = computeHandshake(numberInBinary - 1000, handshake);
+            temp.add(Signal.JUMP);
+            return temp;
+        }
+        if(numberInBinary >= 100) {
+            final List<Signal> temp = computeHandshake(numberInBinary - 100, handshake);
+            temp.add(Signal.CLOSE_YOUR_EYES);
+            return temp;
+        }
+        if(numberInBinary >= 10) {
+            final List<Signal> temp = computeHandshake(numberInBinary - 10, handshake);
+            temp.add(Signal.DOUBLE_BLINK);
+            return temp;
+        }
+        if(numberInBinary >= 1) {
+            final List<Signal> temp = computeHandshake(numberInBinary - 1, handshake);
+            temp.add(Signal.WINK);
+            return temp;
+        }
+        return handshake;
+    }
 }
