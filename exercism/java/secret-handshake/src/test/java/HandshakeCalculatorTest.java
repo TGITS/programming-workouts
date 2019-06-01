8cdf75473ae91d0e20b952c58df9ee63553b3915
cvaudry <cvaudry@norsys.fr>
2019-06-01 12:11:50 +0200

    MAJ de l'exercice SecretHandshake côté Exercism.

--- exercism/java/secret-handshake/src/test/java/HandshakeCalculatorTest.java
+++ exercism/java/secret-handshake/src/test/java/HandshakeCalculatorTest.java
@@ -1,3 +1,4 @@
+import org.junit.Ignore;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -40,7 +41,7 @@ public class HandshakeCalculatorTest {
     public void testThatInput8YieldsAJump() {
         assertEquals(
                 singletonList(Signal.JUMP),
-                handshakeCalculator.calculateHandshake(8));
+               handshakeCalculator.calculateHandshake(8));
     }
 
     @Test
@@ -54,7 +55,7 @@ public class HandshakeCalculatorTest {
     public void testAnInputThatYieldsTwoReversedActions() {
         assertEquals(
                 asList(Signal.DOUBLE_BLINK, Signal.WINK),
-                handshakeCalculator.calculateHandshake(19));
+               handshakeCalculator.calculateHandshake(19));
     }
 
     @Test
