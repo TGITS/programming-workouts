f1f36392dbc7ad65af9c79e73098517cf75e76c1
cvaudry <cvaudry@norsys.fr>
2019-06-01 12:01:58 +0200

    A second refactoring of SecretHandshake for Java Track of exercism

--- exercism/java/secret-handshake/src/main/java/HandshakeCalculator.java
+++ exercism/java/secret-handshake/src/main/java/HandshakeCalculator.java
@@ -2,29 +2,25 @@ import java.util.*;
 
 class HandshakeCalculator {
 
-    private static final Map<Integer, Optional<Signal>> SIGNAL_BY_BINARY_VALUE = new HashMap<>();
+    private final Signal[] signals ;
 
-    static {
-        SIGNAL_BY_BINARY_VALUE.put(0b10000, Optional.empty());
-        SIGNAL_BY_BINARY_VALUE.put(0b01000, Optional.of(Signal.JUMP));
-        SIGNAL_BY_BINARY_VALUE.put(0b00100, Optional.of(Signal.CLOSE_YOUR_EYES));
-        SIGNAL_BY_BINARY_VALUE.put(0b00010, Optional.of(Signal.DOUBLE_BLINK));
-        SIGNAL_BY_BINARY_VALUE.put(0b00001, Optional.of(Signal.WINK));
+    public HandshakeCalculator() {
+        signals = Signal.values();
+        Arrays.sort(this.signals, Comparator.comparingInt(Signal::mask).reversed());
     }
-
-    private static final int[] BINARY_VALUES_FOR_SIGNAL = new int[]{0b10000, 0b01000, 0b00100, 0b00010, 0b00001};
 
     public List<Signal> calculateHandshake(int number) {
         return computeHandshake(number, new ArrayList<>());
     }
 
-    private List<Signal> computeHandshake(int numberInBinary, List<Signal> handshake) {
-        for (int value : BINARY_VALUES_FOR_SIGNAL) {
-            if ((numberInBinary & value) == value) {
-                final List<Signal> temp = computeHandshake(numberInBinary - value, handshake);
-                SIGNAL_BY_BINARY_VALUE.get(value).ifPresent(temp::add);
-                if (!SIGNAL_BY_BINARY_VALUE.get(value).isPresent()) {
+    private List<Signal> computeHandshake(int number, List<Signal> handshake) {
+        for (Signal signal :  signals) {
+            if ((number & signal.mask()) == signal.mask()) {
+                final List<Signal> temp = computeHandshake(number - signal.mask(), handshake);
+                if (signal == Signal.REVERSE) {
                     Collections.reverse(temp);
+                } else {
+                    temp.add(signal);
                 }
                 return temp;
             }
--- exercism/java/secret-handshake/src/main/java/Signal.java
+++ exercism/java/secret-handshake/src/main/java/Signal.java
@@ -1,5 +1,18 @@
 enum Signal {
 
-    WINK, DOUBLE_BLINK, CLOSE_YOUR_EYES, JUMP
+    WINK(0b00001),
+    DOUBLE_BLINK(0b00010),
+    CLOSE_YOUR_EYES(0b00100),
+    JUMP(0b01000),
+    REVERSE(0b10000);
 
+    private final int mask;
+
+    Signal(int mask) {
+        this.mask = mask;
+    }
+
+    public int mask() {
+        return mask;
+    }
 }
