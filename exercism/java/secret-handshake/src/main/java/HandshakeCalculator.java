092fbcd9f180bbe0cbd36dec76fc2f315ca91f37
cvaudry <cvaudry@norsys.fr>
2019-06-01 11:27:16 +0200

    A first refactored solution to Secret Handshake from exercim in the Java Track that use binary operator

--- exercism/java/secret-handshake/src/main/java/HandshakeCalculator.java
+++ exercism/java/secret-handshake/src/main/java/HandshakeCalculator.java
@@ -5,28 +5,22 @@ class HandshakeCalculator {
     private static final Map<Integer, Optional<Signal>> SIGNAL_BY_BINARY_VALUE = new HashMap<>();
 
     static {
-        SIGNAL_BY_BINARY_VALUE.put(10000, Optional.empty());
-        SIGNAL_BY_BINARY_VALUE.put(1000, Optional.of(Signal.JUMP));
-        SIGNAL_BY_BINARY_VALUE.put(100, Optional.of(Signal.CLOSE_YOUR_EYES));
-        SIGNAL_BY_BINARY_VALUE.put(10, Optional.of(Signal.DOUBLE_BLINK));
-        SIGNAL_BY_BINARY_VALUE.put(1, Optional.of(Signal.WINK));
+        SIGNAL_BY_BINARY_VALUE.put(0b10000, Optional.empty());
+        SIGNAL_BY_BINARY_VALUE.put(0b01000, Optional.of(Signal.JUMP));
+        SIGNAL_BY_BINARY_VALUE.put(0b00100, Optional.of(Signal.CLOSE_YOUR_EYES));
+        SIGNAL_BY_BINARY_VALUE.put(0b00010, Optional.of(Signal.DOUBLE_BLINK));
+        SIGNAL_BY_BINARY_VALUE.put(0b00001, Optional.of(Signal.WINK));
     }
 
-    private static final int[] BINARY_VALUES_FOR_SIGNAL = new int[]{10000, 1000, 100, 10, 1};
-
+    private static final int[] BINARY_VALUES_FOR_SIGNAL = new int[]{0b10000, 0b01000, 0b00100, 0b00010, 0b00001};
 
     public List<Signal> calculateHandshake(int number) {
-        return computeHandshake(decimalToBinary(number), new ArrayList<>());
-    }
-
-    private int decimalToBinary(int decimal) {
-        return Integer.parseInt(Integer.toBinaryString(decimal));
+        return computeHandshake(number, new ArrayList<>());
     }
 
     private List<Signal> computeHandshake(int numberInBinary, List<Signal> handshake) {
-
         for (int value : BINARY_VALUES_FOR_SIGNAL) {
-            if (numberInBinary >= value) {
+            if ((numberInBinary & value) == value) {
                 final List<Signal> temp = computeHandshake(numberInBinary - value, handshake);
                 SIGNAL_BY_BINARY_VALUE.get(value).ifPresent(temp::add);
                 if (!SIGNAL_BY_BINARY_VALUE.get(value).isPresent()) {
