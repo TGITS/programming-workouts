dec47e7e2930707205569f279e47b04de49c3be3
cvaudry <cvaudry@norsys.fr>
2019-06-08 13:45:14 +0200

    Refactoring de leap & two-fer de la track javascript de Exercism

--- exercism/javascript/two-fer/two-fer.js
+++ exercism/javascript/two-fer/two-fer.js
@@ -1,6 +1,3 @@
-export const twoFer = (name) => {
-   if (!name || 0 === name.length || !name.trim()) {
-        name = 'you'
-   } 
-   return `One for ${name}, one for me.`
-};
+export function twoFer(name = 'you') {
+  return `One for ${name.trim() ? name : 'you'}, one for me.`;
+}
