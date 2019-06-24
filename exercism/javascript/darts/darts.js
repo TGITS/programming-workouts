d5ecf6daacae1a7137d701eb215cf3f055cc59fa
TGITS <thegeekintheshell@gmail.com>
2019-06-24 08:02:56 +0200

    A first solution to the exercise darts from Exercism in the track Javascript

--- exercism/javascript/darts/darts.js
+++ exercism/javascript/darts/darts.js
@@ -1,8 +1,22 @@
-//
-// This is only a SKELETON file for the 'Darts' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const euclidean_distance = (x, y) => {
+  return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2))
+}
 
-export const solve = () => {
-  throw new Error("Remove this statement and implement this function");
+export const solve = (x, y) => {
+  const distance_from_origin = euclidean_distance(x, y);
+  
+  if (distance_from_origin <= 1) {
+    return 10;
+  }
+
+  if (distance_from_origin > 1 && distance_from_origin <= 5) {
+    return 5;
+  }
+
+  if (distance_from_origin > 5 && distance_from_origin <= 10) {
+    return 1;
+  }
+
+  return 0;
 };
+
