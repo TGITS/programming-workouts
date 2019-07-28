49a76dfcc8bb7e42094e396b7108d18bf363c173
cvaudry <cvaudry@norsys.fr>
2019-07-28 15:59:58 +0200

    Une solution sans tableau associatif avec recherche directe

--- exercism/javascript/resistor-color-duo/resistor-color-duo.js
+++ exercism/javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,16 +1,10 @@
-const colorsMapping = {
-  "black": "0",
-  "brown": "1",
-  "red": "2",
-  "orange": "3",
-  "yellow": "4",
-  "green": "5",
-  "blue": "6",
-  "violet": "7",
-  "grey": "8",
-  "white": "9"
-};
+const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"]
+const colorCode = color =>  { 
+    for(var i = 0; i < COLORS.length; i++) {
+        if(color === COLORS[i]) { return i.toString() }
+    }
+}
 
 export const value = ([color_1, color_2]) => {
-  return Number(colorsMapping[color_1.toLowerCase()] + colorsMapping[color_2.toLowerCase()]);
+  return Number(colorCode(color_1.toLowerCase()) + colorCode(color_2.toLowerCase()));
 };
