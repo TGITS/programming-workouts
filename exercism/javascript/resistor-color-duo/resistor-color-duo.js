0991e6375f6f6d8dfc751bbd9d0dbb59a5be47c1
cvaudry <cvaudry@norsys.fr>
2019-07-28 16:02:24 +0200

    Utilisation de indexOf plutôt que de faire une recherche directe

--- exercism/javascript/resistor-color-duo/resistor-color-duo.js
+++ exercism/javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,9 +1,5 @@
 const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"]
-const colorCode = color =>  { 
-    for(var i = 0; i < COLORS.length; i++) {
-        if(color === COLORS[i]) { return i.toString() }
-    }
-}
+const colorCode = color =>  COLORS.indexOf(color).toString();
 
 export const value = ([color_1, color_2]) => {
   return Number(colorCode(color_1.toLowerCase()) + colorCode(color_2.toLowerCase()));
