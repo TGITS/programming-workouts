0bb23e9b5bde21b5965bc1a3380d271712800e3a
TGITS <thegeekintheshell@gmail.com>
2019-07-30 14:21:02 +0200

    Une deuxième solution à l'exercice reverse-string de exercism dans la track Javascript

--- exercism/javascript/reverse-string/reverse-string.js
+++ exercism/javascript/reverse-string/reverse-string.js
@@ -1,7 +1,7 @@
 export const reverseString = (stringToReverse) => {
   let reversedString = '';
-  for(let i = 0; i < stringToReverse.length; i++) {
-    reversedString = stringToReverse.charAt(i) + reversedString;
+  for(var i = stringToReverse.length-1; i >=0; i--) {
+    reversedString += stringToReverse.charAt(i);
   }
   return reversedString;
 };
