f3b3f51db07c54d0d57308d4ed04eb2a3492b830
TGITS <thegeekintheshell@gmail.com>
2019-07-31 08:15:18 +0200

    MAJ de la solution pour resistor-color-duo de Exercism dans la track Javascript

--- exercism/javascript/resistor-color-duo/resistor-color-duo.js
+++ exercism/javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,5 +1,5 @@
 const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"]
 
 export const value = (colors) => {
-  return Number(colors.map(color =>  COLORS.indexOf(color.toLowerCase()).toString()).join(''));
+  return Number(colors.map(color =>  COLORS.indexOf(color.toLowerCase())).join(''));
 };
