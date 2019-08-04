3ef32cd8e7bd40a6f18f9294df0abd014f5c50d0
cvaudry <cvaudry@norsys.fr>
2019-08-04 12:02:42 +0200

    Une proposition de solution à l'exercice roman numeral de exercism dans la track Javascript. Solution peu satisfaisante (manque d'élégance)

--- exercism/javascript/roman-numerals/roman-numerals.js
+++ exercism/javascript/roman-numerals/roman-numerals.js
@@ -1,8 +1,85 @@
-//
-// This is only a SKELETON file for the 'Roman Numerals' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const toRoman = (num) => {
+  let roman = "";
 
-export const toRoman = () => {
-  throw new Error("Remove this statement and implement this function");
+  while (num >= 1000) {
+    roman = roman + "M";
+    num -= 1000;
+  }
+
+  if (num >= 900) {
+    roman = roman + "CM";
+    num -= 900;
+  }
+
+  if (num < 900 && num >= 500) {
+    let temp = "";
+    while (num > 599) {
+      temp += "C";
+      num -= 100;
+    }
+    roman = roman + "D" + temp;
+    num -= 500
+  }
+
+  if (num >= 400) {
+    roman = roman + "CD"
+    num -= 400;
+  }
+
+  while (num >= 100) {
+    roman = roman + "C";
+    num -= 100;
+  }
+
+  if (num >= 90) {
+    roman = roman + "XC";
+    num -= 90;
+  }
+
+  if (num < 90 && num >= 50) {
+    let temp = "";
+    while (num > 59) {
+      temp += "X";
+      num -= 10;
+    }
+    roman = roman + "L" + temp;
+    num -= 50;
+  }
+
+  if (num >= 40) {
+    roman = roman + "XL";
+    num -= 40;
+  }
+
+  while (num >= 10) {
+    roman = roman + "X";
+    num -= 10;
+  }
+
+  if (num === 9) {
+    roman = roman + "IX";
+    num -= 9;
+  }
+
+  if (num < 9 && num >= 5) {
+    let temp = "";
+    while (num > 5) {
+      temp += "I";
+      num -= 1;
+    }
+    roman = roman + "V" + temp;
+    num -= 5;
+  }
+
+  if (num === 4) {
+    roman = roman + "IV";
+    num -= 4;
+  }
+
+  while (num >= 1) {
+    roman = roman + "I";
+    num -= 1;
+  }
+
+  return roman;
 };
