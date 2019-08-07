70ed261ddf9c2be74173dc7afaeeb6ff3188760d
TGITS <thegeekintheshell@gmail.com>
2019-08-07 14:56:52 +0200

    Proposition de solution à l'exercice Gigasecond de la track Javascript de Exercism

--- exercism/javascript/gigasecond/gigasecond.js
+++ exercism/javascript/gigasecond/gigasecond.js
@@ -1,5 +1,4 @@
 const ONE_TERA_MILLISECONDS = 1e12;
 export const gigasecond = (date) => {
-  let datePlus1TeraMilliseconds = new Date(date.getTime() + ONE_TERA_MILLISECONDS)
-  return datePlus1TeraMilliseconds;
+  return new Date(date.getTime() + ONE_TERA_MILLISECONDS)
 };
