c48136929940736a84300ec1932c299eec438529
cvaudry <cvaudry@norsys.fr>
2019-08-15 11:02:42 +0200

    Une première solution à l'exercice 'Collatz Conjecture' de Exercism dans la track Clojure

--- exercism/clojure/collatz-conjecture/src/collatz_conjecture.clj
+++ exercism/clojure/collatz-conjecture/src/collatz_conjecture.clj
@@ -1,5 +1,13 @@
 (ns collatz-conjecture)
 
-(defn collatz [num] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn collatz [num]
+  (if (<= num 0) (throw (IllegalArgumentException. "The provided number should be strictly positive"))
+      (loop [n num step 0]
+        (if (= 1 n)
+          step
+          (recur (if (= 0 (mod n 2)) (/ n 2) (+ (* n 3) 1))
+                 (inc step))
+          )
+        )
+      )
+  )
