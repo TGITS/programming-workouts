1ab7a8fd407548e69eccfecbed45cbf04720cd58
TGITS <thegeekintheshell@gmail.com>
2019-08-21 16:40:26 +0200

    Une proposition de solution pour l'exercice "hamming" de Exercism dans la track Clojure.

--- exercism/clojure/hamming/src/hamming.clj
+++ exercism/clojure/hamming/src/hamming.clj
@@ -1,5 +1,9 @@
 (ns hamming)
 
-(defn distance [strand1 strand2] ; <- arglist goes here
-  ;; your code goes here
-)
+(defn distance [strand1 strand2]
+  (if (not (= (count strand1) (count strand2)))
+    nil
+    (loop [char-seq1 (seq strand1) char-seq2 (seq strand2) counter 0]
+      (if (empty? char-seq1)
+        counter
+        (recur (rest char-seq1) (rest char-seq2) (if (= (first char-seq1) (first char-seq2)) counter (inc counter)))))))
