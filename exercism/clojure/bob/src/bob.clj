1da5dbd4a780d2c9529d33cd120bedf66b3179af
TGITS <thegeekintheshell@gmail.com>
2019-08-22 14:06:17 +0200

    Une première solution à l'exercice Bob soumise dans la track Clojure.

--- exercism/clojure/bob/src/bob.clj
+++ exercism/clojure/bob/src/bob.clj
@@ -1,5 +1,11 @@
 (ns bob)
 
-(defn response-for [s] ;; <- arglist goes here
-  ;; your code goes here
+(defn response-for [s] 
+  (cond
+    (or (= 0 (count s)) (re-matches #"\s+" s)) "Fine. Be that way!"
+    (re-matches #"([A-Z]|\s)+\?" s) "Calm down, I know what I'm doing!"
+    (re-matches #"([1-9,]|\s)+" s) "Whatever."
+    (re-matches #"([1-9A-Z]|\s|[,%\^\*@#\$\(\)\!])+\!?" s) "Whoa, chill out!"
+    (re-matches #"([1-9a-zA-Z,]|\s)+\?" s) "Sure."
+    :else "Whatever.")
 )
