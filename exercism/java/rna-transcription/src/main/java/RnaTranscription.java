f9a55a0ee4f5663cfcbf9078c90e7976dd74c409
TGITS <thegeekintheshell@gmail.com>
2019-08-23 14:32:58 +0200

    Une proposition de solution pour l'exercice RnaTranscription de Exercism dans la track Java

--- exercism/java/rna-transcription/src/main/java/RnaTranscription.java
+++ exercism/java/rna-transcription/src/main/java/RnaTranscription.java
@@ -1,7 +1,24 @@
+import java.util.stream.Collectors;
+
 class RnaTranscription {
 
     String transcribe(String dnaStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return dnaStrand.chars().mapToObj(c -> (char) c).map(this::transcribe).collect(Collectors.joining(""));
+    }
+
+    private String transcribe(final char c) {
+        switch (c) {
+            case 'G':
+                return "C";
+            case 'C':
+                return "G";
+            case 'T':
+                return "A";
+            case 'A':
+                return "U";
+            default:
+                return "";
+        }
     }
 
 }
