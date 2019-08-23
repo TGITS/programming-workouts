5ab5dd2b643efffb46608a17bc35c929bec4264f
cvaudry <cvaudry@norsys.fr>
2019-08-23 21:02:43 +0200

    Proposition de solution pour l'exercice Pangram de Exercism dans la track Java

--- exercism/java/pangram/src/main/java/PangramChecker.java
+++ exercism/java/pangram/src/main/java/PangramChecker.java
@@ -1,7 +1,16 @@
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class PangramChecker {
 
+    private final static Set<String> alphabet = Set.of("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+
     public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return input.chars()
+                .mapToObj(c -> Character.toString((char) c))
+                .map(String::toLowerCase)
+                .collect(Collectors.toSet())
+                .containsAll(alphabet);
     }
 
 }
