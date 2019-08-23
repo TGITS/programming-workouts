5facb1af190216ed14e868f2fc620a07796b93fb
TGITS <thegeekintheshell@gmail.com>
2019-08-23 14:17:43 +0200

    Proposition de solution pour l'exercice ArmstrongNumbers d'Exercism dans la track Java

--- exercism/java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ exercism/java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -1,9 +1,13 @@
 class ArmstrongNumbers {
 
-	boolean isArmstrongNumber(int numberToCheck) {
-
-		throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-		
-	}
-
+    boolean isArmstrongNumber(int numberToCheck) {
+        String numberAsString = Integer.toString(numberToCheck);
+        int numberLength = numberAsString.length();
+        int sumOfPower = 0;
+        for (int i = 0; i < numberLength; i++) {
+            int digit = Integer.parseInt(Character.toString(numberAsString.charAt(i)));
+            sumOfPower += (int) Math.round(Math.pow(digit, numberLength));
+        }
+        return sumOfPower == numberToCheck;
+    }
 }
