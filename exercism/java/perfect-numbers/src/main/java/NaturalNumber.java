fffb43979bc38e624ad8d7106503ffdcc11242fc
TGITS <thegeekintheshell@gmail.com>
2019-08-23 14:44:14 +0200

    Initialisation de l'exercice Perfect Numbers de Exercism dans la track Java

--- exercism/java/perfect-numbers/src/main/java/NaturalNumber.java
+++ exercism/java/perfect-numbers/src/main/java/NaturalNumber.java
@@ -1,5 +1,12 @@
 class NaturalNumber {
 
+    private final int value;
 
+    public NaturalNumber(int i) {
+        value = i;
+    }
 
+    public Classification getClassification() {
+        return Classification.DEFICIENT;
+    }
 }
