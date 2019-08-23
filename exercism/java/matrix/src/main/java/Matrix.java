4473a3e7bc645e4003ad81bc135323ce109efabf
TGITS <thegeekintheshell@gmail.com>
2019-08-23 09:49:33 +0200

    WIP - Travail sur l'exercice Matrix de Exercism dans la track Java

--- exercism/java/matrix/src/main/java/Matrix.java
+++ exercism/java/matrix/src/main/java/Matrix.java
@@ -1,12 +1,21 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 class Matrix {
+    private String[] rows;
 
     Matrix(String matrixAsString) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        rows = matrixAsString.split("\n");
     }
 
     int[] getRow(int rowNumber) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        List<Integer> temp = Arrays.stream(rows[rowNumber-1].split(" ")).map(String::trim).map(Integer::parseInt).collect(Collectors.toList());
+        int[] result = new int[temp.size()];
+        for(int i =0; i < temp.size() ; i++) {
+            result[i] = temp.get(i);
+        }
+        return result;
     }
 
     int[] getColumn(int columnNumber) {
