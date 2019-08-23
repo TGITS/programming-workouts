32270da19b2d6cb406816c204bf68094fa85b173
TGITS <thegeekintheshell@gmail.com>
2019-08-23 17:49:12 +0200

    Une proposition de solution à l'exercice Perfect Numbers de Exercism dans la Track Java

--- exercism/java/perfect-numbers/src/main/java/NaturalNumber.java
+++ exercism/java/perfect-numbers/src/main/java/NaturalNumber.java
@@ -1,12 +1,38 @@
+import java.util.stream.IntStream;
+
 class NaturalNumber {
 
-    private final int value;
+    private final int number;
 
     public NaturalNumber(int i) {
-        value = i;
+        if(i <= 0) {
+            throw new IllegalArgumentException("You must supply a natural number (positive integer)");
+        }
+        number = i;
     }
 
     public Classification getClassification() {
+        if (isPerfect()) {
+            return Classification.PERFECT;
+        }
+
+        if (isAbundant()) {
+            return Classification.ABUNDANT;
+        }
+
         return Classification.DEFICIENT;
     }
+
+    private int aliquotSum() {
+        return IntStream.range(1, number).filter(i -> number % i == 0).sum();
+    }
+
+    private boolean isPerfect() {
+        return number == aliquotSum();
+    }
+
+    private boolean isAbundant() {
+        return aliquotSum() > number;
+    }
+
 }
