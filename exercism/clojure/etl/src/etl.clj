b1538605ef7b416e93dcf36d6ede7fb0ef20da2a
cvaudry <cvaudry@norsys.fr>
2019-08-24 14:55:36 +0200

    une proposition de solution à l'exercice ETL de exercism dans la track Clojure

--- exercism/clojure/etl/src/etl.clj
+++ exercism/clojure/etl/src/etl.clj
@@ -3,6 +3,5 @@
 (defn keys-list-to-hash [value keys-list]
   (map #(hash-map % value) keys-list))
 
-(defn transform [source] 
-  (reduce merge {} (flatten (map #(etl/keys-list-to-hash % (map clojure.string/lower-case (get source %))) (keys source))))
-  )
+(defn transform [source]
+  (reduce merge {} (flatten (map #(etl/keys-list-to-hash % (map clojure.string/lower-case (get source %))) (keys source)))))
