81a905d2a9dbac8615c7385ffc0eeb252c39d777
cvaudry <cvaudry@norsys.fr>
2019-08-26 21:02:25 +0200

    WIP - Travail sur Flatten Array de Exercism dans la track Clojure (sans utiliser directement flatten de Clojure)

--- exercism/clojure/flatten-array/src/flatten_array.clj
+++ exercism/clojure/flatten-array/src/flatten_array.clj
@@ -1,5 +1,17 @@
 (ns flatten-array)
 
-(defn flatten [arr] ;; <- arglist goes here
-  ;; your code goes here
-)
+(declare flatten)
+
+(defn manage-element [element result-array]
+  (cond 
+    (nil? element) result-array
+    (atom? element) (cons element result-array)
+    (seq? element) (concat (flatten element) result-array)
+    )
+  )
+
+(defn flatten [arr]
+  (loop [process-array arr result-array []]
+    (if (nil? process-array)
+      (vec result-array)
+      (recur (rest process-array) (manage-element (first process-array) result-array)))))
