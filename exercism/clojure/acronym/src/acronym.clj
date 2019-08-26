1747a6cab241bf9bca0d33e74c005876626cacb4
TGITS <thegeekintheshell@gmail.com>
2019-08-26 17:58:23 +0200

    Une première proposition de solution à l'exercice Acronym de Exercism dans la track Clojure

--- exercism/clojure/acronym/src/acronym.clj
+++ exercism/clojure/acronym/src/acronym.clj
@@ -1,5 +1,13 @@
-(ns acronym)
+(ns acronym
+  (:require [clojure.string :as str]))
 
-(defn acronym [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn acronym [phrase]
+  (if (str/blank? phrase)
+    ""
+    (str/join
+     (map #(subs %1 0 1)
+          (flatten
+           (map #(str/split %1 #"[a-z]+")
+                (map #(str/replace %1 #",|:" "")
+                     (map  #(str (str/upper-case (subs %1 0 1)) (subs %1 1))
+                           (str/split phrase #"\s|\-")))))))))
