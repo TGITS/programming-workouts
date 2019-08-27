c368177075d25dd1cab0f5b21a51c5620e10f6c1
TGITS <thegeekintheshell@gmail.com>
2019-08-27 17:12:42 +0200

    Une proposition de solution à l'exercise Scrabble Score de Exercism dans la track Clojure.

--- exercism/clojure/scrabble-score/src/scrabble_score.clj
+++ exercism/clojure/scrabble-score/src/scrabble_score.clj
@@ -1,9 +1,19 @@
-(ns scrabble-score)
+(ns scrabble-score
+  (:require [clojure.string :as string]))
 
-(defn score-letter [] ;; <- arglist goes here
-      ;; your code goes here
-)
+(defn score-letter [letter]
+  (cond
+    (contains? #{"A" "E" "I" "O" "U" "L" "N" "R" "S" "T"} (string/upper-case letter)) 1
+    (contains? #{"D" "G"} (string/upper-case letter)) 2
+    (contains? #{"B" "C" "M" "P"} (string/upper-case letter)) 3
+    (contains? #{"F" "H" "V" "W" "Y"} (string/upper-case letter)) 4
+    (= "K" (string/upper-case letter)) 5
+    (contains? #{"J" "X"} (string/upper-case letter)) 8
+    (contains? #{"Q" "Z"} (string/upper-case letter)) 10))
 
-(defn score-word [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn score-word [word]
+  (->>
+   (seq word)
+   (map str)
+   (map scrabble-score/score-letter)
+   (reduce +)))
