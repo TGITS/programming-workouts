91522f837835b66ee43d923a31d353589e1ee3ae
TGITS <thegeekintheshell@gmail.com>
2019-08-27 11:53:30 +0200

    Une proposition de solution pour l'exercice Flatten Array de Exercism dans la track Clojure

--- exercism/clojure/flatten-array/src/flatten_array.clj
+++ exercism/clojure/flatten-array/src/flatten_array.clj
@@ -1,17 +1,23 @@
-(ns flatten-array)
+(ns flatten-array
+  (:require [clojure.inspector :as inspector]))
 
+; A simple solution that use clojure.core/flatten 
+; (defn flatten [arr] 
+;   (filter (complement nil?) (clojure.core/flatten arr))
+;  )
+
+; A solution that do not use clojure.core/flatten
 (declare flatten)
 
-(defn manage-element [element result-array]
-  (cond 
-    (nil? element) result-array
-    (atom? element) (cons element result-array)
-    (seq? element) (concat (flatten element) result-array)
-    )
-  )
+(defn flatten-element [element flatten-array]
+  (cond
+    (nil? element) flatten-array
+    (inspector/atom? element) (conj flatten-array element )
+    (empty? element) flatten-array
+    (vector? element) (into [] (concat flatten-array (flatten-array/flatten element)))))
 
 (defn flatten [arr]
-  (loop [process-array arr result-array []]
-    (if (nil? process-array)
-      (vec result-array)
-      (recur (rest process-array) (manage-element (first process-array) result-array)))))
+  (loop [nested-array arr flatten-array []]
+    (if (empty? nested-array)
+      flatten-array
+      (recur (rest nested-array) (flatten-element (first nested-array) flatten-array)))))
