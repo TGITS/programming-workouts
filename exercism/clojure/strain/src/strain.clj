82806f780129c6991c18f9cb3d2c850fe54f7642
cvaudry <cvaudry@norsys.fr>
2019-08-31 16:57:45 +0200

    Une 2ème proposition de solution pour l'exercice Strain de Exercism dans la track Clojure

--- exercism/clojure/strain/src/strain.clj
+++ exercism/clojure/strain/src/strain.clj
@@ -1,9 +1,19 @@
 (ns strain)
 
-(defn retain [pred v] 
-  (reduce #(if (pred %2) (conj %1 %2) %1) [] v)
-  )
+;; A solution with reduce 
+;; (defn retain [pred v] 
+;;   (reduce #(if (pred %2) (conj %1 %2) %1) [] v))
 
-(defn discard [pred v] 
-  (reduce #(if (pred %2) %1 (conj %1 %2)) [] v)
-)
+;; (defn discard [pred v] 
+;;   (reduce #(if (pred %2) %1 (conj %1 %2)) [] v))
+
+;; A recursive solution 
+(defn retain [pred v]
+  (if
+   (or (nil? v) (empty? v)) []
+   (if (pred (first v)) (cons  (first v) (retain pred (rest v))) (retain pred (rest v)))))
+
+(defn discard [pred v]
+  (if
+   (or (nil? v) (empty? v)) []
+   (if (pred (first v)) (discard pred (rest v)) (cons  (first v) (discard pred (rest v))))))
