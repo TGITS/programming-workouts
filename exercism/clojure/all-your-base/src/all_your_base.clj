a11a9ceb4177b1072f9e9c1e9e3337c113dfe448
TGITS <thegeekintheshell@gmail.com>
2019-09-03 14:00:31 +0200

    Une solution à l'exercice 'all-your-base' de Exercism dans la track Clojure

--- exercism/clojure/all-your-base/src/all_your_base.clj
+++ exercism/clojure/all-your-base/src/all_your_base.clj
@@ -7,16 +7,18 @@
    (reverse)
    (map vector (range 0 (count number-as-seq)))
    (map #(int (* (nth %1 1) (Math/pow input-base (nth %1 0)))))
-   (reverse)))
+   (reduce +)))
 
-(defn from-base-10 [output-base number-as-seq]
-  (loop [number (Integer/parseInt (str/join number-as-seq) 10) remainder-list '()]
-    (if (= 0 number)  remainder-list
-        (recur (/ number output-base) (cons (rem number output-base) remainder-list)))))
+(defn from-base-10 [output-base number-in-base-10]
+  (loop [number number-in-base-10 remainder-list '()]
+    (if (= 0 number)
+      remainder-list
+      (recur (quot number output-base) (cons (rem number output-base) remainder-list)))))
 
 (defn convert [input-base number-as-seq output-base]
   (cond
-    (or (= 0 input-base) (= 1 input-base) (= 0 output-base) (= 1 output-base)) nil
+    (or (>= 1 input-base) (>= 1 output-base)) nil
+    (empty? number-as-seq) '()
     (= 0 (reduce + number-as-seq)) '(0)
     (> (count (filter #(< %1 0) number-as-seq)) 0) nil
     (> (count (filter #(>= %1 input-base) number-as-seq)) 0) nil
