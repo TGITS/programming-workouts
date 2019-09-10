1fcf1d0aa278cbd3fc16a3c19d5f488015f41aad
cvaudry <cvaudry@norsys.fr>
2019-09-10 21:02:51 +0200

    Une solution au problème Space Age de Exercism dans la track Clojure

--- exercism/clojure/space-age/src/space_age.clj
+++ exercism/clojure/space-age/src/space_age.clj
@@ -2,26 +2,40 @@
 
 (def earth-orbital-period-in-seconds 31557600.0)
 
+(def mercury-orbital-period-in-seconds (* earth-orbital-period-in-seconds 0.2408467))
+
+(def venus-orbital-period-in-seconds (* earth-orbital-period-in-seconds 0.61519726))
+
+(def mars-orbital-period-in-seconds (* earth-orbital-period-in-seconds 1.8808158))
+
+(def jupiter-orbital-period-in-seconds (* earth-orbital-period-in-seconds 11.862615))
+
+(def saturn-orbital-period-in-seconds (* earth-orbital-period-in-seconds 29.447498))
+
+(def neptune-orbital-period-in-seconds (* earth-orbital-period-in-seconds 164.79132))
+
+(def uranus-orbital-period-in-seconds (* earth-orbital-period-in-seconds 84.016846))
+
 (defn on-earth [seconds]
   (/ (double seconds) earth-orbital-period-in-seconds))
 
 (defn on-mercury [seconds]
-  (* (on-earth seconds) 0.2408467))
+  (/ (double seconds) mercury-orbital-period-in-seconds))
 
 (defn on-venus [seconds]
-  seconds)
+  (/ (double seconds) venus-orbital-period-in-seconds))
 
 (defn on-mars [seconds]
-  seconds)
+  (/ (double seconds) mars-orbital-period-in-seconds))
 
 (defn on-jupiter [seconds]
-  seconds)
+  (/ (double seconds) jupiter-orbital-period-in-seconds))
 
 (defn on-saturn [seconds]
-  seconds)
+  (/ (double seconds) saturn-orbital-period-in-seconds))
 
 (defn on-neptune [seconds]
-  seconds)
+  (/ (double seconds) neptune-orbital-period-in-seconds))
 
 (defn on-uranus [seconds]
-  seconds)
+  (/ (double seconds) uranus-orbital-period-in-seconds))
