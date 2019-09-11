696484f91836b2100f85583c96a012ed811ce704
TGITS <thegeekintheshell@gmail.com>
2019-09-11 14:59:55 +0200

    Une solution à l'exercice Phone Number de Exercism dans la track Clojure

--- exercism/clojure/phone-number/src/phone_number.clj
+++ exercism/clojure/phone-number/src/phone_number.clj
@@ -1,13 +1,16 @@
-(ns phone-number)
+(ns phone-number
+  (:require [clojure.string :as string]))
 
-(defn number [num-string] ;; <- arglist goes here
-      ;; your code goes here
-)
+(defn number [num-string]
+  (let [clean-string (string/replace num-string #"\(|\)|\.|\-|\s+" "")]
+    (cond
+      (= 10 (count clean-string)) clean-string
+      (and (= 11 (count clean-string)) (string/starts-with? clean-string "1")) (subs clean-string 1)
+      :else "0000000000")))
 
-(defn area-code [num-string] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn area-code [num-string]
+  (string/join (take 3 (seq (phone-number/number num-string)))))
 
-(defn pretty-print [num-string] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn pretty-print [num-string]
+  (let [clean-string (seq (phone-number/number num-string))]
+    (str "(" (string/join (take 3 clean-string)) ") " (string/join (take 3 (drop 3 clean-string))) "-" (string/join (drop 6 clean-string)))))
