bc4782b81620aeb12cdcc7d97fe586c82c8a0751
cvaudry <cvaudry@norsys.fr>
2019-09-15 12:13:26 +0200

    Une solution à l'exercice Acronym de Exercism dans la Track Java

--- exercism/java/acronym/src/main/java/Acronym.java
+++ exercism/java/acronym/src/main/java/Acronym.java
@@ -1,11 +1,16 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 class Acronym {
 
+    private String phrase;
+
     Acronym(String phrase) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.phrase = phrase;
     }
 
     String get() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return Arrays.stream(this.phrase.replaceAll("\\-|\\,|_", " ").split("\\s+")).map(s -> Character.toString(s.charAt(0)).toUpperCase()).collect(Collectors.joining());
     }
 
 }
