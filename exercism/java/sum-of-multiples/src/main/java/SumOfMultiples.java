5d49b0a5208c29bbae36ba3eb8451ec649396e2b
cvaudry <cvaudry@norsys.fr>
2019-09-15 15:27:07 +0200

    Une solution à l'exercice SumOfMultiples de Exercism dans la track Java

--- exercism/java/sum-of-multiples/src/main/java/SumOfMultiples.java
+++ exercism/java/sum-of-multiples/src/main/java/SumOfMultiples.java
@@ -1,11 +1,20 @@
+import java.util.stream.IntStream;
+
 class SumOfMultiples {
 
+    private int number;
+    private int[] multiples;
+
     SumOfMultiples(int number, int[] set) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.number = number;
+        this.multiples = set;
     }
 
     int getSum() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (number < 2) {
+            return 0;
+        }
+        return IntStream.range(1, this.number).filter(i -> IntStream.of(this.multiples).filter(j -> j != 0).anyMatch(j -> i % j == 0)).sum();
     }
 
 }
