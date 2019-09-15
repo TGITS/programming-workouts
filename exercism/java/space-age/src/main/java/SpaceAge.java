22d4995abac40cba0580fb281e426db30f746f7a
cvaudry <cvaudry@norsys.fr>
2019-09-15 12:12:50 +0200

    Une solution à l'exercice Space Age de Exercism dans la track Java

--- exercism/java/space-age/src/main/java/SpaceAge.java
+++ exercism/java/space-age/src/main/java/SpaceAge.java
@@ -1,43 +1,54 @@
 class SpaceAge {
 
+    private static final double EARTH_ORBITAL_PERIOD_IN_SECONDS = 31557600.0;
+    private static final double MERCURY_ORBITAL_PERIOD_IN_SECONDS = EARTH_ORBITAL_PERIOD_IN_SECONDS * 0.2408467;
+    private static final double VENUS_ORBITAL_PERIOD_IN_SECONDS = EARTH_ORBITAL_PERIOD_IN_SECONDS * 0.61519726;
+    private static final double MARS_ORBITAL_PERIOD_IN_SECONDS = EARTH_ORBITAL_PERIOD_IN_SECONDS * 1.8808158;
+    private static final double JUPITER_ORBITAL_PERIOD_IN_SECONDS = EARTH_ORBITAL_PERIOD_IN_SECONDS * 11.862615;
+    private static final double SATURN_ORBITAL_PERIOD_IN_SECONDS = EARTH_ORBITAL_PERIOD_IN_SECONDS * 29.447498;
+    private static final double URANUS_ORBITAL_PERIOD_IN_SECONDS = EARTH_ORBITAL_PERIOD_IN_SECONDS * 84.016846;
+    private static final double NEPTUNE_ORBITAL_PERIOD_IN_SECONDS = EARTH_ORBITAL_PERIOD_IN_SECONDS * 164.79132;
+
+    private double seconds;
+
     SpaceAge(double seconds) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.seconds = seconds;
     }
 
     double getSeconds() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return seconds;
     }
 
     double onEarth() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return this.seconds / EARTH_ORBITAL_PERIOD_IN_SECONDS;
     }
 
     double onMercury() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return this.seconds / MERCURY_ORBITAL_PERIOD_IN_SECONDS;
     }
 
     double onVenus() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return this.seconds / VENUS_ORBITAL_PERIOD_IN_SECONDS;
     }
 
     double onMars() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return this.seconds / MARS_ORBITAL_PERIOD_IN_SECONDS;
     }
 
     double onJupiter() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return this.seconds / JUPITER_ORBITAL_PERIOD_IN_SECONDS;
     }
 
     double onSaturn() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return this.seconds / SATURN_ORBITAL_PERIOD_IN_SECONDS;
     }
 
     double onUranus() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+       return this.seconds / URANUS_ORBITAL_PERIOD_IN_SECONDS;
     }
 
     double onNeptune() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return this.seconds / NEPTUNE_ORBITAL_PERIOD_IN_SECONDS;
     }
 
 }
