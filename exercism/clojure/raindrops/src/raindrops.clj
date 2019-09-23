69f83997fa824407aaac2a847a18f0d95bcbaade
TGITS <thegeekintheshell@gmail.com>
2019-09-23 18:30:08 +0200

    Une proposition de solution à l'exercice Raindrops dans les Tracks Python et Clojure

--- exercism/clojure/raindrops/src/raindrops.clj
+++ exercism/clojure/raindrops/src/raindrops.clj
@@ -1,5 +1,16 @@
 (ns raindrops)
 
-(defn convert [] ;; <- arglist goes here
-      ;; your code goes here
-)
+(defn is-divisible-by-3 [number]
+  (= 0 (rem number 3)))
+
+(defn is-divisible-by-5 [number]
+  (= 0 (rem number 5)))
+
+(defn is-divisible-by-7 [number]
+  (= 0 (rem number 7)))
+
+(defn convert [number]
+  (str (if (is-divisible-by-3 number) "Pling" "")
+       (if (is-divisible-by-5 number) "Plang" "")
+       (if (is-divisible-by-7 number) "Plong" "")
+       (if (and (not (is-divisible-by-3 number)) (not (is-divisible-by-5 number)) (not (is-divisible-by-7 number))) (str number) "")))
