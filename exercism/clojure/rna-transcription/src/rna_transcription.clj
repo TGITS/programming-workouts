5ed1e5505abc483916514b44914c2bd36167f10a
cvaudry <cvaudry@norsys.fr>
2019-09-29 16:00:57 +0200

    Une nouvelle solution à RNA Transcription de Exercism dans la track Clojure

--- exercism/clojure/rna-transcription/src/rna_transcription.clj
+++ exercism/clojure/rna-transcription/src/rna_transcription.clj
@@ -3,12 +3,9 @@
 (def nucleotide-transcription-map {\A \U \T \A \C \G \G \C})
 
 (defn transcribe [nucleotide]
-  (if (contains? nucleotide-transcription-map nucleotide )
-    (get nucleotide-transcription-map nucleotide)
-    (throw (AssertionError. "The provided nucleotide does not exist"))
-    )
-  )
+  (doto
+   (nucleotide-transcription-map nucleotide)
+    (assert)))
 
 (defn to-rna [dna]
   (apply str (map rna-transcription/transcribe dna)))
-
