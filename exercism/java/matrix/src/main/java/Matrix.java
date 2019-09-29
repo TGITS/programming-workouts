8f94ce5f1c794a98a5c11110bf9140beed0d0977
cvaudry <cvaudry@norsys.fr>
2019-09-29 16:50:20 +0200

    Nouvelle solution à l'exercice Matrix de Exercism dans la Track Java

--- exercism/java/matrix/src/main/java/Matrix.java
+++ exercism/java/matrix/src/main/java/Matrix.java
@@ -1,27 +1,26 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 class Matrix {
-    private String[] rows;
+    private int[][] matrix;
 
     Matrix(String matrixAsString) {
-        rows = matrixAsString.split("\n");
+        String[] rows = matrixAsString.split("\n");
+        matrix = new int[rows.length][];
+        for (int rowIndex = 0; rowIndex < rows.length; rowIndex++) {
+            String[] line = rows[rowIndex].split(" ");
+            matrix[rowIndex] = new int[line.length];
+            for (int columnIndex = 0; columnIndex < line.length; columnIndex++) {
+                matrix[rowIndex][columnIndex] = Integer.parseInt(line[columnIndex]);
+            }
+        }
     }
 
     int[] getRow(int rowNumber) {
-        List<Integer> list = Arrays.stream(rows[rowNumber - 1].split(" ")).map(Integer::parseInt).collect(Collectors.toList());
-        int[] row = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            row[i] = list.get(i);
-        }
-        return row;
+        return matrix[rowNumber - 1];
     }
 
     int[] getColumn(int columnNumber) {
-        int[] column = new int[rows.length];
-        for (int i = 0; i < rows.length; i++) {
-            column[i] = getRow(i + 1)[columnNumber - 1];
+        int[] column = new int[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            column[i] = matrix[i][columnNumber - 1];
         }
         return column;
     }
