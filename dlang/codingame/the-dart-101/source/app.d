e43f3554d84e7d90260d5c0b91e9e806896d6469
TGITS <thegeekintheshell@gmail.com>
2019-10-07 18:03:56 +0200

    Initialisation d'un puzzle CodinGame en D

---
+++ dlang/codingame/the-dart-101/source/app.d
@@ -0,0 +1,23 @@
+//import std.stdio;
+import std;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+void main()
+{
+    int N = readln.strip.to!int;
+    for (int i = 0; i < N; i++) {
+        string PLAYER = readln.strip;
+    }
+    for (int i = 0; i < N; i++) {
+        string SHOOTS = readln.strip;
+    }
+
+    // Write an action using writeln().
+    // To debug: stderr.writeln("Debug messages...");
+
+    writeln("winner");
+}
