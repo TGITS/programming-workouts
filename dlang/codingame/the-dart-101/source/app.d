c96d5edb9f6469bdad03e5374852248eb7366809
TGITS <thegeekintheshell@gmail.com>
2019-10-08 14:49:35 +0200

    WIP - Travail sur le puzzle "the dart 101" de CodinGame en D

--- dlang/codingame/the-dart-101/source/app.d
+++ dlang/codingame/the-dart-101/source/app.d
@@ -1,5 +1,6 @@
-//import std.stdio;
 import std;
+import std.uni : isWhite;
+import std.array : split;
 
 /**
  * Auto-generated code below aims at helping you parse
@@ -8,16 +9,32 @@ import std;
 
 void main()
 {
-    int N = readln.strip.to!int;
-    for (int i = 0; i < N; i++) {
-        string PLAYER = readln.strip;
+    int n = readln.strip.to!int;
+    stderr.writeln("Number of players : ", n);
+    string[] names;
+    string[] shootsForEachPlayer;
+    for (int i = 0; i < n; i++)
+    {
+        names ~= readln.strip;
     }
-    for (int i = 0; i < N; i++) {
-        string SHOOTS = readln.strip;
+    for (int i = 0; i < n; i++)
+    {
+        shootsForEachPlayer ~= readln.strip;
     }
 
+    stderr.writeln("names : ", names);
+    stderr.writeln("shoots : ", shootsForEachPlayer);
     // Write an action using writeln().
     // To debug: stderr.writeln("Debug messages...");
-
+    for (int i = 0; i < n; i++)
+    {
+        evaluateShoots(shootsForEachPlayer[i]);
+    }
+    
     writeln("winner");
+}
+
+void evaluateShoots(string shootsForPlayer)
+{
+    stderr.writeln("shoots : ", shootsForPlayer.split!isWhite);
 }
