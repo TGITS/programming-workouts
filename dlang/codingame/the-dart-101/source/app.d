6bf8b4ee457c2de8353a6b3ebd3133316f7d9903
TGITS <thegeekintheshell@gmail.com>
2019-10-09 14:27:54 +0200

    WIP - Travail sur le puzzle the dart 101 de CodinGame en D

--- dlang/codingame/the-dart-101/source/app.d
+++ dlang/codingame/the-dart-101/source/app.d
@@ -14,6 +14,7 @@ void main()
     stderr.writeln("Number of players : ", n);
     string[] names;
     string[] shootsForEachPlayer;
+    int[] scores;
     for (int i = 0; i < n; i++)
     {
         names ~= readln.strip;
@@ -24,25 +25,76 @@ void main()
     }
 
     stderr.writeln("names : ", names);
-    stderr.writeln("shoots for the ", n ," players : ", shootsForEachPlayer);
+    stderr.writeln("shoots for the ", n, " players : ", shootsForEachPlayer);
     // Write an action using writeln().
     // To debug: stderr.writeln("Debug messages...");
     for (int i = 0; i < n; i++)
     {
-        evaluateShoots(shootsForEachPlayer[i]);
+        scores ~= evaluateShoots(shootsForEachPlayer[i]);
     }
-    
-    writeln("winner");
+
+    const int index_winner = findIndexScoreMax(scores);
+    writeln(names[index_winner]);
 }
 
-void evaluateShoots(string shootsForPlayer)
+int evaluateShoots(string shootsForPlayer)
 {
     stderr.writeln("shoots : ", shootsForPlayer.split!isWhite);
     string[] shoots = shootsForPlayer.split!isWhite;
     int sum = 0;
-    foreach(shoot; shoots) {
-        if(isNumeric(shoot)) {
-            sum += shoot.strip.to!int();
+    int consecutiveMisses = 0;
+    foreach (shoot; shoots)
+    {
+        int shootValue = 0;
+        if (isNumeric(shoot))
+        {
+            shootValue = shoot.strip.to!int();
+        }
+        else if ("X" == shoot.strip)
+        {
+            shootValue -= 20;
+            if (consecutiveMisses == 1)
+            {
+                shootValue -= 10;
+                consecutiveMisses++;
+            }
+            else if (consecutiveMisses == 2)
+            {
+                sum = 0;
+                shootValue = 0;
+                consecutiveMisses = 0;
+            }
+            else
+            {
+                consecutiveMisses++;
+            }
+        } 
+        else 
+        {
+            string[] vals = shoot.split("*");
+            shootValue = vals[0].to!int * vals[1].to!int;
+        }
+        
+        if(sum + shootValue <= 101) {
+            sum += shootValue;
+        }
+
+        if(sum == 101) {
+            break;
         }
     }
+
+    return sum;
+}
+
+int findIndexScoreMax(int[] scores) {
+    int max = scores[0];
+    int max_index = 0;
+    for(int i=1; i < scores.length; i++) {
+        if(max < scores[i]) {
+            max = scores[i];
+            max_index = i;
+        } 
+    }
+    return max_index;
 }
