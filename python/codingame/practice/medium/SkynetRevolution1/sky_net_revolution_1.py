19c4d5b235c8ae13d309b20804cca9b1137586da
TGITS <thegeekintheshell@gmail.com>
2019-10-14 18:34:18 +0200

    WIP - SkyNetRevolution Part 1 en Python

--- python/codingame/practice/medium/SkynetRevolution1/sky_net_revolution_1.py
+++ python/codingame/practice/medium/SkynetRevolution1/sky_net_revolution_1.py
@@ -1,6 +1,17 @@
 import sys
 import math
 
+class Node:
+    def __init__(self, id):
+        self.id = id
+        self.adjacencies = set()
+        self.is_gateway = False
+
+    def addAdjacentNode(self, adjacent_node_index):
+        self.adjacencies.add(adjacent_node_index)
+
+    def markAsGateway(self):
+        self.is_gateway = True
 
 if __name__ == "__main__":
     # Auto-generated code below aims at helping you parse
@@ -9,17 +20,27 @@ if __name__ == "__main__":
     # n: the total number of nodes in the level, including the gateways
     # l: the number of links
     # e: the number of exit gateways
-    n, l, e = [int(i) for i in input().split()]
-    for i in range(l):
+    number_of_nodes, number_of_links, number_of_exits = [int(i) for i in input().split()]
+    nodes = [None] * number_of_nodes
+    for i in range(number_of_links):
         # n1: N1 and N2 defines a link between these nodes
-        n1, n2 = [int(j) for j in input().split()]
-    for i in range(e):
+        index_node_1, index_node_2 = [int(j) for j in input().split()]
+        node_1 = nodes[index_node_1] if nodes[index_node_1] else Node(index_node_1)
+        node_1.addAdjacentNode(index_node_2)
+        nodes[index_node_1] = node_1
+        node_2 = nodes[index_node_2] if nodes[index_node_2] else Node(index_node_2)
+        node_2.addAdjacentNode(index_node_1);
+        nodes[index_node_2] = node_2
+
+    for i in range(number_of_exits):
         ei = int(input())  # the index of a gateway node
+        nodes[ei].markAsGateway()
 
     # game loop
     while True:
-        si = int(input())  # The index of the node on which the Skynet agent is positioned this turn
-
+        skynet_agent_node_index = int(input())  # The index of the node on which the Skynet agent is positioned this turn
+        for node in nodes:
+            print("node " + str(node), file=sys.stderr)
         # Write an action using print
         # To debug: print("Debug messages...", file=sys.stderr)
 
