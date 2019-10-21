f60bf83f959dffc6ae944e8f5275114fd5662aa1
TGITS <thegeekintheshell@gmail.com>
2019-10-21 08:52:49 +0200

    Une solution à SkyNet Revolution P1 de CodinGame en Python.

--- python/codingame/practice/medium/SkynetRevolution1/sky_net_revolution_1.py
+++ python/codingame/practice/medium/SkynetRevolution1/sky_net_revolution_1.py
@@ -1,98 +1,114 @@
 import sys
-import math
+from itertools import tee
 from queue import Queue
 
 
-class Graph:
-    def __init__(self, vertex_list=[]):
-        self.vertex_list = vertex_list
+def pairwise(iterable):
+    """s -> (s0,s1), (s1,s2), (s2, s3), ...
 
-    def _create_node(self, index_node_1, index_node_2):
-        if not self.vertex_list[index_node_1]:
-            self.vertex_list[index_node_1] = Node(index_node_1)
-        self.vertex_list[index_node_1].add_edge(index_node_2)
+    Code taken from https://docs.python.org/3.7/library/itertools.html
+    """
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+
+class Graph:
+    def __init__(self, vertices=[]):
+        self.vertices = vertices
+
+    def _create_vertex_from_index(self, index_vertex_1, index_vertex_2):
+        if not self.vertices[index_vertex_1]:
+            self.vertices[index_vertex_1] = Node(index_vertex_1)
+        self.vertices[index_vertex_1].add_edge(index_vertex_2)
 
     def add_edge(self, v1, v2):
-        self._create_node(v1, v2)
-        self._create_node(v2, v1)
+        self._create_vertex_from_index(v1, v2)
+        self._create_vertex_from_index(v2, v1)
+
+    def severe_edge(self, v1, v2):
+        self.vertices[v1].severe_edge(v2)
+        self.vertices[v2].severe_edge(v1)
 
     def get_adjacent_vertices(self, v):
-        return self.vertex_list[v].get_adjacent_vertices()
+        return self.vertices[v].get_adjacent_vertices()
 
     def get_indegree(self, v):
-        return self.vertex_list[v].get_indegree()
+        return self.vertices[v].get_indegree()
 
     def mark_node_as_gateway(self, v):
-        self.vertex_list[v].mark_as_gateway()
+        self.vertices[v].mark_as_gateway()
 
-    def build_distance_table(self, source_node_index):
+    def build_distance_table(self, source_index):
         distance_table = {}
 
-        for i in range(len(self.vertex_list)):
+        for i in range(len(self.vertices)):
             distance_table[i] = (None, None)
 
-        distance_table[source_node_index] = (0, source_node_index)
+        distance_table[source_index] = (0, source_index)
 
         queue = Queue()
-        queue.put(source_node_index)
+        queue.put(source_index)
 
         while not queue.empty():
-            current_node_index = queue.get()
-            current_distance = distance_table[current_node_index][0]
+            current_vertex_index = queue.get()
+            current_distance = distance_table[current_vertex_index][0]
 
-            for neighbor_index in self.get_adjacent_vertices(current_node_index):
+            for neighbor_index in self.get_adjacent_vertices(current_vertex_index):
                 if distance_table[neighbor_index][0] is None:
                     distance_table[neighbor_index] = (
-                        current_distance + 1, current_node_index)
+                        current_distance + 1, current_vertex_index)
 
                     if len(self.get_adjacent_vertices(neighbor_index)) > 0:
                         queue.put(neighbor_index)
 
         return distance_table
 
-    def compute_shortest_path(self, source_node_index, destination_node_index):
-        distance_table = self.build_distance_table(source_node_index)
+    def compute_shortest_path(self, source_index, destination_index):
+        distance_table = self.build_distance_table(source_index)
 
-        path = [destination_node_index]
+        path = [destination_index]
 
-        previous_vertex = distance_table[destination_node_index][1]
+        previous_vertex = distance_table[destination_index][1]
 
-        while previous_vertex is not None and previous_vertex is not source_node_index:
+        while previous_vertex is not None and previous_vertex is not source_index:
             path.insert(0, previous_vertex)
             previous_vertex = distance_table[previous_vertex][1]
 
         if previous_vertex is None:
             print('No shortest path from {} to {}'.format(
-                source_node_index, destination_node_index), file=sys.stderr)
+                source_index, destination_index), file=sys.stderr)
         else:
-            path.insert(0, source_node_index)
+            path.insert(0, source_index)
             print('Shortest path from {} to {} is : {}'.format(
-                source_node_index, destination_node_index, ' '.join([str(n) for n in path])), file=sys.stderr)
+                source_index, destination_index, ' '.join([str(n) for n in path])), file=sys.stderr)
 
         return path
 
-    def compute_edge_to_severe(self, skynet_agent_node_index):
-        pathes = sorted([self.compute_shortest_path(
-            skynet_agent_node_index, gateway.get_index()) for gateway in self.get_gateways()], key=len)
-        if len(pathes) >= 1:
-            selected_shortest_path = pathes[0]
+    def compute_edge_to_severe(self, skynet_agent_vertex_index):
+        paths = sorted(filter(lambda vertices_list : len(vertices_list) > 1, [self.compute_shortest_path(
+            skynet_agent_vertex_index, gateway.get_index()) for gateway in self.get_gateways()]), key=len)
+        print('Paths : {}'.format(' '.join([str(path) for path in paths])), file=sys.stderr)
+        if len(paths) >= 1:
+            selected_shortest_path = paths[0]
             if len(selected_shortest_path) > 1:
+                self.severe_edge(selected_shortest_path[0], selected_shortest_path[1])
                 return "{} {}".format(selected_shortest_path[0], selected_shortest_path[1])
 
         return " "
 
     def get_gateways(self):
-        return [v for v in self.vertex_list if v.is_gateway]
+        return [v for v in self.vertices if v.is_gateway]
 
     def __str__(self):
         graph_as_string = ""
-        for v in self.vertex_list:
+        for v in self.vertices:
             graph_as_string = graph_as_string + str(v) + "\n"
         return graph_as_string
 
     def __repr__(self):
         graph_as_string = ""
-        for v in self.vertex_list:
+        for v in self.vertices:
             graph_as_string = graph_as_string + str(v) + "\n"
         return graph_as_string
 
@@ -106,8 +122,11 @@ class Node:
     def get_index(self):
         return self.index
 
-    def add_edge(self, adjacent_node_index):
-        self.adjacency_set.add(adjacent_node_index)
+    def add_edge(self, adjacent_vertex_index):
+        self.adjacency_set.add(adjacent_vertex_index)
+
+    def severe_edge(self, adjacent_vertex_index):
+        self.adjacency_set.discard(adjacent_vertex_index)
 
     def mark_as_gateway(self):
         self.is_gateway = True
@@ -119,10 +138,14 @@ class Node:
         return len(self.adjacency_set)
 
     def __str__(self):
-        return 'Node(index={}, adjacency set=[{}], isGateway={})'.format(self.index, ' '.join(map(str, self.adjacency_set)), self.is_gateway)
+        return 'Node(index={}, adjacency set=[{}], isGateway={})'.format(self.index,
+                                                                         ' '.join(map(str, self.adjacency_set)),
+                                                                         self.is_gateway)
 
     def __repr__(self):
-        return 'Node(index={}, adjacency set=[{}], isGateway={})'.format(self.index, ' '.join(map(str, self.adjacency_set)), self.is_gateway)
+        return 'Node(index={}, adjacency set=[{}], isGateway={})'.format(self.index,
+                                                                         ' '.join(map(str, self.adjacency_set)),
+                                                                         self.is_gateway)
 
 
 if __name__ == "__main__":
@@ -150,6 +173,6 @@ if __name__ == "__main__":
     # game loop
     while True:
         # The index of the node on which the Skynet agent is positioned this turn
-        skynet_agent_node_index = int(input())
+        skynet_agent_vertex_index = int(input())
 
-        print(graph.compute_edge_to_severe(skynet_agent_node_index))
+        print(graph.compute_edge_to_severe(skynet_agent_vertex_index))
