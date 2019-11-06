737e36c9f5a7cfe017ce48e4f8880a876744bf1c
cvaudry <Christophe.VAUDRY@ext.ingroupe.com>
2019-11-06 14:45:56 +0100

    Une première solution à l'exercice Matrix de Exercism dans la track Python

--- exercism/python/matrix/matrix.py
+++ exercism/python/matrix/matrix.py
@@ -1,9 +1,18 @@
-class Matrix(object):
+class Matrix:
+
     def __init__(self, matrix_string):
-        pass
+        self._matrix = []
+        rows = matrix_string.split("\n")
+        self._row_size = len(rows)
+        for row_index, row in enumerate(rows):
+            self._matrix.append([])
+            for cell in row.split():
+                self._matrix[row_index].append(int(cell))
 
     def row(self, index):
-        pass
+        internal_index = index - 1
+        return [cell for cell in self._matrix[internal_index]]
 
     def column(self, index):
-        pass
+        internal_index = index - 1
+        return [self._matrix[i][internal_index] for i in range(0, self._row_size)]
