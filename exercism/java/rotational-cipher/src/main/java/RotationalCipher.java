2b1a42ca48a582821824b6e14ab580ff783762b3
TGITS <thegeekintheshell@gmail.com>
2019-11-08 14:15:24 +0100

    Second solution for the exercise Rotational Cipher from Exercism in the track Java.

--- exercism/java/rotational-cipher/src/main/java/RotationalCipher.java
+++ exercism/java/rotational-cipher/src/main/java/RotationalCipher.java
@@ -1,7 +1,6 @@
 class RotationalCipher {
 
     private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
-    private static final String NOT_ROTATED_CHARACTERS = " .,;:!?'0123456789";
     private int shiftKey;
 
     RotationalCipher(int shiftKey) {
@@ -12,26 +11,22 @@ class RotationalCipher {
         StringBuilder rotatedData = new StringBuilder();
         for (int i = 0; i < data.length(); i++) {
             char c = data.charAt(i);
-            if(isNotRotatedCharacter(c)){
-                rotatedData.append(c);
-            } else {
+            if (Character.isLetter(c)) {
                 rotatedData.append(rotateCharacter(c));
+            } else {
+                rotatedData.append(c);
             }
         }
         return rotatedData.toString();
     }
 
-    private boolean isNotRotatedCharacter(char c){
-        return NOT_ROTATED_CHARACTERS.indexOf(c) >= 0;
-    }
-
     private char rotateCharacter(char c) {
         boolean isUpperCase = Character.isUpperCase(c);
-        if(isUpperCase) {
+        if (isUpperCase) {
             c = Character.toLowerCase(c);
         }
         int originalPositionInAlphabet = ALPHABET.indexOf(c);
         int rotatedPosition = (originalPositionInAlphabet + shiftKey) % ALPHABET.length();
-        return isUpperCase ? Character.toUpperCase(ALPHABET.charAt(rotatedPosition)): ALPHABET.charAt(rotatedPosition);
+        return isUpperCase ? Character.toUpperCase(ALPHABET.charAt(rotatedPosition)) : ALPHABET.charAt(rotatedPosition);
     }
 }
