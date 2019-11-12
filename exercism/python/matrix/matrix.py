14ea32b46ad29e842fa7eae44c6aec5073883cdf
TGITS <thegeekintheshell@gmail.com>
2019-11-12 09:57:24 +0100

    A second solution (refactoring of the first one to use list comprehension) to the exercise Matrix form Exercism in the Python track

--- exercism/python/matrix/matrix.py
+++ exercism/python/matrix/matrix.py
@@ -1,18 +1,14 @@
 class Matrix:
 
     def __init__(self, matrix_string):
-        self._matrix = []
         rows = matrix_string.split("\n")
         self._row_size = len(rows)
-        for row_index, row in enumerate(rows):
-            self._matrix.append([])
-            for cell in row.split():
-                self._matrix[row_index].append(int(cell))
+        self._matrix = [[int(cell) for cell in row.split()] for row in rows]
 
     def row(self, index):
         internal_index = index - 1
-        return [cell for cell in self._matrix[internal_index]]
+        return list(self._matrix[internal_index])
 
     def column(self, index):
         internal_index = index - 1
-        return [self._matrix[i][internal_index] for i in range(0, self._row_size)]
+        return [self._matrix[i][internal_index] for i in range(self._row_size)]
