0206fbfdb5267e433c0a40c925ee362097c6c4f3
TGITS <thegeekintheshell@gmail.com>
2019-11-12 14:53:35 +0100

    Une première solution à l'exercice Saddle Points de Exercism dans la Track Java

--- exercism/java/saddle-points/src/main/java/Matrix.java
+++ exercism/java/saddle-points/src/main/java/Matrix.java
@@ -1,13 +1,45 @@
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 class Matrix {
 
+    private int[][] matrix = null;
+    private int[][] transposedMatrix = null;
+    private int numberOfRows;
+    private int numberOfColumns;
+
     Matrix(List<List<Integer>> values) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (!values.isEmpty()) {
+            this.numberOfRows = values.size();
+            this.numberOfColumns = values.get(0).size();
+            matrix = new int[numberOfRows][numberOfColumns];
+            transposedMatrix = new int[numberOfColumns][numberOfRows];
+            for (int i = 0; i < numberOfRows; i++) {
+                for (int j = 0; j < numberOfColumns; j++) {
+                    matrix[i][j] = values.get(i).get(j);
+                    transposedMatrix[j][i] = values.get(i).get(j);
+                }
+            }
+        }
     }
 
     Set<MatrixCoordinate> getSaddlePoints() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (matrix == null) {
+            return Set.of();
+        }
+        Set<MatrixCoordinate> saddlePoints = new HashSet<>();
+        List<Integer> rowMaxList = Arrays.stream(this.matrix).map(array -> Arrays.stream(array).max().getAsInt()).collect(Collectors.toList());
+        List<Integer> rowMinList = Arrays.stream(this.transposedMatrix).map(array -> Arrays.stream(array).min().getAsInt()).collect(Collectors.toList());
+        for (int i = 0; i < numberOfRows; i++) {
+            for (int j = 0; j < numberOfColumns; j++) {
+                if (matrix[i][j] == rowMaxList.get(i) && matrix[i][j] == rowMinList.get(j)) {
+                    saddlePoints.add(new MatrixCoordinate(i + 1, j + 1));
+                }
+            }
+        }
+        return saddlePoints;
     }
 }
