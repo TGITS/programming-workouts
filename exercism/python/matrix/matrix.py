12995f6d242afc26735ee570705305099a5abd5d
cvaudry <Christophe.VAUDRY@ext.ingroupe.com>
2019-11-13 08:59:26 +0100

    A third solution to the exercise Matrix from Exercism in the Python Track.

--- exercism/python/matrix/matrix.py
+++ exercism/python/matrix/matrix.py
@@ -1,14 +1,13 @@
 class Matrix:
 
     def __init__(self, matrix_string):
-        rows = matrix_string.split("\n")
-        self._row_size = len(rows)
+        rows = matrix_string.splitlines()
         self._matrix = [[int(cell) for cell in row.split()] for row in rows]
 
     def row(self, index):
         internal_index = index - 1
-        return list(self._matrix[internal_index])
+        return self._matrix[internal_index]
 
     def column(self, index):
         internal_index = index - 1
-        return [self._matrix[i][internal_index] for i in range(self._row_size)]
+        return [row[internal_index] for row in self._matrix]
