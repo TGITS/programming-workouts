b8d35729dd612e065839287bf442f4f2c1455c63
cvaudry <cvaudry@norsys.fr>
2019-11-24 21:07:11 +0100

    WIP - Exercice two-fer de Exercism dans la track Go

--- exercism/go/two-fer/two_fer.go
+++ exercism/go/two-fer/two_fer.go
@@ -1,15 +1,21 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
 // Package twofer should have a package comment that summarizes what it's about.
 // https://golang.org/doc/effective_go.html#commentary
+// The package twofer is an implementation of "Two-fer"
+// "Two-fer" or "2-fer" is short for two for one. One for you and one for me.
 package twofer
 
-// ShareWith should have a comment documenting it.
+/*
+ShareWith given a string representing a name, return a string with the message:
+
+"One for X, one for me.""
+
+Where X is the given name.
+
+However, if the variable name is missing, this is a null value, an empty string or a blank string, it returns the string:
+
+"One for you, one for me.""
+
+*/
 func ShareWith(name string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
 	return ""
 }
