2e0f11633961ded136deaf23d79f7b0626696af2
cvaudry <cvaudry@norsys.fr>
2019-11-24 15:23:45 +0100

    A refactored version to the exercise Saddle Point of Exercism in the Java Track

--- exercism/java/saddle-points/src/main/java/Matrix.java
+++ exercism/java/saddle-points/src/main/java/Matrix.java
@@ -1,50 +1,53 @@
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 class Matrix {
 
-    private int[][] matrix = null;
-    private int[][] transposedMatrix = null;
-    private int numberOfRows;
-    private int numberOfColumns;
     private Set<MatrixCoordinate> saddlePoints;
 
     Matrix(List<List<Integer>> values) {
-        if (!values.isEmpty()) {
-            this.numberOfRows = values.size();
-            this.numberOfColumns = values.get(0).size();
-            matrix = new int[numberOfRows][numberOfColumns];
-            transposedMatrix = new int[numberOfColumns][numberOfRows];
-            for (int i = 0; i < numberOfRows; i++) {
-                for (int j = 0; j < numberOfColumns; j++) {
-                    matrix[i][j] = values.get(i).get(j);
-                    transposedMatrix[j][i] = values.get(i).get(j);
-                }
-            }
-        }
-        precomputeSaddlePoints();
-    }
-
-    private void precomputeSaddlePoints() {
-        if (matrix == null) {
+        if (values.isEmpty()) {
             this.saddlePoints = Set.of();
         } else {
-            Set<MatrixCoordinate> saddlePoints = new HashSet<>();
-            List<Integer> rowMaxList = Arrays.stream(this.matrix).map(array -> Arrays.stream(array).max().getAsInt()).collect(Collectors.toList());
-            List<Integer> rowMinList = Arrays.stream(this.transposedMatrix).map(array -> Arrays.stream(array).min().getAsInt()).collect(Collectors.toList());
-            for (int i = 0; i < numberOfRows; i++) {
-                for (int j = 0; j < numberOfColumns; j++) {
-                    if (rowMaxList.get(i) == rowMinList.get(j)) {
-                        saddlePoints.add(new MatrixCoordinate(i + 1, j + 1));
-                    }
-                }
-            }
-            this.saddlePoints = saddlePoints;
+            int numberOfRows = values.size();
+            int numberOfColumns = values.get(0).size();
+            int[] rowMax = initializeIntArray(numberOfRows, Integer.MIN_VALUE);
+            int[] columnMin = initializeIntArray(numberOfColumns, Integer.MAX_VALUE);
+            computeRowMaxAndColumnMin(values, rowMax, columnMin, numberOfRows, numberOfColumns);
+            computeNonEmptySaddlePointsSet(rowMax, columnMin, numberOfRows, numberOfColumns);
         }
     }
+
+    private int[] initializeIntArray(int numberOfElements, int initialValue) {
+        int[] intArray = new int[numberOfElements];
+        for (int i = 0; i < numberOfElements; i++) {
+            intArray[i] = initialValue;
+        }
+        return intArray;
+    }
+
+    private void computeRowMaxAndColumnMin(List<List<Integer>> values, int[] rowMax, int[] columnMin, int numberOfRows, int numberOfColumns) {
+        for (int i = 0; i < numberOfRows; i++) {
+            for (int j = 0; j < numberOfColumns; j++) {
+                rowMax[i] = Math.max(rowMax[i], values.get(i).get(j));
+                columnMin[j] = Math.min(columnMin[j], values.get(i).get(j));
+            }
+        }
+    }
+
+    private void computeNonEmptySaddlePointsSet(int[] rowMax, int[] columnMin, int numberOfRows, int numberOfColumns) {
+        Set<MatrixCoordinate> saddlePoints = new HashSet<>();
+        for (int i = 0; i < numberOfRows; i++) {
+            for (int j = 0; j < numberOfColumns; j++) {
+                if (rowMax[i] == columnMin[j]) {
+                    saddlePoints.add(new MatrixCoordinate(i + 1, j + 1));
+                }
+            }
+        }
+        this.saddlePoints = saddlePoints;
+    }
+
 
     Set<MatrixCoordinate> getSaddlePoints() {
         return this.saddlePoints;
