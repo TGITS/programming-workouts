c29915b1aa493b22042cbf2436a84dd82b36a83b
cvaudry <Christophe.VAUDRY@ext.ingroupe.com>
2019-12-02 10:18:06 +0100

    WIP - Première partie de "Run-length encoding" de Exercism dans la track Clojure

--- exercism/clojure/run-length-encoding/src/run_length_encoding.clj
+++ exercism/clojure/run-length-encoding/src/run_length_encoding.clj
@@ -1,29 +1,35 @@
-(ns run-length-encoding)
+(ns run-length-encoding
+  (:require [clojure.string :as string :only join]))
 
 ;; Pensez à faire une version avec reduce
-;; Pensez à changer les noms des fonctions qui sont vraiment pourris
-;; Penser à utilise le threading opératoir plutôt que les maps imbriqués
 ;; Changer les noms des variables c ==> current-char
 ;; Variabiliser (peek (peek acc)) dans un let
-(defn string-to-vector-of-char
-  "transform a string in a vector of vector of characters"
-  [s]
-  (loop [acc [] char-seq s]
-    (let [c (first char-seq)]
-      (if (nil? c) acc
+(defn plain-text-to-vector
+  "transform the plain text in a vector of vector of characters"
+  [plain-text]
+  (loop [acc [] char-seq plain-text]
+    (let [current-char (first char-seq) current-vector (peek acc)]
+      (if (nil? current-char) acc
           (cond
-            (empty? acc) (recur (conj acc (vector c)) (rest char-seq))
-            (= c (peek (peek acc))) (recur (conj (vec (butlast acc)) (conj (peek acc) c)) (rest char-seq))
-            :else (recur (conj acc (vector c)) (rest char-seq)))))))
+            (or (empty? acc) (not (= current-char (peek current-vector)))) (recur (conj acc (vector current-char)) (rest char-seq))
+            (= current-char (peek current-vector)) (recur (conj (vec (butlast acc)) (conj current-vector current-char)) (rest char-seq)))))))
 
-(defn transform-vector-of-char
-  "transform a vector of vector of characters in a vector of 2 dimension vector with the number of character and the associated character"
-  [vector-of-char]
-  (clojure.string/join (map clojure.string/join (map #(vector (let [number-of-chars (count %)] (if (= 1 number-of-chars) "" (str number-of-chars))) (str (peek %))) vector-of-char))))
+(defn vector-to-cipher-text
+  "transform a vector of vector of characters in the run-length encoded string"
+  [char-seq]
+  (->> char-seq
+       (map #(vector (let [number-of-chars (count %)]
+                       (if (= 1 number-of-chars)
+                         "" (str number-of-chars))) (str (peek %))))
+       (map string/join)
+       (string/join)))
 
 (defn run-length-encode
   "encodes a string with run-length-encoding"
-  [plain-text])
+  [plain-text]
+  (->> plain-text
+       (plain-text-to-vector)
+       (vector-to-cipher-text)))
 
 (defn run-length-decode
   "decodes a run-length-encoded string"
