2b26212f083566dcc7d8ac991a5bc4670b3e74f6
cvaudry <cvaudry@norsys.fr>
2019-12-24 19:06:43 +0100

    Une solution à l'exercice binary search de Exercism dans la track Clojure.

--- exercism/clojure/binary-search/src/binary_search.clj
+++ exercism/clojure/binary-search/src/binary_search.clj
@@ -1,9 +1,19 @@
 (ns binary-search)
 
-(defn search-for [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn middle [sequence]
+  (quot (count sequence) 2))
 
-(defn middle [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn index [sequence]
+  (map vector (iterate inc 0) sequence))
+
+(defn search-for [elem sorted-sequence]
+  (let [sequence-of-vector (index sorted-sequence)]
+    (loop [sequence sequence-of-vector]
+      (let [middle-index (middle sequence)
+            middle-vector-value (nth sequence middle-index)
+            middle-value (second middle-vector-value)]
+        (cond
+          (= elem middle-value) (first middle-vector-value)
+          (or (empty? sequence) (= 1 (count sequence))) (throw (Exception. "Value not found"))
+          (< elem middle-value) (recur (take middle-index sequence))
+          (> elem middle-value) (recur (drop middle-index sequence)))))))
