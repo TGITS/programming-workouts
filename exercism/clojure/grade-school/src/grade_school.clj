a43ca2b32966dfabd53aa2e834fa3b3259fc7a92
cvaudry <cvaudry@norsys.fr>
2019-12-25 20:56:03 +0100

    Une solution à l'exercice "Grade School" de Exercism dans la track Clojure

--- exercism/clojure/grade-school/src/grade_school.clj
+++ exercism/clojure/grade-school/src/grade_school.clj
@@ -1,13 +1,10 @@
 (ns grade-school)
 
-(defn grade [school grade]  ;; <- arglist goes here
-    ;; your code goes here
-)
+(defn grade [school grade]
+  (get school grade []))
 
-(defn add [school name grade]  ;; <- arglist goes here
-    ;; your code goes here
-)
+(defn add [school name grade]
+  (update school grade concat (vector name)))
 
-(defn sorted [school]  ;; <- arglist goes here
-    ;; your code goes here
-)
+(defn sorted [school]
+  (let [sorted-school (into (sorted-map) (sort-by key school))] (reduce-kv #(assoc %1 %2 (sort (vec %3))) (empty sorted-school) sorted-school)))
