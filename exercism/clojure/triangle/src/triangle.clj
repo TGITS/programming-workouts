eabb76741c4bd6a8097f56c2d07f160c86b92d31
cvaudry <cvaudry@norsys.fr>
2019-12-25 18:34:38 +0100

    Une première solution à l'exercice Triangle d'Exercism dans la track Clojure

--- exercism/clojure/triangle/src/triangle.clj
+++ exercism/clojure/triangle/src/triangle.clj
@@ -1,5 +1,19 @@
 (ns triangle)
 
-(defn type [] ;; <- arglist goes here
-      ;; your code goes here
-)
+(defn type [side1 side2 side3] 
+  (cond
+    (or 
+     (or (<= (+ side2 side3) side1) 
+         (<= (+ side1 side3) side2) 
+         (<= (+ side1 side2) side3)) 
+     (or (= 0 side1) 
+         (= 0 side2) 
+         (= 0 side3))) :illogical
+    (and (= side1 side2) 
+         (= side2 side3)) :equilateral
+    (or (= side1 side2) 
+        (= side1 side3) 
+        (= side2 side3)) :isosceles
+    :else :scalene
+    )
+  )
