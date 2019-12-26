736afc77a5bc40109ba917f8ec27d7e9f9b05ece
cvaudry <cvaudry@norsys.fr>
2019-12-26 14:00:21 +0100

    Small simplifications on the code for Triangle from Exercism in the Clojure Track

--- exercism/clojure/triangle/src/triangle.clj
+++ exercism/clojure/triangle/src/triangle.clj
@@ -1,19 +1,14 @@
 (ns triangle)
 
-(defn type [side1 side2 side3] 
+(defn type [side1 side2 side3]
   (cond
-    (or 
-     (or (<= (+ side2 side3) side1) 
-         (<= (+ side1 side3) side2) 
-         (<= (+ side1 side2) side3)) 
-     (or (= 0 side1) 
-         (= 0 side2) 
-         (= 0 side3))) :illogical
-    (and (= side1 side2) 
-         (= side2 side3)) :equilateral
-    (or (= side1 side2) 
-        (= side1 side3) 
+    (or
+     (or (<= (+ side2 side3) side1)
+         (<= (+ side1 side3) side2)
+         (<= (+ side1 side2) side3))
+     (= 0 side1 side2 side3)) :illogical
+    (= side1 side2 side3) :equilateral
+    (or (= side1 side2)
+        (= side1 side3)
         (= side2 side3)) :isosceles
-    :else :scalene
-    )
-  )
+    :else :scalene))
