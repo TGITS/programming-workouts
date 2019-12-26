8fb6c277ec1d4e7f0e9078c3914af5dfdf23f00d
cvaudry <cvaudry@norsys.fr>
2019-12-26 21:31:38 +0100

    Refactoring du puzzle "Temperatures" de CodinGame en Clojure

--- clojure/codingame/practice/easy/temperatures/src/temperatures/core.clj
+++ clojure/codingame/practice/easy/temperatures/src/temperatures/core.clj
@@ -9,32 +9,10 @@
            [(first y) (second x)]))
 
 (defn -main [& args]
-  (let [n (read) _ (read-line) temps (read-line)]
-    ; n: the number of temperatures to analyse
-    ; temps: the n temperatures expressed as integers ranging from -273 to 5526
+  (let [_ (read) _ (read-line) temps (read-line)]
     (if (string/blank? temps) (println "0")
-        (let [temp-list (sort (map #(Integer/parseInt %) (clojure.string/split temps #" ")))
-              abs-temp-list (map #(Math/abs %) temp-list)
-              sorted-abs-temp-list (sort abs-temp-list)
-              temp-tuples-list (sort by-first-asc-and-second-desc (zipmap abs-temp-list temp-list))]
-
-          (binding [*out* *err*]
-            (println "n : " (str n)))
-
-          (binding [*out* *err*]
-            (println "temperatures : " temps))
-
-          (binding [*out* *err*]
-            (println "abs-temp-list : " (string/join " " abs-temp-list)))
-
-          (binding [*out* *err*]
-            (println "temp-list : " (string/join " " temp-list)))
-
-          (binding [*out* *err*]
-            (println "temp-tuples-list : " (string/join " " temp-tuples-list)))
-
-          (binding [*out* *err*]
-            (println (str (second (first temp-tuples-list)))))
-
-          (println (str (second (first temp-tuples-list))))))))
+        (let [temperatures (sort (map #(Integer/parseInt %) (clojure.string/split temps #" ")))
+              absolute-temperatures (map #(Math/abs %) temperatures)
+              temperatures-by-pair (sort by-first-asc-and-second-desc (zipmap absolute-temperatures temperatures))]
+          (println (str (second (first temperatures-by-pair))))))))
 
