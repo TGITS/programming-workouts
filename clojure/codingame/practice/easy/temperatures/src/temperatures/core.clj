3681512f168446337f3e36267b0864ef0a0a9cce
cvaudry <cvaudry@norsys.fr>
2019-12-26 21:30:15 +0100

    Refactoring de la solution en Clojure au puzzle "Temperatures" de CodinGame

--- clojure/codingame/practice/easy/temperatures/src/temperatures/core.clj
+++ clojure/codingame/practice/easy/temperatures/src/temperatures/core.clj
@@ -1,44 +1,40 @@
-(require 'clojure.string)
 
 (ns temperatures.core
   (:gen-class))
 
-; Auto-generated code below aims at helping you parse
-; the standard input according to the problem statement.
+(require '[clojure.string :as string])
+
+(defn by-first-asc-and-second-desc [x y]
+  (compare [(first x) (second y)]
+           [(first y) (second x)]))
 
 (defn -main [& args]
   (let [n (read) _ (read-line) temps (read-line)]
     ; n: the number of temperatures to analyse
     ; temps: the n temperatures expressed as integers ranging from -273 to 5526
-    (if (clojure.string/blank? temps) (println "0")
-                  (let [temp-list (sort (map #(Integer/parseInt %) (clojure.string/split temps #" ")))
-                        abs-temp-list (map #(Math/abs %) temp-list)
-                        sorted-abs-temp-list (sort (map #(Math/abs %) temp-list))
-                        temp-tuples-list (sort-by first (map vector abs-temp-list temp-list))
-                        temp-map (into (sorted-map) (zipmap abs-temp-list temp-list))]
+    (if (string/blank? temps) (println "0")
+        (let [temp-list (sort (map #(Integer/parseInt %) (clojure.string/split temps #" ")))
+              abs-temp-list (map #(Math/abs %) temp-list)
+              sorted-abs-temp-list (sort abs-temp-list)
+              temp-tuples-list (sort by-first-asc-and-second-desc (zipmap abs-temp-list temp-list))]
 
-                    ; (binding [*out* *err*]
-                    ;   (println "Debug messages..."))
-                    (binding [*out* *err*]
-                      (println "n : " (str n)))
+          (binding [*out* *err*]
+            (println "n : " (str n)))
 
-                    (binding [*out* *err*]
-                      (println "temperatures : " temps))
+          (binding [*out* *err*]
+            (println "temperatures : " temps))
 
-                    (binding [*out* *err*]
-                      (println "abs-temp-list : " (str abs-temp-list)))
+          (binding [*out* *err*]
+            (println "abs-temp-list : " (string/join " " abs-temp-list)))
 
-                    (binding [*out* *err*]
-                      (println "temp-list : " (str temp-list)))
+          (binding [*out* *err*]
+            (println "temp-list : " (string/join " " temp-list)))
 
-                    (binding [*out* *err*]
-                      (println "temp-tuples-list : " (str (seq temp-tuples-list))))
+          (binding [*out* *err*]
+            (println "temp-tuples-list : " (string/join " " temp-tuples-list)))
 
-                    (binding [*out* *err*]
-                      (println "temp-map : " (str (seq temp-map))))
+          (binding [*out* *err*]
+            (println (str (second (first temp-tuples-list)))))
 
-                    (binding [*out* *err*]
-                      (println (str (nth (first temp-tuples-list) 1))))
-
-                    (println (get temp-map (first sorted-abs-temp-list)))))))
+          (println (str (second (first temp-tuples-list))))))))
 
