b33fbb83b4dabea73b4f9b6647c7849d4ee8cf35
cvaudry <cvaudry@norsys.fr>
2019-12-26 19:33:14 +0100

    Une version refactorée du puzzle "Horse Racing Duals" de CodinGame en Clojure.

--- clojure/codingame/practice/easy/horse-racing-duals/src/horse_racing_duals/core.clj
+++ clojure/codingame/practice/easy/horse-racing-duals/src/horse_racing_duals/core.clj
@@ -2,30 +2,9 @@
   (:gen-class))
 
 (defn -main [& args]
-      (let [N (read) powers (atom (list))]
-           (loop [i N]
-                 (when (> i 0)
-                       (let [Pi (read)]
-                            (reset! powers (cons Pi @powers))
-                            (recur (dec i)))))
-
-           ; (binding [*out* *err*]
-           ;   (println "Debug messages..."))
-           (binding [*out* *err*]
-                    (println (str "powers : " @powers)))
-           ; Write answer to stdout
-           (let [sorted-powers (sort @powers)
-                 reverse-sorted-powers (reverse sorted-powers)
-                 shorten-sorted-powers (rest sorted-powers)
-                 shorten-reverse-sorted-powers (rest reverse-sorted-powers)]
-                (binding [*out* *err*]
-                         (println (str "sorted powers : " sorted-powers)))
-                (binding [*out* *err*]
-                         (println (str "reverse sorted powers : " reverse-sorted-powers)))
-                (binding [*out* *err*]
-                         (println (str "shorten sorted powers : " shorten-sorted-powers)))
-                (binding [*out* *err*]
-                         (println (str "shorten reverse sorted powers : " shorten-reverse-sorted-powers)))
-                (println (str (first (sort (map - shorten-sorted-powers (reverse shorten-reverse-sorted-powers)))))))))
-
-
+  (let [N (read)
+        powers (repeatedly N read)
+        sorted-powers (sort powers)
+        sorted-powers-by-pairs (partition 2 1 sorted-powers)
+        result (str (apply min (map #(- (second %) (first %)) sorted-powers-by-pairs)))]
+    (println result)))
