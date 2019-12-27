73243ae68136483a5285453f6db79d089ec1b9fd
cvaudry <cvaudry@norsys.fr>
2019-12-27 21:06:06 +0100

    Une solution à l'exercice "Robot Name" de Exercism dans la track Clojure

--- exercism/clojure/robot-name/src/robot_name.clj
+++ exercism/clojure/robot-name/src/robot_name.clj
@@ -1,13 +1,32 @@
-(ns robot-name)
+(ns robot-name
+  (:require [clojure.string :as str]))
 
-(defn robot [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(def existing-robot-names (atom #{}))
 
-(defn robot-name [robot] ;; <- arglist goes here
-      ;; your code goes here
-)
+(defrecord Robot [name])
 
-(defn reset-name [robot] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn generate-random-string [size alphabet]
+  (str/join (repeatedly size #(rand-nth (shuffle (random-sample 0.5  alphabet))))))
+
+(defn generate-random-int-as-string [size int-max]
+  (str/join (repeatedly size #(str (rand-int int-max)))))
+
+(defn generate-random-robot-name [prefix-size suffix-size]
+  (str
+   (generate-random-string prefix-size "ABCDEFGHIJKLMNOPKRSTUVWXYZ")
+   (generate-random-int-as-string suffix-size 10)))
+
+(defn generate-robot-name []
+  (loop [robot-name (generate-random-robot-name 2 3)]
+    (if (contains? (deref existing-robot-names) robot-name)
+      (recur (generate-random-robot-name 2 3))
+      (do (swap! existing-robot-names conj robot-name) robot-name))))
+
+(defn robot []
+  (Robot.  (atom (generate-robot-name))))
+
+(defn robot-name [robot]
+  (deref (:name robot)))
+
+(defn reset-name [robot]
+  (reset! (:name robot) (generate-robot-name)))
