efa52966fe794b46c364c9923f8dbb07c474977d
cvaudry <cvaudry@norsys.fr>
2019-12-28 13:52:07 +0100

    A second solution to the exercise "Word Count" from Exercism in the Clojure Track

--- exercism/clojure/word-count/src/word_count.clj
+++ exercism/clojure/word-count/src/word_count.clj
@@ -6,8 +6,7 @@
    A word here is an alphanumeric sequence of 1 character or more. All non-alphanumeric characters are ignored and the case of each word is normalized to lowercase.
    For example 'word' 'WORD' 'WorD' 'word!,' are considered the same word occurence."
   [s]
-  (as-> s input
-    (str/replace input #"\W+" " ")
-    (str/split  input #"\s")
-    (map str/lower-case input)
-    (frequencies input)))
+  (->> s
+       (re-seq #"\w+")
+       (map str/lower-case)
+       (frequencies)))
