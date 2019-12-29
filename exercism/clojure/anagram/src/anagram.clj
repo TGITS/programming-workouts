d599229ed36506858deecab3f4d0a4194f6db917
cvaudry <cvaudry@norsys.fr>
2019-12-29 20:56:03 +0100

    Une première solution à l'exercice "Anagram" de Exercism dans la track Clojure

--- exercism/clojure/anagram/src/anagram.clj
+++ exercism/clojure/anagram/src/anagram.clj
@@ -1,5 +1,18 @@
-(ns anagram)
+(ns anagram
+  (:require [clojure.string :as str]))
 
-(defn anagrams-for [word prospect-list] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn is-anagram
+  "Test if the given argument 'candidate-anagram' is an anagram of the given argument 'word'. The function is case-insensitive. 
+   Furthermore a word is not considered as its own anagram : if the 2 parameters are the same word (potentially with different case), the function returns false"
+  [word candidate-anagram]
+  (let [to-lower-case (str/lower-case word)
+        as-sorted-char-seq (sort to-lower-case)
+        prospect-to-lower-case (str/lower-case candidate-anagram)]
+    (and (not= to-lower-case prospect-to-lower-case) ;; A word is not its own anagrame
+         (= as-sorted-char-seq (sort prospect-to-lower-case))))) ;; if the 2 words are anagrams, they have the same sorted sequence of characters
+
+(defn anagrams-for
+  "Returns the sequence of anagrams of the given argument 'word' that are in the 'prospect_list' argument. The function is case-insensitive. 
+   Furthermore a word is not considered as its own anagram."
+  [word prospect-list]
+  (filter #(is-anagram word %1) prospect-list))
