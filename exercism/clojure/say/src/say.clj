16550e69842403d97c951ae7f7634e6902688a9a
cvaudry <cvaudry@norsys.fr>
2020-01-01 15:04:59 +0100

    Une version refactorisée de l'exercice 'say' de Exercism dans la track Clojure. Solution pas encore soumise et toujours au statut 'WIP'

--- exercism/clojure/say/src/say.clj
+++ exercism/clojure/say/src/say.clj
@@ -1,16 +1,16 @@
-(ns say)
+(ns say
+  (:require [clojure.string :as str]))
 
-(declare number)
-
-(def MINIMUM-VALUE 0)
 (def MAXIMUM-VALUE 999999999999)
-(def DECADE 10)
 (def HUNDRED 100)
 (def THOUSAND 1000)
 (def MILLION 1000000)
 (def BILLION 1000000000)
 
-(defn- units [num]
+(defn units
+  "Spell out in English the number given in arguments if it is a digit (a 'unit' number, this is a number between 0 and 9 inclusive).
+   It throws an IllegalArgumentException otherwise"
+  [num]
   (condp = num
     0 "zero"
     1 "one"
@@ -23,7 +23,10 @@
     8 "eight"
     9 "nine"))
 
-(defn- decades-strictly-below-twenty [num]
+(defn decades-strictly-below-twenty
+  "Spell out in English the number given in arguments if it is a positive number less or equal to 19.
+   It throws an IllegalArgumentException otherwise."
+  [num]
   (condp = num
     10 "ten"
     11 "eleven"
@@ -34,48 +37,58 @@
     16 "sixteen"
     17 "seventeen"
     18 "eighteen"
-    19 "nineteen"))
+    19 "nineteen"
+    (units num)))
 
-(defn- unit-part-of-decade [num]
+(defn- units-part-of-a-decade [num]
   (let [num num] (if (> num 0) (str "-" (units num)) "")))
 
-(defn- decades-above-twenty [num]
+(defn decades-and-units
+  "Spell out in English the number given in arguments if it is a positive number less or equal to 99.
+   It throws an IllegalArgumentException otherwise."
+  [num]
   (cond
-    (>= 99 num 90) (str "ninety" (unit-part-of-decade (- num 90)))
-    (>= 89 num 80) (str "eighty" (unit-part-of-decade (- num 80)))
-    (>= 79 num 70) (str "seventy" (unit-part-of-decade (- num 70)))
-    (>= 69 num 60) (str "sixty" (unit-part-of-decade (- num 60)))
-    (>= 59 num 50) (str "fifty" (unit-part-of-decade (- num 50)))
-    (>= 49 num 40) (str "forty" (unit-part-of-decade (- num 40)))
-    (>= 39 num 30) (str "thirty" (unit-part-of-decade (- num 30)))
-    (>= 29 num 20) (str "twenty" (unit-part-of-decade (- num 20)))))
+    (>= 99 num 90) (str "ninety" (units-part-of-a-decade (- num 90)))
+    (>= 89 num 80) (str "eighty" (units-part-of-a-decade (- num 80)))
+    (>= 79 num 70) (str "seventy" (units-part-of-a-decade (- num 70)))
+    (>= 69 num 60) (str "sixty" (units-part-of-a-decade (- num 60)))
+    (>= 59 num 50) (str "fifty" (units-part-of-a-decade (- num 50)))
+    (>= 49 num 40) (str "forty" (units-part-of-a-decade (- num 40)))
+    (>= 39 num 30) (str "thirty" (units-part-of-a-decade (- num 30)))
+    (>= 29 num 20) (str "twenty" (units-part-of-a-decade (- num 20)))
+    :else (decades-strictly-below-twenty num)))
 
-(defn- decades [num]
-  (if (<= 10 num 19)
-    (decades-strictly-below-twenty num)
-    (decades-above-twenty num)))
-
-(defn- hundreds [num]
+(defn hundreds
+  "Spell out in English the number given in arguments if it is a positive number less or equal to 999.
+   It throws an IllegalArgumentException otherwise"
+  [num]
   (let [number-of-hundreds (quot num HUNDRED) decades-remainder (rem num HUNDRED)]
-    (str (units number-of-hundreds) " hundred" (if (not= 0 decades-remainder) (str " " (decades decades-remainder)) ""))))
+    (str/trim (str (if (not= 0 number-of-hundreds) (str (units number-of-hundreds) " hundred") "") (if (not= 0 decades-remainder) (str " " (decades-and-units decades-remainder)) "")))))
 
-(defn- thousands [num]
+(defn thousands
+  "Spell out in English the number given in arguments if it is a positive number less or equal to 999999.
+   It throws an IllegalArgumentException otherwise."
+  [num]
   (let [number-of-thousands (quot num THOUSAND) thousands-remainder (rem num THOUSAND)]
-    (str (number number-of-thousands) " thousand" (if (not= 0 thousands-remainder) (str " " (hundreds thousands-remainder)) ""))))
+    (str/trim (str (if (not= 0 number-of-thousands) (str (hundreds number-of-thousands) " thousand") "") (if (not= 0 thousands-remainder) (str " " (hundreds thousands-remainder)) "")))))
 
-(defn- millions [num]
+(defn millions
+  "Spell out in English the number given in arguments if it is a positive number less or equal to 999999999.
+   It throws an IllegalArgumentException otherwise."
+  [num]
   (let [number-of-millions (quot num MILLION) millions-remainder (rem num MILLION)]
-    (str (number number-of-millions) " million" (if (not= 0 millions-remainder) (str " " (thousands millions-remainder)) ""))))
+    (str/trim (str (if (not= 0 number-of-millions) (str (hundreds number-of-millions) " million") "") (if (not= 0 millions-remainder) (str " " (thousands millions-remainder)) "")))))
 
-(defn- billions [num]
+(defn billions
+  "Spell out in English the number given in arguments if it is a positive number less or equal to 999999999999.
+   It throws an IllegalArgumentException otherwise."
+  [num]
   (let [number-of-billions (quot num BILLION) billions-remainder (rem num BILLION)]
-    (str (number number-of-billions) " billion" (if (not= 0 billions-remainder) (str " " (millions billions-remainder)) ""))))
+    (str/trim (str (if (not= 0 number-of-billions) (str (hundreds number-of-billions) " billion") "") (if (not= 0 billions-remainder) (str " " (millions billions-remainder)) "")))))
 
-(defn number [num]
-  (cond (or (< num MINIMUM-VALUE) (> num MAXIMUM-VALUE)) (throw (IllegalArgumentException. "Value out of range"))
-        (<= MINIMUM-VALUE num 9)  (units num)
-        (<= DECADE num 99)  (decades num)
-        (<= HUNDRED num 999) (hundreds num)
-        (<= THOUSAND num 999999) (thousands num)
-        (<= MILLION num 999999999) (millions num)
-        (<= BILLION num MAXIMUM-VALUE) (billions num)))
+(defn number 
+  [num]
+  (cond
+    (= 0 num) (units num)
+    (< 0 num MAXIMUM-VALUE) (billions num)
+    :else (throw (IllegalArgumentException.))))
--- exercism/clojure/say/test/say_test.clj
+++ exercism/clojure/say/test/say_test.clj
@@ -2,6 +2,129 @@
   (:require [clojure.test :refer [deftest is]]
             say))
 
+(deftest below-zero-is-out-of-range-for-units-test
+  (is (thrown? IllegalArgumentException (say/units -1))))
+
+(deftest zero-for-units-test
+  (is (= "zero" (say/units 0))))
+
+(deftest one-for-units-test
+  (is (= "one" (say/units 1))))
+
+(deftest two-for-units-test
+  (is (= "two" (say/units 2))))
+
+(deftest three-for-units-test
+  (is (= "three" (say/units 3))))
+
+(deftest four-for-units-test
+  (is (= "four" (say/units 4))))
+
+(deftest five-for-units-test
+  (is (= "five" (say/units 5))))
+
+(deftest six-for-units-test
+  (is (= "six" (say/units 6))))
+
+(deftest seven-for-units-test
+  (is (= "seven" (say/units 7))))
+
+(deftest eight-for-units-test
+  (is (= "eight" (say/units 8))))
+
+(deftest nine-for-units-test
+  (is (= "nine" (say/units 9))))
+
+(deftest above-nine-is-out-of-range-for-units-test-1
+  (is (thrown? IllegalArgumentException (say/units 10))))
+
+(deftest above-nine-is-out-of-range-for-units-test-2
+  (is (thrown? IllegalArgumentException (say/units 154))))
+
+(deftest ten-for-decades-strictly-below-twenty-test
+  (is (= "ten" (say/decades-strictly-below-twenty 10))))
+
+(deftest eleven-for-decades-strictly-below-twenty-test
+  (is (= "eleven" (say/decades-strictly-below-twenty 11))))
+
+(deftest twelve-for-decades-strictly-below-twenty-test
+  (is (= "twelve" (say/decades-strictly-below-twenty 12))))
+
+(deftest thirteen-for-decades-strictly-below-twenty-test
+  (is (= "thirteen" (say/decades-strictly-below-twenty 13))))
+
+(deftest fourteen-for-decades-strictly-below-twenty-test
+  (is (= "fourteen" (say/decades-strictly-below-twenty 14))))
+
+(deftest fifteen-for-decades-strictly-below-twenty-test
+  (is (= "fifteen" (say/decades-strictly-below-twenty 15))))
+
+(deftest sixteen-for-decades-strictly-below-twenty-test
+  (is (= "sixteen" (say/decades-strictly-below-twenty 16))))
+
+(deftest seventeen-for-decades-strictly-below-twenty-test
+  (is (= "seventeen" (say/decades-strictly-below-twenty 17))))
+
+(deftest eighteen-for-decades-strictly-below-twenty-test
+  (is (= "eighteen" (say/decades-strictly-below-twenty 18))))
+
+(deftest nineteen-for-decades-strictly-below-twenty-test
+  (is (= "nineteen" (say/decades-strictly-below-twenty 19))))
+
+(deftest above-nineteen-for-decades-strictly-below-twenty-test-1
+  (is (thrown? IllegalArgumentException (say/decades-strictly-below-twenty 20))))
+
+(deftest above-nineteen-for-decades-strictly-below-twenty-test-2
+  (is (thrown? IllegalArgumentException (say/decades-strictly-below-twenty 123456789))))
+
+(deftest eight-for-decades-and-units-test
+  (is (= "eight" (say/decades-and-units 8))))
+
+(deftest nine-for-decades-and-units-test
+  (is (= "nine" (say/decades-and-units 9))))
+
+(deftest ten-for-decades-and-units-test
+  (is (= "ten" (say/decades-and-units 10))))
+
+(deftest eleven-for-decades-and-units-test
+  (is (= "eleven" (say/decades-and-units 11))))
+
+(deftest twelve-for-decades-and-units-test
+  (is (= "twelve" (say/decades-and-units 12))))
+
+(deftest thirteen-for-decades-and-units-test
+  (is (= "thirteen" (say/decades-and-units 13))))
+
+(deftest fourteen-for-decades-and-units-test
+  (is (= "fourteen" (say/decades-and-units 14))))
+
+(deftest fifteen-for-decades-and-units-test
+  (is (= "fifteen" (say/decades-and-units 15))))
+
+(deftest sixteen-for-decades-and-units-test
+  (is (= "sixteen" (say/decades-and-units 16))))
+
+(deftest seventeen-for-decades-and-units-test
+  (is (= "seventeen" (say/decades-and-units 17))))
+
+(deftest eighteen-for-decades-and-units-test
+  (is (= "eighteen" (say/decades-and-units 18))))
+
+(deftest nineteen-for-decades-and-units-test
+  (is (= "nineteen" (say/decades-and-units 19))))
+
+(deftest twenty-for-decades-and-units-test
+  (is (= "twenty" (say/decades-and-units 20))))
+
+(deftest twenty-one-for-decades-and-units-test
+  (is (= "twenty-one" (say/decades-and-units 21))))
+
+(deftest thirty-for-decades-and-units-test
+  (is (= "thirty" (say/decades-and-units 30))))
+
+(deftest forty-for-decades-and-units-test
+  (is (= "forty" (say/decades-and-units 40))))
+
 (deftest zero-test
   (is (= "zero" (say/number 0))))
 
