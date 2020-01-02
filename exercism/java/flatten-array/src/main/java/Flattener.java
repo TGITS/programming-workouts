ef7c3869ea2f60d039b6b7e2f90ec90d6336ff0c
cvaudry <cvaudry@norsys.fr>
2020-01-02 18:55:50 +0100

    feature : exercism/java/flatten
    Une première solution avec soumission à l'exercice 'flatten' de Exercism dans la track Java

--- exercism/java/flatten-array/src/main/java/Flattener.java
+++ exercism/java/flatten-array/src/main/java/Flattener.java
@@ -3,29 +3,12 @@ import java.util.List;
 
 public class Flattener {
 
-    public List flatten(List list) {
-        //return recursiveFlatten(list, new ArrayList());
-        return iterativeFlatten(list);
-    }
-
-    private List recursiveFlatten(List list, List flattenList) {
-        if(list == null || list.isEmpty()) {
-            return flattenList;
-        }
-        var elem = list.get(0);
-        if(elem instanceof List) {
-            flattenList.addAll(recursiveFlatten((List) elem, new ArrayList<>()));
-        } else if (elem != null){
-            flattenList.add(elem);
-        }
-        return recursiveFlatten(list.subList(1, list.size()),flattenList);
-    }
-
-    private List iterativeFlatten(List list){
-        List flattenList = new ArrayList();
+    @SuppressWarnings("unchecked")
+    public List<Object> flatten(List<Object> list) {
+        List<Object> flattenList = new ArrayList<>();
         for(var elem:list) {
             if(elem instanceof List) {
-                flattenList.addAll(iterativeFlatten((List) elem));
+                flattenList.addAll(flatten((List<Object>) elem));
             } else if (elem != null){
                 flattenList.add(elem);
             }
