564df78a1c9516cf5c24f287de98351550fe3af1
cvaudry <cvaudry@norsys.fr>
2020-01-22 21:09:45 +0100

    Initialisation du puzzle unit_fraction de CG en clojure

---
+++ clojure/codingame/practice/easy/unit_fraction/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to unit_fraction
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ clojure/codingame/practice/easy/unit_fraction/test/unit_fraction/core_test.clj
@@ -0,0 +1,7 @@
+(ns unit-fraction.core-test
+  (:require [clojure.test :refer :all]
+            [unit-fraction.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
