b28d31fb685f5086b44b888a63c08455fe9af1f5
cvaudry <cvaudry@norsys.fr>
2020-02-07 21:24:29 +0100

    Nouvelle version de l'exercice accumulate de Exercism dans la track Exercism

--- exercism/typescript/accumulate/accumulate.ts
+++ exercism/typescript/accumulate/accumulate.ts
@@ -1,7 +1,7 @@
 export function accumulate<T,S>(array: Array<T>, accumulator: (x: T) => S) {
-    let mappedArray: Array<S> = new Array<S>(array.length);
-    for (let i = 0; i < array.length; i++) {
-        mappedArray[i] = accumulator(array[i])
+    let mappedArray: Array<S> = new Array<S>();
+    for (let i of array) {
+        mappedArray.push(accumulator(i))
     }
     return mappedArray;
 }
