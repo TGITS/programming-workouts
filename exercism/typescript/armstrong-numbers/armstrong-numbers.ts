f3e977bd17b1a777bf1e225620c97742581424d1
VAUDRY Christophe <christophe.vaudry@ext.imprimerienationale.fr>
2020-02-07 12:49:18 +0100

    Une solution à l'exercice Armstrong Number d'Exercism dans la track Typescript

--- exercism/typescript/armstrong-numbers/armstrong-numbers.ts
+++ exercism/typescript/armstrong-numbers/armstrong-numbers.ts
@@ -1,11 +1,7 @@
 export class ArmstrongNumbers {
     public static isArmstrongNumber = (num: number): boolean => {
-        const value:string = String(num);
-        const numberOfDigits:number = value.length;
-        
-        if (num > 0 && num < 10) {
-            return true;
-        }
-        return false
+        const numInString:string = String(num);
+        const numberOfDigits:number = numInString.length;
+        return num === Array.from(numInString).map(Number).reduce((accumulator, currentValue) => accumulator + currentValue ** numberOfDigits, 0);
     }
 }
