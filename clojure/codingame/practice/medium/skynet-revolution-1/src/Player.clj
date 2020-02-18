73dc05a5e55da14ca3b5cdcc1ec52aeeaca06e30
VAUDRY Christophe <christophe.vaudry@ext.imprimerienationale.fr>
2020-02-18 17:44:12 +0100

    WIP - Travail sur une version en Clojure de SkyNet P1 de CG

--- clojure/codingame/practice/medium/skynet-revolution-1/src/Player.clj
+++ clojure/codingame/practice/medium/skynet-revolution-1/src/Player.clj
@@ -7,23 +7,25 @@
 (defn output [msg] (println msg) (flush))
 (defn debug [msg] (binding [*out* *err*] (println msg) (flush)))
 
-(defn -main [& args]
-  (let [N (read) L (read) E (read)]
-    ; N: the total number of nodes in the level, including the gateways
-    ; L: the number of links
-    ; E: the number of exit gateways
-    (loop [i L]
-      (when (> i 0)
-        (let [N1 (read) N2 (read)]
-          ; N1: N1 and N2 defines a link between these nodes
-          (recur (dec i)))))
-    (loop [i E]
-      (when (> i 0)
-        (let [EI (read)]
+(defn -main [& _]
+  (let [number-of-nodes (read)
+        number-of-links (read)
+        number-of-exits (read)
+    ; number-of-nodes: the total number of nodes in the level, including the gateways
+    ; number-of-links: the number of links
+    ; number-of-exits: the number of exit gateways
+        graph (loop [i number-of-links map-of-nodes {}]
+                (if (> i 0)
+                  (let [n1 (read) n2 (read)]
+          ; n1: n1 and n2 defines a link between these nodes
+                    (recur (dec i) (merge-with {n1  n2, n2 n1} map-of-nodes))) (map-of-nodes)))
+        index-of-exits (loop [i number-of-exits exits #{}]
+                         (if (> i 0)
+                           (let [exit-index (read)]
           ; EI: the index of a gateway node
-          (recur (dec i)))))
+                             (recur (dec i) (conj exits exit-index))) exits))]
     (while true
-      (let [SI (read)]
+      (let [skynet-index (read)]
         ; SI: The index of the node on which the Skynet agent is positioned this turn
 
         ; (debug "Debug messages...")
