02b6fa288cda7533ca88ad8a4ec1e1f8c1a3a40d
cvaudry <cvaudry@norsys.fr>
2020-03-31 21:53:13 +0200

    WIP - Exercice Spage Age de Exercism dans la Track Python

--- exercism/python/space-age/space_age.py
+++ exercism/python/space-age/space_age.py
@@ -1,3 +1,14 @@
 class SpaceAge(object):
     def __init__(self, seconds):
-        pass
+        self._earth_orbital_period = 31557600
+        self._mercury_orbital_period = 0.2408467 * self._earth_orbital_period
+        self._seconds = seconds
+
+    def do_computation(self, orbital_period):
+        return round(self._seconds / orbital_period, 2)
+
+    def on_earth(self):
+        return self.do_computation(self._earth_orbital_period)
+
+    def on_mercury(self):
+        return self.do_computation(self._mercury_orbital_period)
