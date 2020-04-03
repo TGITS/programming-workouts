521c0aaffdc76a275325bbd6fe883ca80832967c
cvaudry <cvaudry@norsys.fr>
2020-04-03 21:52:27 +0200

    Une solution à l'exercice DND Character de Exercism dans la track Python

--- exercism/python/dnd-character/dnd_character.py
+++ exercism/python/dnd-character/dnd_character.py
@@ -1,3 +1,21 @@
+from random import randint
+from math import floor
+
+
+def modifier(value):
+    return int(floor((value - 10) / 2))
+
+
 class Character:
+
     def __init__(self):
-        pass
+        self.strength = self.ability()
+        self.dexterity = self.ability()
+        self.constitution = self.ability()
+        self.intelligence = self.ability()
+        self.wisdom = self.ability()
+        self.charisma = self.ability()
+        self.hitpoints = 10 + modifier(self.constitution)
+
+    def ability(self):
+        return sum(sorted([randint(1, 6) for i in range(4)])[1:])
