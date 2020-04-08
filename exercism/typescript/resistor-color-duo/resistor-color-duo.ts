aa3eaf27c4b80221f95f3b5ead1202ab9d183ba2
cvaudry <cvaudry@norsys.fr>
2020-04-08 21:37:30 +0200

    WIP : nouvelle version de l'exercice resistor-color-duo de Exercism dans la track Typescript

--- exercism/typescript/resistor-color-duo/resistor-color-duo.ts
+++ exercism/typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,14 +1,14 @@
-const colorAndAssociatedValue: { [index: string]: string } = {
-  "black": "0",
-  "brown": "1",
-  "red": "2",
-  "orange": "3",
-  "yellow": "4",
-  "green": "5",
-  "blue": "6",
-  "violet": "7",
-  "grey": "8",
-  "white": "9"
+enum Color {
+  black = 0,
+  brown = 1,
+  red = 2,
+  orange = 3,
+  yellow = 4,
+  green = 5,
+  blue = 6,
+  violet = 7,
+  grey = 8,
+  white = 9
 }
 
 export class ResistorColor {
@@ -21,6 +21,6 @@ export class ResistorColor {
     this.colors = colors;
   }
 
-  colors2Values = (): string => this.colors.slice(0, 2).map(s => colorAndAssociatedValue[s]).join("")
+  colors2Values = (): string => this.colors.slice(0, 2).map((s: any) => Color[s]).join("")
   value = (): number => Number(this.colors2Values())
 }
