473978c7dc7a08beca7848f87f22165f1e0bcb04
cvaudry <cvaudry@norsys.fr>
2020-04-19 16:02:52 +0200

    Une nouvelle solution utilisant streams et reduce pour l'exercice "Flatten Array" de exercism dans la track Java

--- exercism/java/flatten-array/src/main/java/Flattener.java
+++ exercism/java/flatten-array/src/main/java/Flattener.java
@@ -1,18 +1,24 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Flattener {
 
     @SuppressWarnings("unchecked")
     public List<Object> flatten(List<Object> list) {
-        List<Object> flattenList = new ArrayList<>();
-        for(var elem:list) {
-            if(elem instanceof List) {
-                flattenList.addAll(flatten((List<Object>) elem));
-            } else if (elem != null){
-                flattenList.add(elem);
-            }
-        }
-        return flattenList;
+        return (List<Object>) list.stream()
+                .filter(Objects::nonNull)
+                .reduce(new ArrayList<>(), (previousAccumulatedElem, currentElem) -> accumulate((List<Object>) previousAccumulatedElem, currentElem));
     }
+
+    @SuppressWarnings("unchecked")
+    private List<Object> accumulate(List<Object> previousAccumulatedElem, Object currentElem) {
+        if (currentElem instanceof List) {
+            previousAccumulatedElem.addAll(flatten((List<Object>) currentElem));
+        } else {
+            previousAccumulatedElem.add(currentElem);
+        }
+        return previousAccumulatedElem;
+    }
+
 }
