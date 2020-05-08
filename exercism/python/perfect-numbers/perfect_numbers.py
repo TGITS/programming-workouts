8ed0aef850fbb725e6a39e5012aab896fb1fcb12
cvaudry <cvaudry@norsys.fr>
2020-05-08 17:59:49 +0200

    Une solution à l'exercice perfect numbers de Exercism dans la track Python

--- exercism/python/perfect-numbers/perfect_numbers.py
+++ exercism/python/perfect-numbers/perfect_numbers.py
@@ -1,2 +1,23 @@
 def classify(number):
-    pass
+    if (number <= 0):
+        raise ValueError("Numbers less or equal to zero are not allowed")
+    elif is_abundant(number):
+        return "abundant"
+    elif is_deficient(number):
+        return "deficient"
+    elif is_perfect(number):
+        return "perfect"
+    else:
+       raise AssertionError("Value not allowed")
+
+def is_perfect(number):
+    return compute_divisors_sum(number) == number
+
+def is_abundant(number):
+    return compute_divisors_sum(number) > number
+
+def is_deficient(number):
+    return compute_divisors_sum(number) < number
+
+def compute_divisors_sum(number):
+    return sum([i for i in range(1,number) if number % i == 0])
