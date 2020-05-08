d48a9829153c3550ba77382ce413a3af09b95dc0
cvaudry <cvaudry@norsys.fr>
2020-05-08 21:33:57 +0200

    MAJ du formatage de l'exercism isogram de Exercism dans la track Python Une solution à l'exercice resistor-color-duo de Exercism dans la track Python

--- exercism/python/isogram/isogram.py
+++ exercism/python/isogram/isogram.py
@@ -2,5 +2,5 @@ def is_isogram(string):
     """
         Test whether the given string parameter is an isogram or not
     """
-    processed_input = string.lower().replace("-","").replace(" ", "")
+    processed_input = string.lower().replace("-", "").replace(" ", "")
     return len(set(processed_input)) == len(processed_input)
--- exercism/python/resistor-color-duo/resistor_color_duo.py
+++ exercism/python/resistor-color-duo/resistor_color_duo.py
@@ -1,2 +1,6 @@
 def value(colors):
-    pass
+    return color_code(colors[0])*10 + color_code(colors[1])
+
+
+def color_code(color):
+    return ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'].index(color)
