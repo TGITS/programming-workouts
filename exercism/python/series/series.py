bebfed3d1e8de5d04bdaa2e5c6f4ef1194cdd0c3
cvaudry <cvaudry@norsys.fr>
2020-05-09 19:45:13 +0200

    Une solution à l'exercice series de Exercism dans la track Python

--- exercism/python/series/series.py
+++ exercism/python/series/series.py
@@ -1,2 +1,6 @@
 def slices(series, length):
-    pass
+    series_size = len(series)
+    if length <= 0 or length > series_size:
+        raise ValueError("Given length is too large")
+
+    return [series[i:i+length] for i in range(0, series_size-length+1)]
