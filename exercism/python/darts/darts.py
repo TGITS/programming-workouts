7b98f9898622799731e863ccb136156d659bffe4
cvaudry <cvaudry@norsys.fr>
2020-05-09 17:15:05 +0200

    Une solution à l'exercice darts de Exercism dans la track Python

--- exercism/python/darts/darts.py
+++ exercism/python/darts/darts.py
@@ -1,2 +1,16 @@
+from math import sqrt
+
+
 def score(x, y):
-    pass
+    if distance_from_origin(x, y) > 10:
+        return 0
+    elif distance_from_origin(x, y) > 5:
+        return 1
+    elif distance_from_origin(x, y) > 1:
+        return 5
+    else:
+        return 10
+
+
+def distance_from_origin(x, y):
+    return sqrt(x**2 + y**2)
