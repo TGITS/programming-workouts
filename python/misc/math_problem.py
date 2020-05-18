b00029d97750b7dfcf01af89969fe1140298e6f1
VAUDRY Christophe <christophe.vaudry@ext.imprimerienationale.fr>
2020-05-18 18:11:04 +0200

    Petit problème de math résolu par programmation

---
+++ python/misc/math_problem.py
@@ -0,0 +1,3 @@
+
+print([n for n in range(0,101) if n%2 == 1 and n%3 == 2 and n%4 == 3 and n%5 == 4 and n%6 == 5 ])
+print([n for n in range(100,300) if n%2 == 1 and n%3 == 2 and n%4 == 3 and n%5 == 4 and n%6 == 5 and n%7 == 1])
