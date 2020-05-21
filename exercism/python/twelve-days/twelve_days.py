1aeccef45bd766b07418a05a1675eca02f9bd57b
VAUDRY Christophe <christophe.vaudry@ext.imprimerienationale.fr>
2020-05-21 16:19:42 +0200

    A first solution to the twelve days of Christmas from Exercism in the Python track

--- exercism/python/twelve-days/twelve_days.py
+++ exercism/python/twelve-days/twelve_days.py
@@ -1,2 +1,21 @@
 def recite(start_verse, end_verse):
-    pass
+    """ Function that gives as a list the verses from the twelve days of Christmas between start_verse and end_verse """
+    verse_start = "On the {} day of Christmas my true love gave to me: "
+    days = ["first", "second", "third", "fourth", "fifth", "sixth",
+            "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"]
+    gifts = [
+        "a Partridge in a Pear Tree.",
+        "two Turtle Doves, and ",
+        "three French Hens, ",
+        "four Calling Birds, ",
+        "five Gold Rings, ",
+        "six Geese-a-Laying, ",
+        "seven Swans-a-Swimming, ",
+        "eight Maids-a-Milking, ",
+        "nine Ladies Dancing, ",
+        "ten Lords-a-Leaping, ",
+        "eleven Pipers Piping, ",
+        "twelve Drummers Drumming, "
+    ]
+
+    return [verse_start.format(days[i-1]) + "".join([gift for gift in gifts[0:i]][::-1]) for i in range(start_verse, end_verse+1)]
