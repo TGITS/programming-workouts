cc593b7846de8b33e195ee6a52b9173aad026dad
cvaudry <cvaudry@norsys.fr>
2020-06-03 21:55:37 +0200

    Une seconde version à l'exercice twelve days of Christmas de Exercism dans la track Python

--- exercism/python/twelve-days/twelve_days.py
+++ exercism/python/twelve-days/twelve_days.py
@@ -1,26 +1,6 @@
-def recite(start_verse, end_verse):
-    """ Function that gives as a list the verses from the twelve days of Christmas between start_verse and end_verse """
-    verse_start = "On the {} day of Christmas my true love gave to me: "
-    days = ["first", "second", "third", "fourth", "fifth", "sixth",
-            "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"]
-    gifts = [
-        "a Partridge in a Pear Tree.",
-        "two Turtle Doves, and ",
-        "three French Hens, ",
-        "four Calling Birds, ",
-        "five Gold Rings, ",
-        "six Geese-a-Laying, ",
-        "seven Swans-a-Swimming, ",
-        "eight Maids-a-Milking, ",
-        "nine Ladies Dancing, ",
-        "ten Lords-a-Leaping, ",
-        "eleven Pipers Piping, ",
-        "twelve Drummers Drumming, "
-    ]
-
-    return [verse_start.format(days[i-1]) + "".join([gift for gift in gifts[0:i]][::-1]) for i in range(start_verse, end_verse+1)]
-
 def recite_verse(verse_number):
+    """Function that the verse from the twelve days of Christmas specified by its number"""
+    
     verse_start = "On the {} day of Christmas my true love gave to me: "
     days = ["first", "second", "third", "fourth", "fifth", "sixth",
             "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"]
@@ -40,9 +20,17 @@ def recite_verse(verse_number):
     ]
 
     verse = verse_start.format(days[verse_number-1])
-    if(verse_number == 1):
-        verse = verse + gifts[verse_number-1]
+
+    if verse_number == 1:
+        verse = verse + gifts[0]
     else:
-        for i in range(start_verse, end_verse+1):
-            verse = verse 
+        for i in range(verse_number-1, 0, -1):
+            verse = verse + gifts[i]
+        verse = verse + " and " + gifts[0]
+
     return verse
+
+
+def recite(start_verse, end_verse):
+    """Function that gives as a list the verses from the twelve days of Christmas between start_verse and end_verse"""
+    return [recite_verse(i) for i in range(start_verse, end_verse+1)]
