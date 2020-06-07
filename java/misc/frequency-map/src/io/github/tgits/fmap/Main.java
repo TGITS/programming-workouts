e835cbd62104c49e31cd22e8c26c6ecbbfc91b44
VAUDRY Christophe <christophe.vaudry@ext.imprimerienationale.fr>
2020-06-07 17:23:24 +0200

    MAJ du petit projet sur les tableaux de fréqences en Java

--- java/misc/frequency-map/src/io/github/tgits/fmap/Main.java
+++ java/misc/frequency-map/src/io/github/tgits/fmap/Main.java
@@ -6,13 +6,18 @@ import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import static java.lang.System.out;
+import static java.util.stream.Collectors.summingInt;
+
 public class Main {
 
 	public static void main(String[] args) {
 		List<String> characters = List.of("A", "B", "C", "A", "D", "A", "C");
-		System.out.println("Séquence de caractères pour lequel il faut établir le tableau de fréquences : " + characters);
-		System.out.println("Tableau de fréquences déterminé à l'ancienne : " + createFrequencyMapOldSchool(characters));
-		System.out.println("Tableau de fréquences déterminé avec l'API Streams : " + createFrequencyMapWithStreams(characters));
+		out.println("Séquence de caractères pour lequel il faut établir le tableau de fréquences : " + characters);
+		out.println("Tableau de fréquences créé en mode impératif avec des longs : " + createFrequencyMapOldSchool(characters));
+		out.println("Tableau de fréquences déterminé avec l'API Streams avec des longs: " + createFrequencyMapWithStreams(characters));
+		out.println("Tableau de fréquences créé en mode impératif avec des ints : " + createFrequencyMapOldSchoolWithInts(characters));
+		out.println("Tableau de fréquences déterminé avec l'API Streams avec des longs: " + createFrequencyMapWithStreamsAndInts(characters));
 	}
 
 	private static <T> Map<T, Long> createFrequencyMapOldSchool(List<T> elements) {
@@ -31,5 +36,23 @@ public class Main {
 
 	private static <T> Map<T, Long> createFrequencyMapWithStreams(List<T> elements) {
 		return elements.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+	}
+
+	private static <T> Map<T, Integer> createFrequencyMapOldSchoolWithInts(List<T> elements) {
+		Map<T, Integer> frequency = new HashMap<>();
+
+		for (T element : elements) {
+			int previousCount = 0;
+			if (frequency.get(element) != null)
+				previousCount = frequency.get(element);
+
+			frequency.put(element, previousCount + 1);
+		}
+
+		return frequency;
+	}
+
+	private static <T> Map<T, Integer> createFrequencyMapWithStreamsAndInts(List<T> elements) {
+		return elements.stream().collect(Collectors.groupingBy(Function.identity(), summingInt(element -> 1)));
 	}
 }
