dbd2fafe38f7d735ce1fc5d08e932ee1063b94a2
VAUDRY Christophe <christophe.vaudry@ext.imprimerienationale.fr>
2020-06-08 10:01:04 +0200

    Une première solution à l'exercice "reverse string" de Exercism dans la track Dart

--- exercism/dart/reverse-string/lib/reverse_string.dart
+++ exercism/dart/reverse-string/lib/reverse_string.dart
@@ -1,3 +1,11 @@
-String reverse() {
-  // Put your code here
+String reverse(String input) {
+  if(input.isEmpty) {
+    return "";
+  }
+  
+  StringBuffer result = new StringBuffer();
+  for (int i = input.length - 1; i >= 0; i--) {
+    result.write(input[i]);
+  }
+  return result.toString();
 }
