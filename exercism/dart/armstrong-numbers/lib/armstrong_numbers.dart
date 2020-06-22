67caef9c961bfa16754f0ac96a077f3452b6b958
cvaudry <cvaudry@norsys.fr>
2020-06-22 21:26:00 +0200

    MAJ de l'exercice armstrong number de Exercism dans la track Dart

--- exercism/dart/armstrong-numbers/lib/armstrong_numbers.dart
+++ exercism/dart/armstrong-numbers/lib/armstrong_numbers.dart
@@ -1,12 +1,13 @@
 import 'dart:math';
 
 class ArmstrongNumbers {
-  bool isArmstrongNumber(int number) {
-    return number ==
-        '$number'
-            .split("")
-            .map(int.parse)
-            .map((digit) => pow(digit, '$number'.length))
-            .fold(0, (previousValue, element) => previousValue + element);
+  num _armstrongNumber(int number) {
+    return '$number'
+        .split("")
+        .map(int.parse)
+        .map((digit) => pow(digit, '$number'.length))
+        .fold(0, (previousValue, value) => previousValue + value);
   }
+
+  bool isArmstrongNumber(int number) => number == _armstrongNumber(number);
 }
