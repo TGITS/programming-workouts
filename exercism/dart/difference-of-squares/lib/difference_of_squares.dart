2c8be7332155b6fa06ec19fa64984e934f897b79
VAUDRY Christophe <christophe.vaudry@ext.imprimerienationale.fr>
2020-06-23 13:41:58 +0200

    Une première solution à l'exercice difference of squares de Exercism dans la track Dart (exercice mentoré)

--- exercism/dart/difference-of-squares/lib/difference_of_squares.dart
+++ exercism/dart/difference-of-squares/lib/difference_of_squares.dart
@@ -1,9 +1,19 @@
+import 'dart:math';
+
 class DifferenceOfSquares {
-  int squareOfSum(int maxNumber) {
-    return 1;
+  int _square(int number) => pow(number, 2) as int;
+  int squareOfSum(int max) {
+    return _square(new List<int>.generate(max, (i) => i + 1)
+        .fold(0, (previousValue, value) => previousValue + value));
   }
 
-  int sumOfSquares(int maxNumber) {
-    return 1;
+  int sumOfSquares(int max) {
+    return new List<int>.generate(max, (i) => i + 1)
+        .map(_square)
+        .fold(0, (previousValue, value) => previousValue + value);
+  }
+
+  int differenceOfSquares(int max) {
+    return squareOfSum(max) - sumOfSquares(max);
   }
 }
