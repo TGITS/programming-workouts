6508de3f6b0c8eb7c81d690f8c4ab5d93fd55bd2
VAUDRY Christophe <christophe.vaudry@ext.imprimerienationale.fr>
2020-06-24 14:09:11 +0200

    WIP - Travail sur la solution du Kata Diamond via Exercism dans la Track Dart

--- exercism/dart/diamond/lib/diamond.dart
+++ exercism/dart/diamond/lib/diamond.dart
@@ -1,5 +1,9 @@
 class Diamond {
+
+  static final String UPPERCASE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
   List<String> rows(String letter) {
-    return new List();
+    List<String> computedListOfLetters = UPPERCASE_LETTERS.split("").takeWhile((value) => value != letter).toList();
+    return computedListOfLetters;
   }
 }
