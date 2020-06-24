75c873669bb2bdac7581d9b3869ec36487003cf3
cvaudry <cvaudry@norsys.fr>
2020-06-24 21:38:38 +0200

    Ajout de programmes d'exemple dart (utilisation de stagehand)

---
+++ dart/misc/console-full-example/test/console_full_example_test.dart
@@ -0,0 +1,8 @@
+import 'package:console_full_example/console_full_example.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
---
+++ dart/misc/package-simple-example/example/package_simple_example_example.dart
@@ -0,0 +1,6 @@
+import 'package:package_simple_example/package_simple_example.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
