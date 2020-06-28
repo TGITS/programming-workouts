e62726a62194376c7935b3a0ec8fd5ee3d216870
cvaudry <cvaudry@norsys.fr>
2020-06-28 17:48:39 +0200

    Initialisation de plusieurs projets Dart

---
+++ dart/misc/LM_HS_108/calendar_shared/test/calendar_shared_test.dart
@@ -0,0 +1,16 @@
+import 'package:calendar_shared/calendar_shared.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    Awesome awesome;
+
+    setUp(() {
+      awesome = Awesome();
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
