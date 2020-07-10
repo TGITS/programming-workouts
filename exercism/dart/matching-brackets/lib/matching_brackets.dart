0799b307fb900e29d3364fc65b2ea8a4b2eff546
cvaudry <cvaudry@norsys.fr>
2020-07-10 14:18:27 +0200

    Une solution à l'exercice "Matching Bracket" de Exercism dans la track Dart.

--- exercism/dart/matching-brackets/lib/matching_brackets.dart
+++ exercism/dart/matching-brackets/lib/matching_brackets.dart
@@ -1,3 +1,37 @@
 class MatchingBrackets {
-  // Put your code here
+  bool isPaired(String s) {
+    int length = s.length;
+    List<String> stack = List();
+    for (int i = 0; i < length; i++) {
+      switch (s[i]) {
+        case "[":
+          stack.add("[");
+          break;
+        case "{":
+          stack.add("{");
+          break;
+        case "(":
+          stack.add("(");
+          break;
+        case "]":
+          if (stack.isEmpty) return false;
+          String last = stack.removeLast();
+          if (last != "[") return false;
+          break;
+        case "}":
+          if (stack.isEmpty) return false;
+          String last = stack.removeLast();
+          if (last != "{") return false;
+          break;
+        case ")":
+          if (stack.isEmpty) return false;
+          String last = stack.removeLast();
+          if (last != "(") return false;
+          break;
+        default:
+          break;
+      }
+    }
+    return stack.isEmpty;
+  }
 }
--- exercism/dart/matching-brackets/test/matching_brackets_test.dart
+++ exercism/dart/matching-brackets/test/matching_brackets_test.dart
@@ -12,32 +12,32 @@ void main() {
     test("empty string", () {
       final bool result = matchingBrackets.isPaired("");
       expect(result, equals(true));
-    }, skip: true);
+    }, skip: false);
 
     test("unpaired brackets", () {
       final bool result = matchingBrackets.isPaired("[[");
       expect(result, equals(false));
-    }, skip: true);
+    }, skip: false);
 
     test("wrong ordered brackets", () {
       final bool result = matchingBrackets.isPaired("}{");
       expect(result, equals(false));
-    }, skip: true);
+    }, skip: false);
 
     test("wrong closing bracket", () {
       final bool result = matchingBrackets.isPaired("{]");
       expect(result, equals(false));
-    }, skip: true);
+    }, skip: false);
 
     test("paired with whitespace", () {
       final bool result = matchingBrackets.isPaired("{ }");
       expect(result, equals(true));
-    }, skip: true);
+    }, skip: false);
 
     test("partially paired brackets", () {
       final bool result = matchingBrackets.isPaired("{[])");
       expect(result, equals(false));
-    }, skip: true);
+    }, skip: false);
 
     test("simple nested brackets", () {
       final bool result = matchingBrackets.isPaired("{[]}");
@@ -47,47 +47,48 @@ void main() {
     test("several paired brackets", () {
       final bool result = matchingBrackets.isPaired("{}[]");
       expect(result, equals(true));
-    }, skip: true);
+    }, skip: false);
 
     test("paired and nested brackets", () {
       final bool result = matchingBrackets.isPaired("([{}({}[])])");
       expect(result, equals(true));
-    }, skip: true);
+    }, skip: false);
 
     test("unopened closing brackets", () {
       final bool result = matchingBrackets.isPaired("{[)][]}");
       expect(result, equals(false));
-    }, skip: true);
+    }, skip: false);
 
     test("unpaired and nested brackets", () {
       final bool result = matchingBrackets.isPaired("([{])");
       expect(result, equals(false));
-    }, skip: true);
+    }, skip: false);
 
     test("paired and wrong nested brackets", () {
       final bool result = matchingBrackets.isPaired("[({]})");
       expect(result, equals(false));
-    }, skip: true);
+    }, skip: false);
 
     test("paired and incomplete brackets", () {
       final bool result = matchingBrackets.isPaired("{}[");
       expect(result, equals(false));
-    }, skip: true);
+    }, skip: false);
 
     test("too many closing brackets", () {
       final bool result = matchingBrackets.isPaired("[]]");
       expect(result, equals(false));
-    }, skip: true);
+    }, skip: false);
 
     test("math expression", () {
-      final bool result = matchingBrackets.isPaired("(((185 + 223.85) * 15) - 543)/2");
+      final bool result =
+          matchingBrackets.isPaired("(((185 + 223.85) * 15) - 543)/2");
       expect(result, equals(true));
-    }, skip: true);
+    }, skip: false);
 
     test("complex latex expression", () {
-      final bool result = matchingBrackets
-          .isPaired("\\left(\\begin{array}{cc} \\frac{1}{3} & x\\\\ \\mathrm{e}^{x} &... x^2 \\end{array}\\right)");
+      final bool result = matchingBrackets.isPaired(
+          "\\left(\\begin{array}{cc} \\frac{1}{3} & x\\\\ \\mathrm{e}^{x} &... x^2 \\end{array}\\right)");
       expect(result, equals(true));
-    }, skip: true);
+    }, skip: false);
   });
 }
