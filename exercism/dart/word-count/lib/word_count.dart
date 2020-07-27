58a6637fdc821cf01fe5e0dc04edc8b0d67aa507
cvaudry <cvaudry@norsys.fr>
2020-07-27 15:56:35 +0200

    A new solution to the exercise "Word Count" form Exercism in the dart track

--- exercism/dart/word-count/lib/word_count.dart
+++ exercism/dart/word-count/lib/word_count.dart
@@ -2,27 +2,12 @@ class WordCount {
   Map<String, int> _collectAsMap(Map<String, int> accumulator, String key) =>
       accumulator..update(key, (int value) => ++value, ifAbsent: () => 1);
 
-  String _deleteFrontAndBackQuotesIfAny(String inputString) {
-    String processedString = inputString;
-    if (processedString.startsWith('\'')) {
-      processedString = processedString.substring(1);
-    }
-
-    if (processedString.endsWith('\'')) {
-      processedString =
-          processedString.substring(0, processedString.length - 1);
-    }
-
-    return processedString;
-  }
-
   Map<String, int> countWords(String input) {
     return input
-        .replaceAll(RegExp(r'[,;:!&@%&\?\$\^\.\*\\\s]+'), ' ')
+        .replaceAll(RegExp(r"(?!'\w)([\W']+|['\W])"), ' ')
         .split(' ')
         .where((s) => s.trim().isNotEmpty)
         .map((s) => s.toLowerCase())
-        .map(_deleteFrontAndBackQuotesIfAny)
         .fold({}, _collectAsMap);
   }
 }
