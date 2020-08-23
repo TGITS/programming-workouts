bb42e169665f804551d456644915468c801246dd
cvaudry <cvaudry@norsys.fr>
2020-08-23 19:02:11 +0200

    Initialisation de l'exercice Alphametics de Exercism dans la Track Java

---
+++ exercism/java/alphametics/src/main/java/Alphametics.java
@@ -0,0 +1,10 @@
+import java.util.LinkedHashMap;
+
+public class Alphametics {
+    public Alphametics(String s) {
+    }
+
+    public LinkedHashMap<Character, Integer> solve() {
+        return null;
+    }
+}
