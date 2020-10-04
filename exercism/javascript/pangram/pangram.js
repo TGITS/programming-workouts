ecccdc6a2b51119021deb4ff38698bf9bce72bc0
cvaudry <cvaudry@norsys.fr>
2020-10-04 21:25:14 +0200

    WIP : travail sur la solution de pangram dans la track Javascript d'exercism

--- exercism/javascript/pangram/pangram.js
+++ exercism/javascript/pangram/pangram.js
@@ -1,5 +1,6 @@
 const alphabet = new Set("abcdefghijklmnopqrstuvwxyz".split(''));
 
 export const isPangram = (s) => {
-  return new Set(s.toLowerCase().split('').filter(x => alphabet.has(x))).size == alphabet.size;
+  //return new Set(s.toLowerCase().split('').filter(x => alphabet.has(x))).size == alphabet.size;
+  return new Set(s.toLowerCase().match(/[a-z]/g)).size == alphabet.size;
 };
