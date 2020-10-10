f4c3bc3d2e4c7dd7da28041e118e1b54a6d22737
cvaudry <cvaudry@norsys.fr>
2020-10-10 18:32:23 +0200

    Une version intermédiaire de l'exercice RNA Transcription de Exercism dans la track Typescript

--- exercism/typescript/rna-transcription/rna-transcription.ts
+++ exercism/typescript/rna-transcription/rna-transcription.ts
@@ -1,21 +1,26 @@
+type Cytosine = 'C';
+type Guanine = 'G';
+type Adenine = 'A';
+type Thymine = 'T';
+type Uracil = 'U';
+type RnaNucleotide = Cytosine | Guanine | Adenine | Uracil;
+type DnaNucleotide = Cytosine | Guanine | Adenine | Thymine;
+
 class Transcriptor {
+
+
     toRna(strand: string): string {
-        try {
-            return strand.split('').map(n => this.toRnaNucleotide(n)).join('');
-        }
-        catch (e) {
-            throw new Error('Invalid input DNA.');
-        }
+        return strand.split('').map(n => this.toRnaNucleotide(n)).join('');
     }
 
-    private toRnaNucleotide(nucleotide: string): string {
+    private toRnaNucleotide(nucleotide: string): RnaNucleotide {
         switch (nucleotide) {
             case 'G': return 'C';
             case 'C': return 'G';
             case 'T': return 'A';
             case 'A': return 'U';
             default: {
-                throw new Error('Invalid input nucleotide.')
+                throw new Error('Invalid input DNA.')
             }
         }
     }
