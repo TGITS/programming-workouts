21a9b2dc3394ee5087ff8ce7627a9600f99d66b9
cvaudry <cvaudry@norsys.fr>
2020-10-11 19:06:03 +0200

    Nouvelle version de l'exercice RNA Transcription de Exercism dans la track Typescript

--- exercism/typescript/rna-transcription/rna-transcription.ts
+++ exercism/typescript/rna-transcription/rna-transcription.ts
@@ -1,38 +1,25 @@
-import { CANCELLED } from "dns";
+type RnaNucleotide = 'C' | 'G' | 'A' | 'U';
 
-type Cytosine = 'C';
-type Guanine = 'G';
-type Adenine = 'A';
-type Thymine = 'T';
-type Uracil = 'U';
-type RnaNucleotide = Cytosine | Guanine | Adenine | Uracil;
-type DnaNucleotide = Cytosine | Guanine | Adenine | Thymine;
-
-const DNA_TO_RNA: Record<DnaNucleotide, RnaNucleotide> = {
-    G : 'C',
-    C : 'G',
-    T : 'A',
-    A : 'U',
+const DNA_TO_RNA: Record<string, RnaNucleotide> = {
+    'G' : 'C',
+    'C' : 'G',
+    'T' : 'A',
+    'A' : 'U',
 }
 
 class Transcriptor {
 
-
     toRna(strand: string): string {
-        return strand.split('').map(n => this.toRnaNucleotide(n)).join('');
+        return strand
+                .split('')
+                .map(nucleotide => DNA_TO_RNA[nucleotide] || this.invalidInput())
+                .join('');
     }
 
-    private toRnaNucleotide(nucleotide: string): RnaNucleotide {
-        switch (nucleotide) {
-            case 'G': return DNA_TO_RNA.G;
-            case 'C': return DNA_TO_RNA.C;
-            case 'T': return DNA_TO_RNA.T;
-            case 'A': return DNA_TO_RNA.A;
-            default: {
-                throw new Error('Invalid input DNA.')
-            }
-        }
+    private invalidInput():void {
+        throw new Error('Invalid input DNA.')
     }
+
 }
 
 export default Transcriptor
