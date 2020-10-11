f102fa7ba2837b9d5bae098219e8124693409d9e
cvaudry <cvaudry@norsys.fr>
2020-10-11 15:57:27 +0200

    Nouvelle version de RNA Transcription dans la track Typescript d'Exercism

--- exercism/typescript/rna-transcription/rna-transcription.ts
+++ exercism/typescript/rna-transcription/rna-transcription.ts
@@ -1,3 +1,5 @@
+import { CANCELLED } from "dns";
+
 type Cytosine = 'C';
 type Guanine = 'G';
 type Adenine = 'A';
@@ -5,6 +7,13 @@ type Thymine = 'T';
 type Uracil = 'U';
 type RnaNucleotide = Cytosine | Guanine | Adenine | Uracil;
 type DnaNucleotide = Cytosine | Guanine | Adenine | Thymine;
+
+const DNA_TO_RNA: Record<DnaNucleotide, RnaNucleotide> = {
+    G : 'C',
+    C : 'G',
+    T : 'A',
+    A : 'U',
+}
 
 class Transcriptor {
 
@@ -15,10 +24,10 @@ class Transcriptor {
 
     private toRnaNucleotide(nucleotide: string): RnaNucleotide {
         switch (nucleotide) {
-            case 'G': return 'C';
-            case 'C': return 'G';
-            case 'T': return 'A';
-            case 'A': return 'U';
+            case 'G': return DNA_TO_RNA.G;
+            case 'C': return DNA_TO_RNA.C;
+            case 'T': return DNA_TO_RNA.T;
+            case 'A': return DNA_TO_RNA.A;
             default: {
                 throw new Error('Invalid input DNA.')
             }
