8c6149d1d8e0445e4c84bc33eabf75f31545a3fe
cvaudry <cvaudry@norsys.fr>
2020-10-23 18:39:58 +0200

    MAJ du code pour le billet

--- python/misc/learning_python/reversing_string.py
+++ python/misc/learning_python/reversing_string.py
@@ -1,2 +1,21 @@
-lannister = "lannister"
-print(lannister[::-1])
+bonjour = 'Hello World'
+
+print("\n#######################\n")
+
+print(bonjour)
+print(reversed(bonjour))
+
+print(bonjour)
+print("".join(reversed(bonjour)))
+
+print(bonjour)
+for c in reversed(bonjour):
+    print(c)
+
+print([c for c in reversed(bonjour)])
+
+print("\n#######################\n")
+
+print(bonjour)
+print(bonjour[::-1])
+print([c for c in bonjour[::-1]])
