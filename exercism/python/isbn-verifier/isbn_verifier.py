612e1dea900f7adb545623848ed12ea934b85f36
VAUDRY Christophe <christophe.vaudry@ext.imprimerienationale.fr>
2020-12-11 08:42:11 +0100

    Une solution à l'exercice "ISBN Verifier" de Exercism dans la track Python

--- exercism/python/isbn-verifier/isbn_verifier.py
+++ exercism/python/isbn-verifier/isbn_verifier.py
@@ -1,3 +1,16 @@
-def is_valid(isbn):
-    return sum([(index + 1) * int(number) for index, number in enumerate(list(isbn.replace('-',''))[::-1])]) % 11 == 0
+import re
 
+
+def is_valid(isbn):
+    clean_isbn = isbn.replace('-', '')
+    if re.fullmatch(r"\d{9}(\d|X)", clean_isbn, re.IGNORECASE):
+        return sum([(index + 1) * to_int(digit_or_x)
+                    for index, digit_or_x in enumerate(clean_isbn[::-1])]) % 11 == 0
+    return False
+
+
+def to_int(digit_as_string):
+    if 'X' == digit_as_string.upper():
+        return 10
+    else:
+        return int(digit_as_string)
