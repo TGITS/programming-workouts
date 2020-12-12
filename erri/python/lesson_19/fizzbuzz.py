eaeb3a8ee87f489d05aba090f64c337a02bababa
cvaudry <cvaudry@norsys.fr>
2020-12-12 15:15:31 +0100

    Travail du jour avec Erri

---
+++ erri/python/lesson_19/fizzbuzz.py
@@ -0,0 +1,27 @@
+def est_divisible_par_3(n):
+    reste = n % 3
+    return reste == 0
+
+
+def est_divisible_par_5(n):
+    reste = n % 5
+    return reste == 0
+
+
+def fizzbuzz(n):
+    fb = ""
+    if est_divisible_par_3(n):
+        fb = fb + "Fizz"
+    if est_divisible_par_5(n):
+        fb = fb + "Buzz"
+    if not est_divisible_par_3(n) and not est_divisible_par_5(n):
+        fb = fb + str(n)
+    return fb
+
+
+if __name__ == "__main__":
+    resultat = ""
+    for n in range(1, 101):
+        resultat = resultat + fizzbuzz(n) + " "
+
+    print(resultat)
