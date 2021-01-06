2cdbc9edb1db62183586a1b01d0c069cab88bf23
VAUDRY Christophe <christophe.vaudry@ext.imprimerienationale.fr>
2021-01-06 07:15:35 +0100

    Une première proposition de solution à l'exercice "TwoFer" de Exercism dans la Track F#

--- exercism/fsharp/two-fer/TwoFer.fs
+++ exercism/fsharp/two-fer/TwoFer.fs
@@ -1,3 +1,6 @@
 ﻿module TwoFer
 
-let twoFer (input: string option): string = failwith "You need to implement this function."
+let twoFer (input: string option): string =
+    match input with
+    | Some name -> $"One for {name}, one for me."
+    | None -> "One for you, one for me."
