c57346572e222b400ecc8ec972c14aaad4a79813
VAUDRY Christophe <christophe.vaudry@ext.imprimerienationale.fr>
2021-02-24 07:57:13 +0100

    Une première solution à l'exercice "Word Count" de Exercism dans la track Elixir.

--- exercism/elixir/word-count/lib/word_count.ex
+++ exercism/elixir/word-count/lib/word_count.ex
@@ -6,5 +6,10 @@ defmodule WordCount do
   """
   @spec count(String.t()) :: map
   def count(sentence) do
+    String.split(sentence,~r{\s|_},trim: true) 
+    |> Enum.map(&(String.replace(&1,~r{[\s,:;!&@%\?\$\^]},""))) 
+    |> Enum.filter(&(String.length(&1) > 0)) 
+    |> Enum.map(&(String.downcase(&1))) 
+    |> Enum.frequencies()
   end
 end
