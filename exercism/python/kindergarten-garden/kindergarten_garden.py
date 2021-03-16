26878c5d2d1b487174dde7940e201dd5344b3da0
VAUDRY Christophe <christophe.vaudry@ext.imprimerienationale.fr>
2021-03-16 13:54:45 +0100

    Petite correction formelle sur la solution en Python à "kindergarten_garden" de Exercism dans la track Python Initialisation de l'exercice "Grade School" de Exercism dans la Track Python

--- exercism/python/kindergarten-garden/kindergarten_garden.py
+++ exercism/python/kindergarten-garden/kindergarten_garden.py
@@ -2,11 +2,11 @@ class Garden:
     def __init__(self, diagram, students=['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Fred', 'Ginny', 'Harriet', 'Ileana', 'Joseph', 'Kincaid', 'Larry']):
         self.rows = diagram.split("\n")
         self.students = sorted(students)
-        self.plant_name_by_initial = { 'V': 'Violets', 'R' : 'Radishes', 'C':'Clover', 'G' : 'Grass'}
+        self.plant_names = { 'V': 'Violets', 'R' : 'Radishes', 'C':'Clover', 'G' : 'Grass'}
 
     def plants(self, child_name):
         child_plant_index = self.students.index(child_name) * 2
         child_plants = []
         for row in self.rows :
-            child_plants.extend([self.plant_name_by_initial[c] for c in row[child_plant_index:child_plant_index+2]])
+            child_plants.extend([self.plant_names[c] for c in row[child_plant_index:child_plant_index+2]])
         return child_plants
