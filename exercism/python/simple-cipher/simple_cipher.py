927e72bafd9a0be67f28096d51707e5245443891
VAUDRY Christophe <christophe.vaudry@ext.imprimerienationale.fr>
2021-03-18 18:21:40 +0100

    WIP : travail sur simple cipher de Exercism dans la track Python

--- exercism/python/simple-cipher/simple_cipher.py
+++ exercism/python/simple-cipher/simple_cipher.py
@@ -1,9 +1,23 @@
+import string
+import secrets
+
+
 class Cipher:
     def __init__(self, key=None):
-        pass
+        self.alphabet = string.ascii_lowercase
+        self.key = key if key else ''.join(
+            secrets.choice(self.alphabet) for _ in range(100))
 
     def encode(self, text):
-        pass
+        encoded = ""
+        for i, c in enumerate(text.lower()):
+            encoded += chr(((ord(c) +
+                             ord(self.key[i % len(self.key)]) - ord('a')) - ord('a')) % 26 + ord('a'))
+        return encoded
 
     def decode(self, text):
-        pass
+        decoded = ""
+        for i, c in enumerate(text.lower()):
+            decoded += chr(((ord(c) -
+                             ord(self.key[i] % len(self.key)) - ord('a')) % 26 + ord('a')))
+        return decoded
