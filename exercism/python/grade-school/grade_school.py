9047e733ba3454d7b9ac435c43078ee4d261f94e
VAUDRY Christophe <christophe.vaudry@ext.imprimerienationale.fr>
2021-03-22 07:20:18 +0100

    Une première solution à l'exercice "grade shool" de Exercism dans la track Python

--- exercism/python/grade-school/grade_school.py
+++ exercism/python/grade-school/grade_school.py
@@ -1,12 +1,15 @@
 class School:
     def __init__(self):
-        pass
+        self.students_by_grade = {}
 
     def add_student(self, name, grade):
-        pass
+        if grade not in self.students_by_grade:
+            self.students_by_grade[grade] = []
+        
+        self.students_by_grade[grade].append(name)
 
     def roster(self):
-        pass
+        return [student for _, students in sorted(self.students_by_grade.items()) for student in sorted(students)]
 
     def grade(self, grade_number):
-        pass
+        return sorted(self.students_by_grade[grade_number]) if grade_number in self.students_by_grade else []
