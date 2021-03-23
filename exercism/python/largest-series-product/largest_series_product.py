e2611222ffde9b7cef791c17abed5102651166bb
VAUDRY Christophe <christophe.vaudry@ext.imprimerienationale.fr>
2021-03-23 08:09:15 +0100

    Une solution à l'exercice "Largest series product" de Exercism dans la track Python

--- exercism/python/largest-series-product/largest_series_product.py
+++ exercism/python/largest-series-product/largest_series_product.py
@@ -1,2 +1,12 @@
+from math import prod
+
+
 def largest_product(series, size):
-    pass
+    if size == 0:
+        return 1
+
+    if size < 0:
+        raise ValueError("The span cannot be a negative integer")
+
+    index_max = len(series) - size + 1
+    return max([prod([int(c) for c in list(series[i:i+size])]) for i in range(0, index_max)])
