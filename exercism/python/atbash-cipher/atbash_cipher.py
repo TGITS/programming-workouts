8735a4fdec6ced5d02047dc5288f66ab6ed173f2
cvaudry <cvaudry@norsys.fr>
2021-03-24 21:28:09 +0100

    WIP - Exercice Atbash Cipher de Exercism dans la Track Python

--- exercism/python/atbash-cipher/atbash_cipher.py
+++ exercism/python/atbash-cipher/atbash_cipher.py
@@ -1,6 +1,21 @@
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+key = "zyxwvutsrqponmlkjihgfedcba"
+
+
 def encode(plain_text):
-    pass
+    ciphered_text = ""
+    counter = 0
+    for c in plain_text.lower().replace(' ','').replace(',',"").replace('.',''):
+        if c in alphabet:
+            ciphered_text += key[alphabet.index(c)]
+        else:
+            ciphered_text += c
+        counter += 1
+        if counter % 5 == 0:
+            ciphered_text += ' '
+    
+    return ciphered_text
 
 
 def decode(ciphered_text):
-    pass
+    return "".join([alphabet[key.index(c)] for c in ciphered_text.replace(' ','')])
