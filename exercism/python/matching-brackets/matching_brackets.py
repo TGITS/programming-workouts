659668a376d09e9b6f87d91e28d2b92ce0519165
VAUDRY Christophe <christophe.vaudry@ext.imprimerienationale.fr>
2021-03-24 08:14:05 +0100

    Une solution à l'exercice "matching brackets" de Exercism dans la track Python

--- exercism/python/matching-brackets/matching_brackets.py
+++ exercism/python/matching-brackets/matching_brackets.py
@@ -1,2 +1,15 @@
 def is_paired(input_string):
-    pass
+
+    if len(input_string) == 0:
+        return True
+
+    pairing_char = {'}': '{', ')': '(', ']': '['}
+    stack = []
+
+    for c in input_string:
+        if c in pairing_char.values():
+            stack.append(c)
+        elif c in pairing_char.keys() and (len(stack) == 0 or stack.pop() != pairing_char[c]):
+            return False
+
+    return len(stack) == 0
