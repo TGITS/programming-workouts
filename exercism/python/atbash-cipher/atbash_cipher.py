c0cb689cdcdd308158af4a49f8a6ee988558e0a2
VAUDRY Christophe <christophe.vaudry@ext.imprimerienationale.fr>
2021-03-25 08:08:20 +0100

    Une première solution à l'exercice Atbash Cipher de Exercism dans la track Python

--- exercism/python/atbash-cipher/atbash_cipher.py
+++ exercism/python/atbash-cipher/atbash_cipher.py
@@ -5,17 +5,24 @@ key = "zyxwvutsrqponmlkjihgfedcba"
 def encode(plain_text):
     ciphered_text = ""
     counter = 0
-    for c in plain_text.lower().replace(' ','').replace(',',"").replace('.',''):
+    for c in plain_text.lower().replace(' ', '').replace(',', "").replace('.', ''):
+        if counter != 0 and counter % 5 == 0:
+            ciphered_text += ' '
         if c in alphabet:
             ciphered_text += key[alphabet.index(c)]
         else:
             ciphered_text += c
         counter += 1
-        if counter % 5 == 0:
-            ciphered_text += ' '
-    
+
     return ciphered_text
 
 
 def decode(ciphered_text):
-    return "".join([alphabet[key.index(c)] for c in ciphered_text.replace(' ','')])
+    plain_text = ""
+    for c in ciphered_text.replace(' ', ''):
+        if c in key:
+            plain_text += alphabet[key.index(c)]
+        else:
+            plain_text += c
+
+    return plain_text
