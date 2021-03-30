5df7c0f40d9dd0456429792ec85a76ba34bb204a
VAUDRY Christophe <christophe.vaudry@ext.imprimerienationale.fr>
2021-03-30 13:50:44 +0200

    Une solution à l'exercice "Secret Handshake" de Exercism dans la Track Python

--- exercism/python/secret-handshake/secret_handshake.py
+++ exercism/python/secret-handshake/secret_handshake.py
@@ -1,2 +1,18 @@
+events_by_code = {10000: lambda x: x.reverse(), 1000: lambda x: x.append("jump"), 100: lambda x: x.append(
+    "close your eyes"), 10: lambda x: x.append("double blink"), 1: lambda x: x.append("wink")}
+
+
 def commands(number):
-    pass
+    binary = int(bin(number)[2:])
+
+    actions = []
+    for code in reversed(sorted(events_by_code.keys())):
+        if binary >= code:
+            binary -= code
+            actions.append(code)
+
+    secret_handshake = []
+    for code in reversed(actions):
+        events_by_code[code](secret_handshake)
+
+    return secret_handshake
