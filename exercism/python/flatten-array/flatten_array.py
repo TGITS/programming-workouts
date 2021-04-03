a238c6d8ed1b637d12cc3f52d70f1839119889e1
cvaudry <cvaudry@norsys.fr>
2021-04-03 15:30:28 +0200

    Une solution à l'exercice "flatten_array" de exercism dans la track Python.

--- exercism/python/flatten-array/flatten_array.py
+++ exercism/python/flatten-array/flatten_array.py
@@ -1,2 +1,10 @@
 def flatten(iterable):
-    pass
+    flatten_iterable = []
+    for elem in iterable:
+        if elem is not None :
+            if type(elem) is list:
+                flatten_iterable.extend(flatten(elem))
+            else:
+                flatten_iterable.append(elem)
+    
+    return flatten_iterable
