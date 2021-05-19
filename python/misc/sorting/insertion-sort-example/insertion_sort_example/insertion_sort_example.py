0241f959761ef0f6e390ff38a5bb9bb87d4b6037
VAUDRY Christophe <christophe.vaudry@ext.imprimerienationale.fr>
2021-05-19 17:51:13 +0200

    Correction dans un toml file Initialisation d'un projet example sur le tri par insertion

---
+++ python/misc/sorting/insertion-sort-example/insertion_sort_example/insertion_sort_example.py
@@ -0,0 +1,5 @@
+def insertion_sort(array):
+    pass
+
+def insert(array, index, value_to_insert):
+    pass
---
+++ python/misc/sorting/insertion-sort-example/tests/test_insertion_sort_example.py
@@ -0,0 +1,5 @@
+from insertion_sort_example import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
