6e69c13d5041fedd59452c60dea06f8695a1d086
VAUDRY Christophe <christophe.vaudry@ext.imprimerienationale.fr>
2021-05-25 13:48:47 +0200

    Une première solution à l'exercice clock de Exercism dans la track Python

--- exercism/python/clock/clock.py
+++ exercism/python/clock/clock.py
@@ -1,15 +1,28 @@
 class Clock:
-    def __init__(self, hour, minute):
-        pass
 
-    def __repr__(self):
-        pass
+    def __init__(self, hour: int, minute: int):
+        self.minute = minute % 60
+        overflow = minute // 60
+        self.hour = (hour + overflow) % 24
 
-    def __eq__(self, other):
-        pass
+    def __repr__(self) -> str:
+        return f'{str(self.hour).rjust(2, "0")}:{str(self.minute).rjust(2, "0")}'
 
-    def __add__(self, minutes):
-        pass
+    def __eq__(self, other) -> bool:
+        if isinstance(other, Clock):
+            return self.hour == other.hour and self.minute == other.minute
+        return False
 
-    def __sub__(self, minutes):
-        pass
+    def __add__(self, minutes: int):
+        total_minutes = self.minute + minutes
+        self.minute = total_minutes % 60
+        overflow = total_minutes // 60
+        self.hour = (self.hour + overflow) % 24
+        return self
+
+    def __sub__(self, minutes: int):
+        total_minutes = self.minute - minutes
+        self.minute = total_minutes % 60
+        overflow = total_minutes // 60
+        self.hour = (self.hour + overflow) % 24
+        return self
