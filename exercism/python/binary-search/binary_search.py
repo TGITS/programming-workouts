873fea01c54d41696277d154d89016c0b6faf90d
VAUDRY Christophe <christophe.vaudry@ext.imprimerienationale.fr>
2021-05-26 13:55:23 +0200

    Une solution iérative à l'exercice binary_search de Exercism dans la track Python

--- exercism/python/binary-search/binary_search.py
+++ exercism/python/binary-search/binary_search.py
@@ -1,24 +1,4 @@
 def find(search_list, value):
-    return find_between_limit(search_list, value, 0, len(search_list) - 1)
-
-def find_between_limit(search_list, value, min_index, max_index):
-    if min_index == max_index:
-        if value == search_list[min_index]:
-            return min_index
-        else:
-            raise ValueError(f"Value {value} not found")
-    elif min_index < max_index:
-        middle_index = (max_index - min_index) // 2 + min_index
-        if value == search_list[middle_index]:
-            return middle_index
-        elif value > search_list[middle_index]:
-            return find_between_limit(search_list, value, middle_index + 1, max_index)
-        else:
-            return find_between_limit(search_list, value, min_index, middle_index - 1)
-    else:
-        raise ValueError(f"Value {value} not found")
-
-def find_iteratively(search_list, value):
     min_index = 0
     max_index = len(search_list) - 1
 
