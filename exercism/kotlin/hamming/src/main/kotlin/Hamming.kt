9bd73c0edf339a1ab3bd9ec0e29b54d7176e5023
TGITS <thegeekintheshell@gmail.com>
2021-07-03 15:40:54 +0200

    Une deuxième solution à Hamming dans la track Kotlin de Exercism

--- exercism/kotlin/hamming/src/main/kotlin/Hamming.kt
+++ exercism/kotlin/hamming/src/main/kotlin/Hamming.kt
@@ -5,15 +5,6 @@ object Hamming {
             throw IllegalArgumentException("left and right strands must be of equal length");
         }
 
-        var countDifferences = 0
-        if (leftStrand.length > 0) {
-            for (i in 0 until leftStrand.length) {
-                if (leftStrand[i] != rightStrand[i]) countDifferences++
-            }
-        }
-        
-        return countDifferences
-
-        //return leftStrand.asSequence().zip(rightStrand.asSequence()).filter({pair -> pair.first != pair.second}).count()
+        return leftStrand.asSequence().zip(rightStrand.asSequence()).filter({pair -> pair.first != pair.second}).count()
     }
 }
