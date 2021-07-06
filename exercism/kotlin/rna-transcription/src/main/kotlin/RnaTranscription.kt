83d7dadcd8c5b2724584409be12970efd916e039
TGITS <thegeekintheshell@gmail.com>
2021-07-06 08:29:32 +0200

    Une première solution à l'exercice "RNA Transcription" de Exercism dans la track Kotlin.

--- exercism/kotlin/rna-transcription/src/main/kotlin/RnaTranscription.kt
+++ exercism/kotlin/rna-transcription/src/main/kotlin/RnaTranscription.kt
@@ -1,1 +1,11 @@
-fun transcribeToRna(dna: String): String = ""
+fun transcribeToRna(dna: String): String {
+    return dna.asSequence().map { c ->
+        when (c) {
+            'G' -> 'C'
+            'C' -> 'G'
+            'T' -> 'A'
+            'A' -> 'U'
+            else -> throw IllegalArgumentException("Invalid nucleotid")
+        }
+    }.joinToString("")
+}
