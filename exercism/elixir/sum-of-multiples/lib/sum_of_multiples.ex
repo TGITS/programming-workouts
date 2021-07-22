8e844cab36a5f40912b8248e661ed463e687b752
TGITS <thegeekintheshell@gmail.com>
2021-07-22 17:26:05 +0200

    Une première solution à l'exercice "Sum of Multiples" de Exercism dans la track Elixir.

--- exercism/elixir/sum-of-multiples/lib/sum_of_multiples.ex
+++ exercism/elixir/sum-of-multiples/lib/sum_of_multiples.ex
@@ -3,6 +3,9 @@ defmodule SumOfMultiples do
   Adds up all numbers from 1 to a given end number that are multiples of the factors provided.
   """
   @spec to(non_neg_integer, [non_neg_integer]) :: non_neg_integer
-  def to(limit, factors) do
-  end
+  def to(1, _), do: 0
+  def to(limit, factors), do: 1..(limit-1) |> Enum.filter(&any_factors?(&1, factors)) |> Enum.sum()
+
+  @spec any_factors?(non_neg_integer, [non_neg_integer]) :: boolean
+  def any_factors?(num, factors), do: factors |> Enum.any?(&(rem(num, &1) == 0))
 end
