fe0bb4f6945d1cc6ff3bb257fee1e51e03b68ae4
TGITS <thegeekintheshell@gmail.com>
2021-08-09 13:43:27 +0200

    MAJ de l'exercice "hamming" de Exercism dans la track Kotlin

--- exercism/kotlin/hamming/src/main/kotlin/Hamming.kt
+++ exercism/kotlin/hamming/src/main/kotlin/Hamming.kt
@@ -1,9 +1,7 @@
 object Hamming {
 
     fun compute(leftStrand: String, rightStrand: String): Int {
-        if (leftStrand.length != rightStrand.length) {
-            throw IllegalArgumentException("left and right strands must be of equal length");
-        }
+        require(leftStrand.length == rightStrand.length) { "left and right strands must be of equal length" }
 
         return leftStrand.asSequence().zip(rightStrand.asSequence()).filter({pair -> pair.first != pair.second}).count()
     }
