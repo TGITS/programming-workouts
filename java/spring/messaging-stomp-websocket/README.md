01a48da065c13bf69f482448deade70c668513e1
TGITS <thegeekintheshell@gmail.com>
2021-11-26 12:29:57 +0100

    Code correspondant au tutoriel sur WebSocket avec Spring (https://spring.io/guides/gs/messaging-stomp-websocket/)

---
+++ java/spring/messaging-stomp-websocket/README.md
@@ -0,0 +1,3 @@
+# README
+
+Projet et code venant du tutoriel [Using WebSocket to build an interactive web application](https://spring.io/guides/gs/messaging-stomp-websocket/), créé en suivant le tutoriel.
---
+++ java/spring/messaging-stomp-websocket/src/main/java/com/example/messagingstompwebsocket/Greeting.java
@@ -0,0 +1,42 @@
+package com.example.messagingstompwebsocket;
+
+public class Greeting {
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((content == null) ? 0 : content.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Greeting other = (Greeting) obj;
+        if (content == null) {
+            if (other.content != null)
+                return false;
+        } else if (!content.equals(other.content))
+            return false;
+        return true;
+    }
+
+}
