b9266567ceb498041e04af246e6c626fc56fe42a
TGITS <thegeekintheshell@gmail.com>
2022-08-21 17:15:55 +0200

    Travail sur les exercices armstrong-numbers et perfect-numbers de Exercism dans la track Rust

--- exercism/rust/armstrong-numbers/src/lib.rs
+++ exercism/rust/armstrong-numbers/src/lib.rs
@@ -1,3 +1,11 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+    let num_as_string = num.to_string();
+    let number_of_digits:u32 = num_as_string.len().try_into().unwrap();
+    let mut calculus:u32 = 0;
+    for c in num_as_string.chars() {
+        if let Some(x) = c.to_digit(10)  {
+            calculus = calculus + x.pow(number_of_digits);
+        }
+    }
+    calculus == num
 }
--- exercism/rust/perfect-numbers/src/lib.rs
+++ exercism/rust/perfect-numbers/src/lib.rs
@@ -6,5 +6,24 @@ pub enum Classification {
 }
 
 pub fn classify(num: u64) -> Option<Classification> {
-    unimplemented!("classify {}", num);
+    if num == 0 {
+        return None;
+    }
+
+    let mut sum: u64 = 0;
+    for n in 1..num {
+        if num % n == 0 {
+            sum += n;
+        }
+    }
+
+    if sum == num {
+        return Some(Classification::Perfect);
+    } else if sum > num {
+        return Some(Classification::Abundant);
+    } else if sum < num {
+        return Some(Classification::Deficient);
+    } else {
+        return None;
+    }
 }
