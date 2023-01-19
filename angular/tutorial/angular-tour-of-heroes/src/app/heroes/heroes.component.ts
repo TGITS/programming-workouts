905516c7b680d00db2647cc986d7eba194b4123d
TGITS <thegeekintheshell@gmail.com>
2023-01-19 09:22:09 +0100

    Travail sur le tutorial Angular "Tour Of Heroes"

--- angular/tutorial/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular/tutorial/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,21 +10,23 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  
-  constructor(private heroService: HeroService) {}
-  
-  ngOnInit(): void {
-    this.getHeroes();
-  }
 
   heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
 }
