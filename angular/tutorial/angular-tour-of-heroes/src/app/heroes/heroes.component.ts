5620af292a72cbff1956e1fb023de64e67860809
TGITS <thegeekintheshell@gmail.com>
2023-01-19 07:13:17 +0100

    Travail sur le tutorial Angular "Tour of Heroes"

--- angular/tutorial/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular/tutorial/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
-  heroes:Hero[] = HEROES;
+export class HeroesComponent implements OnInit {
+  
+  constructor(private heroService: HeroService) {}
+  
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  heroes: Hero[] = [];
   selectedHero?: Hero;
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
