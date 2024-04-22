7cabf8b86e5143036f13351c8455cf3106d5d8fb
TGITS <thegeekintheshell@gmail.com>
2024-04-22 18:46:08 +0200

    MAJ des notes

---
+++ data/duckdb/node/minimalist-test/index.js
@@ -0,0 +1,9 @@
+var duckdb = require("duckdb");
+var db = new duckdb.Database(":memory:"); // or a file name for a persistent DB
+db.all("SELECT 42 AS fortytwo", function (err, res) {
+  if (err) {
+    console.warn(err);
+    return;
+  }
+  console.log(res[0].fortytwo);
+});
