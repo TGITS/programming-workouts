d177068527f506d8e702763f04596d54aa64d7e0
TGITS <thegeekintheshell@gmail.com>
2024-12-03 21:51:01 +0100

    Travail AoC Jour 3

--- advent_of_code/2024/day3/python/aoc2024_3_1/AoC2024_d3_p1.py
+++ advent_of_code/2024/day3/python/aoc2024_3_1/AoC2024_d3_p1.py
@@ -1,2 +1,22 @@
+import re
+
+def extract_data(input_name: str) -> str:
+    lines = []
+    with open(input_name, "r") as input:
+        lines = input.readlines()
+
+    return "".join(lines)
+
+
 if __name__ == "__main__":
-    pass
+    data = extract_data("input.txt")
+    print(data)
+    # regex : mul\((\d+)\,(\d+)\)
+    match = re.search(r'mul\((\d+)\,(\d+)\)', data)
+    if match:
+        print('found', match.group())
+    else:
+        print('did not find')
+    to_compute = re.findall(r'mul\((\d+)\,(\d+)\)', data)
+    print(to_compute)
+    print(sum([int(t[0]) * int(t[1]) for t in to_compute])) #167090022
--- advent_of_code/2024/day3/python/aoc2024_3_2/AoC2024_d3_p2.py
+++ advent_of_code/2024/day3/python/aoc2024_3_2/AoC2024_d3_p2.py
@@ -1,2 +1,21 @@
+import re
+
+
+def extract_data(input_name: str) -> str:
+    data = None
+    with open(input_name, "r") as input:
+        data = input.read().replace('\n','').strip()
+
+    return "do()" + data + "don't()"
+
+
 if __name__ == "__main__":
-    pass
+    data = extract_data("input.txt")
+    filtered_data = "".join(re.findall(r"do\(\)(.*?)don't\(\)", data))
+    # First we suppress from the data the pattern don't\(\).*do\(\))*
+    # filtered_data = re.sub(r"don't\(\).*?do\(\)", '', data)
+    to_compute = re.findall(r'mul\((\d+)\,(\d+)\)', filtered_data)
+    print()
+    print(to_compute)
+    print()
+    print(sum([int(t[0]) * int(t[1]) for t in to_compute])) #89823704
