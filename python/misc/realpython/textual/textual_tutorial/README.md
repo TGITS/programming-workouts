c8faed96883e6795d334a7d274e05ddf4f962a87
TGITS <thegeekintheshell@gmail.com>
2025-04-07 19:31:39 +0200

    Travail sur tutoriel de "Real Python" sur Textual.

--- python/misc/realpython/textual/textual_tutorial/README.md
+++ python/misc/realpython/textual/textual_tutorial/README.md
@@ -1,7 +1,12 @@
 # README
 
 * Adapted from the Real Python Tutorial : ["Python Textual: Build Beautiful UIs in the Terminal"](https://realpython.com/python-textual/)
+  * The only difference with the original tutorial, is the use of uv
 * `uv init --python 3.13 textual_tutorial`
 * `uv add pytest ruff textual-dev --dev`
 * `uv add textual`
 * `uv add textual-dev --dev`
+* `uv run textual run --dev .\static_and_label_tcss.py`
+* `uv run textual serve .\static_and_label_tcss.py`
+* `uv run python .\horizontal_scroll.py`
+* `uv run python .\layouts.py`
---
+++ python/misc/realpython/textual/textual_tutorial/layouts.py
@@ -0,0 +1,31 @@
+from textual.app import App
+from textual.containers import Horizontal, HorizontalScroll, VerticalScroll
+from textual.widgets import Label, Static
+
+NUM_BOXES = 12
+
+
+class NestedContainerApp(App):
+    CSS_PATH = "layouts.tcss"
+
+    def compose(self):
+        with Horizontal(id="horizontal"):
+            yield Static("Left", classes="box")
+            with HorizontalScroll(id="horizontalscroll"):
+                for i in range(NUM_BOXES):
+                    yield Static(f"Center.{i + 1}", classes="box yellowbox")
+            with VerticalScroll(id="verticalscroll"):
+                for i in range(NUM_BOXES):
+                    yield Static(
+                        f"Right.{i + 1}",
+                        classes="box redbox",
+                    )
+                yield Label(
+                    "I am a docked label.\nI do not move!",
+                    id="docked-label",
+                )
+
+
+if __name__ == "__main__":
+    app = NestedContainerApp()
+    app.run()
