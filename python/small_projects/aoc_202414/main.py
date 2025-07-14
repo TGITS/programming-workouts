b9a738fd5affff1afac55779f457195996d4b172
TGITS <thegeekintheshell@gmail.com>
2025-07-14 18:17:09 +0200

    Travail préparatoire

---
+++ python/small_projects/aoc_202414/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from aoc-202414!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ python/small_projects/simple_image_converter/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from simple-image-converter!")
+
+
+if __name__ == "__main__":
+    main()
